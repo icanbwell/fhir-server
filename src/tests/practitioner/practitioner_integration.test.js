10c6522bd8bc2b9417f512f3603efaa7c4bbd578
imranq2 <imranq2@hotmail.com>
2020-11-15 17:34:47 -0800

    use async.waterfall in test

--- src/tests/practitioner/practitioner_integration.test.js
+++ src/tests/practitioner/practitioner_integration.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const { MongoClient } = require('mongodb');
 const supertest = require('supertest');
 
@@ -7,6 +8,7 @@ const { CLIENT, CLIENT_DB } = require('../../constants');
 const practitionerResource = require('./fixtures/providers/practitioner.json');
 const locationResource = require('./fixtures/providers/location.json');
 const practitionerRoleResource = require('./fixtures/providers/practitioner_role.json');
+const async = require('async');
 
 const request = supertest(app);
 
@@ -32,54 +34,77 @@ describe('Practitioner Integration Tests', () => {
 
   describe('Practitioner Integration Tests', () => {
     test('Provider Files Loads', (done) => {
-      request
-        .post('/4_0_0/Practitioner')
-        .send(practitionerResource)
-        .set('Content-Type', 'application/fhir+json')
-        .set('Accept', 'application/fhir+json')
-        .end((err, resp) => {
-          expect(err).toBeNull();
-          expect(resp.status).toBe(201);
-          expect(resp.headers.location.includes('4_0_0/Practitioner'));
-          // resourceId = resp.headers.location.split('/Practitioner/')[1];
-          done();
-        });
-        request
-        .post('/4_0_0/PractitionerRole')
-        .send(practitionerRoleResource)
-        .set('Content-Type', 'application/fhir+json')
-        .set('Accept', 'application/fhir+json')
-        .end((err, resp) => {
-          expect(err).toBeNull();
-          expect(resp.status).toBe(201);
-          expect(resp.headers.location.includes('4_0_0/Practitioner'));
-          // resourceId = resp.headers.location.split('/Practitioner/')[1];
-          done();
-        });
-        request
-        .post('/4_0_0/Location')
-        .send(locationResource)
-        .set('Content-Type', 'application/fhir+json')
-        .set('Accept', 'application/fhir+json')
-        .end((err, resp) => {
-          expect(err).toBeNull();
-          expect(resp.status).toBe(201);
-          expect(resp.headers.location.includes('4_0_0/Practitioner'));
-          // resourceId = resp.headers.location.split('/Practitioner/')[1];
-          done();
-        });
-        // now check the practitioner
-        request
-        .get('/4_0_0/Practitioner')
-        .set('Content-Type', 'application/fhir+json')
-        .set('Accept', 'application/fhir+json')
-        .end((err, resp) => {
-          console.log(JSON.stringify(resp.body, null, 2));
-          expect(err).toBeNull();
-          expect(resp.status).toBe(200);
-          // expect(resp.headers.location.includes('4_0_0/Practitioner'));
-          // resourceId = resp.headers.location.split('/Practitioner/')[1];
-          done();
+      async.waterfall([
+        (cb) => // first confirm there are no practitioners
+          request
+            .get('/4_0_0/Practitioner')
+            .set('Content-Type', 'application/fhir+json')
+            .set('Accept', 'application/fhir+json')
+            .end((err, resp) => {
+              console.log('------- response 1 ------------');
+              console.log(JSON.stringify(resp.body, null, 2));
+              console.log('------- end response 1 ------------');
+              expect(err).toBeNull();
+              expect(resp.status).toBe(200);
+              // expect(resp.headers.location.includes('4_0_0/Practitioner'));
+              // resourceId = resp.headers.location.split('/Practitioner/')[1];
+              done();
+            }),
+        (results, cb) => {
+          request
+            .post('/4_0_0/Practitioner')
+            .send(practitionerResource)
+            .set('Content-Type', 'application/fhir+json')
+            .set('Accept', 'application/fhir+json')
+            .end((err, resp) => {
+              expect(err).toBeNull();
+              expect(resp.status).toBe(201);
+              expect(resp.headers.location.includes('4_0_0/Practitioner'));
+              // resourceId = resp.headers.location.split('/Practitioner/')[1];
+              done();
+            });
+        },
+        (results, cb) => {
+          request
+            .post('/4_0_0/PractitionerRole')
+            .send(practitionerRoleResource)
+            .set('Content-Type', 'application/fhir+json')
+            .set('Accept', 'application/fhir+json')
+            .end((err, resp) => {
+              expect(err).toBeNull();
+              expect(resp.status).toBe(201);
+              expect(resp.headers.location.includes('4_0_0/Practitioner'));
+              // resourceId = resp.headers.location.split('/Practitioner/')[1];
+              done();
+            });
+        },
+        (results, cb) => request
+          .post('/4_0_0/Location')
+          .send(locationResource)
+          .set('Content-Type', 'application/fhir+json')
+          .set('Accept', 'application/fhir+json')
+          .end((err, resp) => {
+            expect(err).toBeNull();
+            expect(resp.status).toBe(201);
+            expect(resp.headers.location.includes('4_0_0/Practitioner'));
+            // resourceId = resp.headers.location.split('/Practitioner/')[1];
+            done();
+          }),
+        (results, cb) => request
+          .get('/4_0_0/Practitioner')
+          .set('Content-Type', 'application/fhir+json')
+          .set('Accept', 'application/fhir+json')
+          .end((err, resp) => {
+            // console.log(JSON.stringify(resp.body, null, 2));
+            expect(err).toBeNull();
+            expect(resp.status).toBe(200);
+            // expect(resp.headers.location.includes('4_0_0/Practitioner'));
+            // resourceId = resp.headers.location.split('/Practitioner/')[1];
+            done();
+          }),
+        (results, cb) => request(app).get('/api/users').expect(200, cb),
+      ],
+        (err, results) => {
         });
     });
   });
