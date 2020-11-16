feaae216c7767aa00f079e927a9cc14ba76d8056
imranq2 <imranq2@hotmail.com>
2020-11-15 18:15:04 -0800

    add expect promises

--- src/tests/practitioner/practitioner_integration.test.js
+++ src/tests/practitioner/practitioner_integration.test.js
@@ -40,32 +40,57 @@ describe('Practitioner Integration Tests', () => {
             .get('/4_0_0/Practitioner')
             .set('Content-Type', 'application/fhir+json')
             .set('Accept', 'application/fhir+json')
-            .expect(200, cb),
+            .expect(200, (err, resp) => {
+              console.log('------- response 1 ------------');
+              console.log(JSON.stringify(resp.body, null, 2));
+              console.log('------- end response 1 ------------');
+              return cb(err, resp);
+            }),
         (results, cb) =>
           request
             .post('/4_0_0/Practitioner')
             .send(practitionerResource)
             .set('Content-Type', 'application/fhir+json')
             .set('Accept', 'application/fhir+json')
-            .expect(200, cb),
+            .expect(200, (err, resp) => {
+              console.log('------- response 2 ------------');
+              console.log(JSON.stringify(resp.body, null, 2));
+              console.log('------- end response 2  ------------');
+              return cb(err, resp);
+            }),
         (results, cb) =>
           request
             .post('/4_0_0/PractitionerRole')
             .send(practitionerRoleResource)
             .set('Content-Type', 'application/fhir+json')
             .set('Accept', 'application/fhir+json')
-            .expect(200, cb),
+            .expect(200, (err, resp) => {
+              console.log('------- response 3 ------------');
+              console.log(JSON.stringify(resp.body, null, 2));
+              console.log('------- end response 3  ------------');
+              return cb(err, resp);
+            }),
         (results, cb) => request
           .post('/4_0_0/Location')
           .send(locationResource)
           .set('Content-Type', 'application/fhir+json')
           .set('Accept', 'application/fhir+json')
-          .expect(200, cb),
+          .expect(200, (err, resp) => {
+            console.log('------- response 4 ------------');
+            console.log(JSON.stringify(resp.body, null, 2));
+            console.log('------- end response 4  ------------');
+            return cb(err, resp);
+          }),
         (results, cb) => request
           .get('/4_0_0/Practitioner')
           .set('Content-Type', 'application/fhir+json')
           .set('Accept', 'application/fhir+json')
-          .expect(200, cb),
+          .expect(200, (err, resp) => {
+            console.log('------- response 5 ------------');
+            console.log(JSON.stringify(resp.body, null, 2));
+            console.log('------- end response 5  ------------');
+            return cb(err, resp);
+          }),
       ],
         (err, results) => {
           console.log('done');
