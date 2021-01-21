463ae2230715cc9ef91856e08ad1ac2fc878f4a2
imranq2 <imranq2@hotmail.com>
2021-01-20 20:13:36 -0800

    add sorting descending test

--- src/tests/practitioner/sort_results/sort_results.test.js
+++ src/tests/practitioner/sort_results/sort_results.test.js
@@ -51,6 +51,14 @@ describe('PractitionerReturnIdTests', () => {
         await connection.close();
     });
 
+    function sleep(milliseconds) {
+        const date = Date.now();
+        let currentDate = null;
+        do {
+            currentDate = Date.now();
+        } while (currentDate - date < milliseconds);
+    }
+
     describe('Practitioner Sort Tests', () => {
         test('sort equals', (done) => {
             async.waterfall([
@@ -105,6 +113,94 @@ describe('PractitionerReturnIdTests', () => {
                             }),
                     (results, cb) => request
                         .get('/4_0_0/Practitioner?_sort=id')
+                        .set('Content-Type', 'application/fhir+json')
+                        .set('Accept', 'application/fhir+json')
+                        .expect(200, cb)
+                        .expect((resp) => {
+                            console.log('------- response Practitioner sorted ------------');
+                            console.log(JSON.stringify(resp.body, null, 2));
+                            console.log('------- end response sort ------------');
+                            // clear out the lastUpdated column since that changes
+                            let body = resp.body;
+                            expect(body.length).toBe(2);
+                            body.forEach(element => {
+                                delete element['meta']['lastUpdated'];
+                            });
+                            let expected = expectedPractitionerResource;
+                            expected.forEach(element => {
+                                delete element['meta']['lastUpdated'];
+                                delete element['$schema'];
+                            });
+                            // expected[0]['meta'] = { 'versionId': '2' };
+                            expect(body).toStrictEqual(expected);
+                        }, cb),
+                ],
+                (err, results) => {
+                    if (!err) {
+                        console.log('done');
+                    }
+
+                    if (err) {
+                        console.error(err);
+                        done.fail(err);
+                    }
+                    done();
+                });
+        });
+        test('sort equals nested descending', (done) => {
+            async.waterfall([
+                    (cb) => // first confirm there are no practitioners
+                        request
+                            .get('/4_0_0/Practitioner')
+                            .set('Content-Type', 'application/fhir+json')
+                            .set('Accept', 'application/fhir+json')
+                            .expect(200, (err, resp) => {
+                                expect(resp.body.length).toBe(0);
+                                console.log('------- response 1 ------------');
+                                console.log(JSON.stringify(resp.body, null, 2));
+                                console.log('------- end response 1 ------------');
+                                return cb(err, resp);
+                            }),
+                    (results, cb) =>
+                        request
+                            .post('/4_0_0/Practitioner/1679033641/$merge')
+                            .send(practitionerResource)
+                            .set('Content-Type', 'application/fhir+json')
+                            .set('Accept', 'application/fhir+json')
+                            .expect(200, (err, resp) => {
+                                console.log('------- response practitionerResource ------------');
+                                console.log(JSON.stringify(resp.body, null, 2));
+                                console.log('------- end response  ------------');
+                                expect(resp.body['created']).toBe(true);
+                                sleep(3000); // sleep so the lastUpdated data is different for next one
+                                return cb(err, resp);
+                            }),
+                    (results, cb) =>
+                        request
+                            .post('/4_0_0/Practitioner/0/$merge')
+                            .send(practitionerResource2)
+                            .set('Content-Type', 'application/fhir+json')
+                            .set('Accept', 'application/fhir+json')
+                            .expect(200, (err, resp) => {
+                                console.log('------- response practitionerResource ------------');
+                                console.log(JSON.stringify(resp.body, null, 2));
+                                console.log('------- end response  ------------');
+                                expect(resp.body['created']).toBe(true);
+                                return cb(err, resp);
+                            }),
+                    (results, cb) =>
+                        request
+                            .get('/4_0_0/Practitioner')
+                            .set('Content-Type', 'application/fhir+json')
+                            .set('Accept', 'application/fhir+json')
+                            .expect(200, (err, resp) => {
+                                console.log('------- response 3 ------------');
+                                console.log(JSON.stringify(resp.body, null, 2));
+                                console.log('------- end response 3 ------------');
+                                return cb(err, resp);
+                            }),
+                    (results, cb) => request
+                        .get('/4_0_0/Practitioner?_sort=-meta.lastUpdated')
                         .set('Content-Type', 'application/fhir+json')
                         .set('Accept', 'application/fhir+json')
                         .expect(200, cb)
