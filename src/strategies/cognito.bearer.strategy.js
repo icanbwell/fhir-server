33edd591728f72e1bd3d8e89971d62ca308344fe
imranq2 <imranq2@hotmail.com>
2021-03-24 21:15:23 -0700

    update strategy

--- src/strategies/cognito.bearer.strategy.js
+++ src/strategies/cognito.bearer.strategy.js
@@ -1,5 +1,5 @@
 const Strategy = require('passport-http-bearer').Strategy;
-const cognitoAuthService = require('../utils/cognitoAuthService');
+// const cognitoAuthService = require('../utils/cognitoAuthService');
 
 /**
  * Bearer Strategy
@@ -9,16 +9,21 @@ const cognitoAuthService = require('../utils/cognitoAuthService');
  *
  * Requires ENV variables for introspecting the token
  */
-module.exports.strategy = new Strategy(function (token, done) {
-    // https://docs.aws.amazon.com/cognito/latest/developerguide/amazon-cognito-user-pools-using-tokens-verifying-a-jwt.html
-    cognitoAuthService.validate(token, function (err, result, decodedToken) {
-        if (err) {
-            return done(null, false);
-        }
-        const client_id = decodedToken.client_id;
-        const scope = decodedToken.scope;
-        console.info('client_id: ' + client_id + 'scope: ' + scope);
-        const context = null;
-        return done(null, client_id, {scope, context});
-    });
-});
+module.exports.strategy = new Strategy(
+    function (token, done) {
+        return done(null, false);
+    }
+    // async function (token, done) {
+    //     // https://docs.aws.amazon.com/cognito/latest/developerguide/amazon-cognito-user-pools-using-tokens-verifying-a-jwt.html
+    //     try {
+    //         const decodedToken = await cognitoAuthService.validate(token);
+    //         const client_id = decodedToken.client_id;
+    //         const scope = decodedToken.scope;
+    //         console.info('client_id: ' + client_id + 'scope: ' + scope);
+    //         const context = null;
+    //         return done(null, client_id, {scope, context});
+    //     } catch (e) {
+    //         return done(null, false);
+    //     }
+    // }
+);
