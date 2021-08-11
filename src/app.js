4251997f0e64611acd62525d5ddf78108072c943
imranq2 <imranq2@hotmail.com>
2021-08-11 10:34:41 -0700

    remove console.log

--- src/app.js
+++ src/app.js
@@ -77,14 +77,15 @@ class MyFHIRServer extends FHIRServer.Server {
                 // console.log('--- user agent ----');
                 // console.log(req.useragent);
                 const parts = req.url.split(/[/?,&]+/);
-                console.log('----- parts -----');
-                console.log(parts);
-                console.log(parts.length);
+                // console.log('----- parts -----');
+                // console.log(parts);
+                // console.log(parts.length);
                 if (parts && parts.length > 2 && !parts.includes('raw=1') && parts[1] === '4_0_0') {
                     const resourceName = parts[2].toLowerCase();
                     if (req.accepts('text/html') && req.method === 'GET' && req.useragent && req.useragent.isDesktop) {
                         let oldJson = res.json;
                         res.json = (data) => {
+                            // const myReq = req;
                             // const myReq = req;
                             let parsedData = JSON.parse(JSON.stringify(data));
                             // console.log(parsedData); // do something with the data
