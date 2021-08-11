566d804e5fac16a5bd55387962c5e57acee0d4c0
imranq2 <imranq2@hotmail.com>
2021-08-11 08:13:47 -0700

    add check for url parts

--- src/app.js
+++ src/app.js
@@ -71,32 +71,35 @@ class MyFHIRServer extends FHIRServer.Server {
             //     // return res.send(data); // just call as normal with data
             //     return res.render(__dirname + '/views/pages/index', { resource: data});
             // };
-            console.log('--- user agent ----');
-            console.log(req.useragent);
-            if (req.accepts('text/html') && req.method === 'GET' && req.useragent && req.useragent.isDesktop) {
-                let oldJson = res.json;
-                res.json = (data) => {
-                    // const myReq = req;
-                    const resourceName = req.url.split('/')[2].toLowerCase();
-                    let parsedData = JSON.parse(JSON.stringify(data));
-                    console.log(parsedData); // do something with the data
-                    if (parsedData.entry) {
-                        parsedData = parsedData.entry.map((entry) => entry.resource);
-                    } else if (!Array.isArray(parsedData)) {
-                        parsedData = [parsedData];
-                    }
-                    res.json = oldJson; // set function back to avoid the 'double-send'
-                    // return res.json(data); // just call as normal with data
-                    res.set('Content-Type', 'text/html');
-                    // This is so we can include the bootstrap css from CDN
-                    res.set('Content-Security-Policy', "style-src 'self' stackpath.bootstrapcdn.com;");
-                    const customViews = ['patient', 'practitioner'];
-                    if (customViews.includes(resourceName)) {
-                        return res.render(__dirname + '/views/pages/' + resourceName, {resource: parsedData});
-                    } else {
-                        return res.render(__dirname + '/views/pages/index', {resource: parsedData});
-                    }
-                };
+            // console.log('--- user agent ----');
+            // console.log(req.useragent);
+            const parts = req.url.split('/');
+            if (parts && parts.length > 1) {
+                const resourceName = parts[2].toLowerCase();
+                if (req.accepts('text/html') && req.method === 'GET' && req.useragent && req.useragent.isDesktop) {
+                    let oldJson = res.json;
+                    res.json = (data) => {
+                        // const myReq = req;
+                        let parsedData = JSON.parse(JSON.stringify(data));
+                        console.log(parsedData); // do something with the data
+                        if (parsedData.entry) {
+                            parsedData = parsedData.entry.map((entry) => entry.resource);
+                        } else if (!Array.isArray(parsedData)) {
+                            parsedData = [parsedData];
+                        }
+                        res.json = oldJson; // set function back to avoid the 'double-send'
+                        // return res.json(data); // just call as normal with data
+                        res.set('Content-Type', 'text/html');
+                        // This is so we can include the bootstrap css from CDN
+                        res.set('Content-Security-Policy', "style-src 'self' stackpath.bootstrapcdn.com;");
+                        const customViews = ['patient', 'practitioner'];
+                        if (customViews.includes(resourceName)) {
+                            return res.render(__dirname + '/views/pages/' + resourceName, {resource: parsedData});
+                        } else {
+                            return res.render(__dirname + '/views/pages/index', {resource: parsedData});
+                        }
+                    };
+                }
             }
             next();
         });
