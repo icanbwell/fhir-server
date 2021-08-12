c954e3269ace6dbda346a3ba7f71490250507a2e
imranq2 <imranq2@hotmail.com>
2021-08-11 17:04:15 -0700

    add explanationofbenefit.ejs

--- src/app.js
+++ src/app.js
@@ -100,7 +100,7 @@ class MyFHIRServer extends FHIRServer.Server {
                             // This is so we can include the bootstrap css from CDN
                             res.set('Content-Security-Policy', "style-src 'self' stackpath.bootstrapcdn.com;");
                             // console.log('resource: ' + resourceName);
-                            const customViews = ['patient', 'practitioner', 'practitionerrole', 'location', 'organization'];
+                            const customViews = ['patient', 'practitioner', 'practitionerrole', 'location', 'organization', 'explanationofbenefit'];
                             const options = {resources: parsedData, url: req.url};
                             if (customViews.includes(resourceName)) {
                                 return res.render(__dirname + '/views/pages/' + resourceName, options);
