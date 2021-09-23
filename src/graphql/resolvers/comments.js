026b802ce057da41b15859b3b6690812281526e8
imranq2 <imranq2@hotmail.com>
2021-09-23 10:22:20 -0700

    add link from Comment to Post

--- src/graphql/resolvers/comments.js
+++ src/graphql/resolvers/comments.js
@@ -1,10 +1,17 @@
+const {posts, comments} = require('../fakedata');
+
 module.exports = {
-  // Query: {
-  //   comments: () => {},
-  //   comment: () => {},
-  // },
-  // Comment: {
-  //   posts: () => {},
-  // },
+  Query: {
+    // eslint-disable-next-line no-unused-vars
+    comments: async (parent, args, context, info) => { return comments;},
+    // eslint-disable-next-line no-unused-vars
+    comment: async (parent, args, context, info) => { return comments[0];},
+  },
+  Comment: {
+      // eslint-disable-next-line no-unused-vars
+    post: async (parent, args, context, info) => {
+      return posts.filter(x => x.id === parent.postId)[0];
+    },
+  },
 };
 
