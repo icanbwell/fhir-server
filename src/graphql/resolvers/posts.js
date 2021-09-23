8dbfff2b9e3b6d9fd32c24482aa5c899b975de22
imranq2 <imranq2@hotmail.com>
2021-09-23 10:02:46 -0700

    return comments for post

--- src/graphql/resolvers/posts.js
+++ src/graphql/resolvers/posts.js
@@ -4,6 +4,14 @@ const posts = [
   }
 ];
 
+const comments = [
+  {
+    id: '101',
+    postId: '1',
+    text: 'my comment'
+  }
+];
+
 module.exports = {
   Query: {
     // eslint-disable-next-line no-unused-vars
@@ -13,7 +21,9 @@ module.exports = {
   },
   Post: {
     // eslint-disable-next-line no-unused-vars
-    comments: async (parent, args, context, info) => {},
+    comments: async (parent, args, context, info) => {
+      return comments.filter(x => x.postId === parent.id);
+    },
   },
 };
 
