6edf09c42e00cb4735de76dbeb7795dd83ec3e36
imranq2 <imranq2@hotmail.com>
2021-09-30 19:37:50 -0700

    separate providers into files

--- src/enrich/enrich.js
+++ src/enrich/enrich.js
@@ -1,48 +1,4 @@
-class EnrichmentProvider {
-    /**
-     * Whether this Enrichment can enrich the specified resourceType
-     * @param {string} resourceType
-     * @return {boolean}
-     */
-    // eslint-disable-next-line no-unused-vars
-    canEnrich(resourceType) {
-        throw Error('Not Implemented');
-    }
-
-    /**
-     * enrich the specified resources
-     * @param {Resource[]} resources
-     * @param {string} resourceType
-     * @return {Promise<Resource[]>}
-     */
-    // eslint-disable-next-line no-unused-vars
-    async enrich(resources, resourceType) {
-        throw Error('Not Implemented');
-    }
-}
-
-class ExplanationOfBenefitsEnrichmentProvider extends EnrichmentProvider {
-    /**
-     * Whether this Enrichment can enrich the specified resourceType
-     * @param {string} resourceType
-     * @return {boolean}
-     */
-    canEnrich(resourceType) {
-        return (resourceType === 'ExplanationOfBenefit');
-    }
-
-    /**
-     * enrich the specified resources
-     * @param {Resource[]} resources
-     * @param {string} resourceType
-     * @return {Promise<Resource[]>}
-     */
-    // eslint-disable-next-line no-unused-vars
-    async enrich(resources, resourceType) {
-        return resources;
-    }
-}
-
+const ExplanationOfBenefitsEnrichmentProvider = require('./providers/explanationOfBenefitsEnrichmentProvider');
 
 /**
  * Registered set of enrichment providers
