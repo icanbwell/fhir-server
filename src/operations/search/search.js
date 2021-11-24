541ec91464ae76cb98e0802dda99779fac49d631
imranq2 <imranq2@hotmail.com>
2021-11-23 21:34:27 -0800

    temp: always return id and url

--- src/operations/search/search.js
+++ src/operations/search/search.js
@@ -136,6 +136,8 @@ module.exports.search = async (args, user, scope, resourceName, collection_name,
                     projection[`${property}`] = 1;
                     columns.add(property);
                 }
+                projection['id'] = 1;
+                projection['url'] = 1;
                 // also exclude _id so if there is a covering index the query can be satisfied from the covering index
                 projection['_id'] = 0;
                 options['projection'] = projection;
@@ -277,6 +279,8 @@ module.exports.search = async (args, user, scope, resourceName, collection_name,
                         element_to_return[`${property}`] = element[`${property}`];
                     }
                 }
+                element_to_return['id'] = element['id'];
+                element_to_return['url'] = element['url'];
                 resources.push(element_to_return);
             } else {
                 resources.push(new Resource(element));
@@ -375,7 +379,7 @@ module.exports.search = async (args, user, scope, resourceName, collection_name,
                 bundle['meta'] = {
                     tag: tag
                 };
-                logDebug(user, JSON.stringify(tag));
+                logDebug(user, JSON.stringify(bundle));
             }
             return bundle;
         } else {
