11cf92f9c27c2d404be51677aea07db43c6b5fb1
imranq2 <imranq2@hotmail.com>
2021-12-18 18:09:46 -0800

    fix test

--- src/operations/search/query/r4.js
+++ src/operations/search/query/r4.js
@@ -172,11 +172,16 @@ module.exports.buildR4SearchQuery = (resourceName, args) => {
                                     ]
                                 }
                             );
-                            columns.add(`${propertyObj.field}.system`);
-                            columns.add(`${propertyObj.field}.code`);
-                            // and_segments.push(tokenQueryBuilder(args[`${queryParameter}`], 'code', `${propertyObj.field}.coding`, ''));
-                            // columns.add(`${propertyObj.field}.coding.system`);
-                            // columns.add(`${propertyObj.field}.coding.code`);
+                            // HACK ALERT: token can be for either CodeableConcept or Coding.  Currently, we can't figure out in advance
+                            //  Ideally we would set the former when we have a Coding and the latter when we have a CodeableConcept
+                            //  but this would require us parsing the FHIR resource schemas to know that
+                            if (propertyObj.field === 'meta.security' || propertyObj.field === 'meta.tag') {
+                                columns.add(`${propertyObj.field}.system`);
+                                columns.add(`${propertyObj.field}.code`);
+                            } else {
+                                columns.add(`${propertyObj.field}.coding.system`);
+                                columns.add(`${propertyObj.field}.coding.code`);
+                            }
                             break;
                         case fhirFilterTypes.email:
                             and_segments.push(tokenQueryBuilder(args[`${queryParameter}`], 'value', `${propertyObj.field}`, 'email'));
