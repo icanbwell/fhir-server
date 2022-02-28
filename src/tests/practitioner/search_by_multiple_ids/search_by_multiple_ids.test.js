2d4b3fc2497c318ce886773d5ecbe861739e2020
imranq2 <imranq2@hotmail.com>
2022-02-28 10:28:46 -0800

    add test for receiving body as x-www-form-urlencoded

--- src/tests/practitioner/search_by_multiple_ids/search_by_multiple_ids.test.js
+++ src/tests/practitioner/search_by_multiple_ids/search_by_multiple_ids.test.js
@@ -11,7 +11,7 @@ const expectedPractitionerResource = require('./fixtures/expected/expected_pract
 const expectedSinglePractitionerResource = require('./fixtures/expected/expected_single_practitioner.json');
 
 const request = supertest(app);
-const {commonBeforeEach, commonAfterEach, getHeaders} = require('../../common');
+const {commonBeforeEach, commonAfterEach, getHeaders, getHeadersFormUrlEncoded} = require('../../common');
 
 describe('PractitionerReturnIdTests', () => {
     beforeEach(async () => {
@@ -191,6 +191,60 @@ describe('PractitionerReturnIdTests', () => {
                 .post('/4_0_0/Practitioner/_search?_sort=id')
                 .send({id: '0,1679033641'})
                 .set(getHeaders())
+                .expect(200);
+
+            console.log('------- response Practitioner sorted ------------');
+            console.log(JSON.stringify(resp.body, null, 2));
+            console.log('------- end response sort ------------');
+            // clear out the lastUpdated column since that changes
+            let body = resp.body;
+            expect(body.length).toBe(2);
+            body.forEach(element => {
+                delete element['meta']['lastUpdated'];
+            });
+            let expected = expectedPractitionerResource;
+            expected.forEach(element => {
+                delete element['meta']['lastUpdated'];
+                delete element['$schema'];
+            });
+            // expected[0]['meta'] = { 'versionId': '2' };
+            expect(body).toStrictEqual(expected);
+        });
+        test('search by multiple id works via POST (x-www-form-urlencoded)', async () => {
+            let resp = await request
+                .post('/4_0_0/Practitioner/1679033641/$merge')
+                .send(practitionerResource)
+                .set(getHeaders())
+                .expect(200);
+            console.log('------- response practitionerResource ------------');
+            console.log(JSON.stringify(resp.body, null, 2));
+            console.log('------- end response  ------------');
+            expect(resp.body['created']).toBe(true);
+
+            resp = await request
+                .post('/4_0_0/Practitioner/0/$merge')
+                .send(practitionerResource2)
+                .set(getHeaders())
+                .expect(200);
+            console.log('------- response practitionerResource ------------');
+            console.log(JSON.stringify(resp.body, null, 2));
+            console.log('------- end response  ------------');
+            expect(resp.body['created']).toBe(true);
+
+            resp = await request
+                .post('/4_0_0/Practitioner/1/$merge')
+                .send(practitionerResource3)
+                .set(getHeaders())
+                .expect(200);
+            console.log('------- response practitionerResource ------------');
+            console.log(JSON.stringify(resp.body, null, 2));
+            console.log('------- end response  ------------');
+            expect(resp.body['created']).toBe(true);
+
+            resp = await request
+                .post('/4_0_0/Practitioner/_search?_sort=id')
+                .send('id=0,1679033641')
+                .set(getHeadersFormUrlEncoded())
                 .expect(200);
 
             console.log('------- response Practitioner sorted ------------');
