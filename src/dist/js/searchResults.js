c72f2eb4c4f543b6ef6655859a19b678989566c8
bwell-bruce <bruce.braithwaite@icanbwell.com>
2022-03-18 16:29:22 -0600

    Sort search results

--- src/dist/js/searchResults.js
+++ src/dist/js/searchResults.js
@@ -9,3 +9,22 @@ function rowClick(event) {
     const url = window.location.pathname.replace('/_search', `/${identifier}`);
     window.open(url, '_blank');
 }
+
+const tableHeaders = document.querySelectorAll('th[data-field]');
+tableHeaders.forEach((th) => {
+    th.addEventListener('click', () => {
+        const field = th.getAttribute('data-field');
+        sortByField(field);
+    });
+});
+
+function sortByField(fieldName) {
+    const sortElement = document.querySelector('input[name=_sort]');
+    sortElement.value =
+        sortElement.value === fieldName
+            ? `-${fieldName}`
+            : sortElement.value === `-${fieldName}`
+            ? ''
+            : fieldName;
+    submitSearchForm();
+}
