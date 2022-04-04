5b482dbc291b9b3fc4b45ad83c47b5079463b10f
imranq2 <imranq2@hotmail.com>
2022-04-03 20:16:41 -0700

    add logging for graphql

--- src/middleware/graphqlLoggingPlugin.js
+++ src/middleware/graphqlLoggingPlugin.js
@@ -1,18 +1,29 @@
 const {logRequest, logError} = require('../operations/common/logging');
 
+/***
+ * Plugin to log calls to GraphQL
+ */
 class MyApolloServerLoggingPlugin {
     /***
      * This plugin logs calls to the GraphQL
      * https://www.apollographql.com/docs/apollo-server/integrations/plugins/
-     * @param endpoint
+     * @param {string} endpoint
      */
     constructor(endpoint) {
+        /**
+         * @type {string}
+         */
         this.endpoint = endpoint;
     }
 
-    // noinspection JSUnusedLocalSymbols
-    // eslint-disable-next-line no-unused-vars
-    // Fires whenever a GraphQL request is received from a client.
+    /**
+     * The requestDidStart event fires whenever Apollo Server receives a GraphQL request, before any of the lifecycle
+     * events listed above. You can respond to this event just like you respond to serverWillStart, but you also use
+     * this function to define responses for a request's lifecycle events
+     * https://www.apollographql.com/docs/apollo-server/integrations/plugins/
+     * @param {import("apollo-server-core/dist/requestPipeline").GraphQLRequestContext} requestContext
+     * @return {Promise<{executionDidEnd(*): Promise<void>}|{executionDidStart(): Promise<{executionDidEnd(*): Promise<void>}>, parsingDidStart(): Promise<function(*): Promise<void>>, validationDidStart(): Promise<function(*): Promise<void>>}|(function(*): Promise<void>)|*>}
+     */
     async requestDidStart(requestContext) {
         const req = requestContext.request;
         const user = (req.authInfo && req.authInfo.context && req.authInfo.context.username)
@@ -21,15 +32,38 @@ class MyApolloServerLoggingPlugin {
         logRequest(user, `GraphQL Request ${this.endpoint} Op:${req.operationName}, query:${req.query}`);
 
         return {
-            // https://www.apollographql.com/docs/apollo-server/integrations/plugins-event-reference/#didencountererrors
-            async didEncounterErrors(/*requestContext*/) {
-                logError(user, `GraphQL Request ${this.endpoint} Op:${req.operationName}, query:${req.query}`);
+            async parsingDidStart() {
+                return async (err) => {
+                    if (err) {
+                        logError(user, `GraphQL Request Parsing Error ${this.endpoint} Op:${req.operationName}, query:${req.query}, error: ${err}`);
+                    }
+                };
             },
-
-            // Fires whenever Apollo Server will validate a
-            // request's document AST against your GraphQL schema.
-            async validationDidStart(/*requestContext*/) {
-                console.log('Validation started!');
+            async validationDidStart() {
+                // This end hook is unique in that it can receive an array of errors,
+                // which will contain every validation error that occurred.
+                return async (errs) => {
+                    if (errs) {
+                        errs.forEach(
+                            err => logError(
+                                user,
+                                `GraphQL Request Validation Error ${this.endpoint} Op:${req.operationName}, query:${req.query}, error: ${err}`
+                            )
+                        );
+                    }
+                };
+            },
+            async executionDidStart() {
+                return {
+                    async executionDidEnd(err) {
+                        if (err) {
+                            logError(
+                                user,
+                                `GraphQL Request Execution Error ${this.endpoint} Op:${req.operationName}, query:${req.query}, error: ${err}`
+                            );
+                        }
+                    }
+                };
             },
         };
     }
