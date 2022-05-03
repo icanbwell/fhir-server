756b68168150b81ae9739890d222fe084483a2c6
imranq2 <imranq2@hotmail.com>
2022-05-02 21:46:26 -0700

    add async iterator

--- src/operations/search/readResourcesFromCursor.js
+++ src/operations/search/readResourcesFromCursor.js
@@ -1,6 +1,23 @@
 const {pipeline} = require('stream/promises');
 const {prepareResource} = require('../common/resourcePreparer');
 const {logError, logDebug} = require('../common/logging');
+const stream = require('stream');
+
+
+class ResourcesWritable extends stream.Writable {
+    constructor(options) {
+        options = options || {};
+        super({objectMode: true});
+
+        this.resources = options.resources;
+    }
+
+    _write(chunk, encoding, callback) {
+        console.log(`write: chunk:${this.resources.length}`);
+        this.resources.push(chunk);
+        callback();
+    }
+}
 
 /**
  * Reads resources from Mongo cursor
@@ -25,53 +42,75 @@ async function readResourcesFromCursor(cursor, user, scope, args, Resource, reso
      * https://mongodb.github.io/node-mongodb-native/4.5/interfaces/CursorStreamOptions.html
      * @type {Readable}
      */
-    // const stream = cursor.stream();
+    const cursorStream = cursor.stream();
 
     try {
         // https://mongodb.github.io/node-mongodb-native/4.5/classes/FindCursor.html
         // https://nodejs.org/docs/latest-v16.x/api/stream.html#streams-compatibility-with-async-generators-and-async-iterators
+        // https://nodejs.org/docs/latest-v16.x/api/stream.html#additional-notes
+        // const resourcesWritable = new ResourcesWritable({resources: resources});
         await pipeline(
-            async function* () {
-                let chunk_number = 0;
-                // https://stackoverflow.com/questions/23915967/mongodb-nodejs-native-cursor-closing-prematurely
-                while (await cursor.hasNext()) {
-                    logDebug(user, `Buffered count=${cursor.bufferedCount()}`);
-                    chunk_number += 1;
-                    /**
-                     * element
-                     * @type {Resource}
-                     */
-                    console.log(`read: chunk:${chunk_number}`);
-                    yield await cursor.next();
-                }
-            },
+            cursorStream,
+            // async function* () {
+            //     let chunk_number = 0;
+            //     // https://stackoverflow.com/questions/23915967/mongodb-nodejs-native-cursor-closing-prematurely
+            //     while (await cursor.hasNext()) {
+            //         logDebug(user, `Buffered count=${cursor.bufferedCount()}`);
+            //         chunk_number += 1;
+            //         /**
+            //          * element
+            //          * @type {Resource}
+            //          */
+            //         console.log(`read: chunk:${chunk_number}`);
+            //         yield await cursor.next();
+            //     }
+            // },
+            // async function* (source) {
+            //     let chunk_number = 0;
+            //     for await (const chunk of source) {
+            //         chunk_number += 1;
+            //         console.log(`prepareResource: chunk:${chunk_number}`);
+            //         yield await prepareResource(user, scope, args, Resource, chunk, resourceName);
+            //     }
+            // },
             // new ResourcePreparerTransform(user, scope, args, Resource, resourceName),
-            async function* (source) {
+            // async function* (source) {
+            //     let chunk_number = 0;
+            //     for await (const chunk of source) {
+            //         chunk_number += 1;
+            //         console.log(`prepareResource: chunk:${chunk_number}`);
+            //         yield await prepareResource(user, scope, args, Resource, chunk, resourceName);
+            //     }
+            // },
+            // // streamToArray
+            // function (source) {
+            //     let chunk_number = 0;
+            //     for (const chunk of source) {
+            //         chunk_number += 1;
+            //         // let item_number = 0;
+            //         console.log(`streamToArray: chunk:${chunk_number}`);
+            //         // for (const item1 of chunk) {
+            //         //     item_number += 1;
+            //         //     console.log(`streamToArray: chunk:${chunk_number}, item:${item_number}`);
+            //         //     // resources.push(item1);
+            //         // }
+            //         // yield 1;
+            //     }
+            // }
+            // resourcesWritable
+            async function (source) {
                 let chunk_number = 0;
                 for await (const chunk of source) {
                     chunk_number += 1;
-                    console.log(`prepareResource: chunk:${chunk_number}`);
-                    yield await prepareResource(user, scope, args, Resource, chunk, resourceName);
+                    console.log(`streamToArray: chunk:${chunk_number}`);
+                    resources.push(chunk);
                 }
-            },
-            // streamToArray
-            async function* (source) {
-                let chunk_number = 0;
-                for await (const chunk of source) {
-                    chunk_number += 1;
-                    let item_number = 0;
-                    for (const item1 of chunk) {
-                        item_number += 1;
-                        console.log(`streamToArray: chunk:${chunk_number}, item:${item_number}`);
-                        resources.push(item1);
-                    }
-                    yield 1;
-                }
-            },
+            }
         );
     } catch (e) {
         logError(user, e);
     }
+    logDebug(user, 'Done with loading resources');
     return resources;
 }
 
