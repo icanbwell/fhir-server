dd439a985d67b80668be3504a4986edcd5c0b851
imranq2 <imranq2@hotmail.com>
2022-05-24 13:28:47 -0700

    update tests

--- src/tests/schedule/merge_array_items/merge_array_items.test.js
+++ src/tests/schedule/merge_array_items/merge_array_items.test.js
@@ -3,6 +3,7 @@ const supertest = require('supertest');
 const {app} = require('../../../app');
 // test file
 const schedule1Resource = require('./fixtures/Schedule/schedule1.json');
+const schedule2Resource = require('./fixtures/Schedule/schedule2.json');
 
 // expected
 const expectedScheduleResources = require('./fixtures/expected/expected_Schedule.json');
@@ -31,10 +32,17 @@ describe('Schedule Tests', () => {
                 .expect(200);
             assertMergeIsSuccessful(resp.body);
 
+            resp = await request
+                .post('/4_0_0/Schedule/1/$merge?validate=true')
+                .send(schedule2Resource)
+                .set(getHeaders())
+                .expect(200);
+            assertMergeIsSuccessful(resp.body, false);
+
             // ACT & ASSERT
             // search by token system and code and make sure we get the right Schedule back
             resp = await request
-                .get('/4_0_0/Schedule/?_bundle=1&id=1720233406-SCH-MPCS')
+                .get('/4_0_0/Schedule/?_bundle=1')
                 .set(getHeaders())
                 .expect(200);
             assertCompareBundles(resp.body, expectedScheduleResources);
