621492d41f49c2d636e5e786ed4283715f5aa091
imranq2 <imranq2@hotmail.com>
2022-05-24 14:17:00 -0700

    add partialText handling

---
+++ src/operations/query/filters/partialText.js
@@ -0,0 +1,29 @@
+const {partialTextQueryBuilder} = require('../../../utils/querybuilder.util');
+
+/**
+ * Filters by missing
+ * https://www.hl7.org/fhir/search.html#modifiers
+ * @param {Object} args
+ * @param {string} queryParameter
+ * @param {Object[]} and_segments
+ * @param {import('../common/types').SearchParameterDefinition} propertyObj
+ * @param {Set} columns
+ */
+function filterByPartialText(args, queryParameter, and_segments, propertyObj, columns) {
+    /**
+     * @type {string}
+     */
+    const textToSearchFor = args[`${queryParameter}:text`];
+    and_segments.push(
+        partialTextQueryBuilder(
+            `${propertyObj.field}.text`,
+            textToSearchFor,
+            true
+        )
+    );
+    columns.add(`${propertyObj.field}`);
+}
+
+module.exports = {
+    filterByPartialText: filterByPartialText
+};
