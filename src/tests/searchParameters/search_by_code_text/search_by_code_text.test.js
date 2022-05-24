15ded4dcd17db4c3e1fb010eea96cd0036c7d7d5
imranq2 <imranq2@hotmail.com>
2022-05-24 13:36:59 -0700

    add test

---
+++ src/tests/searchParameters/search_by_code_text/search_by_code_text.test.js
@@ -0,0 +1,43 @@
+const supertest = require('supertest');
+
+const {app} = require('../../../app');
+// test file
+const medication1Resource = require('./fixtures/Medication/medication1.json');
+
+// expected
+const expectedMedicationResources = require('./fixtures/expected/expected_Medication.json');
+
+const request = supertest(app);
+const {commonBeforeEach, commonAfterEach, getHeaders} = require('../../common');
+const {assertCompareBundles, assertMergeIsSuccessful} = require('../../fhirAsserts');
+
+describe('Medication Tests', () => {
+    beforeEach(async () => {
+        await commonBeforeEach();
+    });
+
+    afterEach(async () => {
+        await commonAfterEach();
+    });
+
+    describe('Medication search_by_code_text Tests', () => {
+        test('search_by_code_text works', async () => {
+            // ARRANGE
+            // add the resources to FHIR server
+            let resp = await request
+                .post('/4_0_0/Medication/1/$merge?validate=true')
+                .send(medication1Resource)
+                .set(getHeaders())
+                .expect(200);
+            assertMergeIsSuccessful(resp.body);
+
+            // ACT & ASSERT
+            // search by token system and code and make sure we get the right Medication back
+            resp = await request
+                .get('/4_0_0/Medication/?_bundle=1')
+                .set(getHeaders())
+                .expect(200);
+            assertCompareBundles(resp.body, expectedMedicationResources);
+        });
+    });
+});
