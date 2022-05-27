97f47ab3b573a57dc4dc7408d326557a057b6f0f
imranq2 <imranq2@hotmail.com>
2022-05-26 17:56:55 -0700

    set default limit when streaming if no _count is passed

--- src/operations/search/setDefaultLimit.js
+++ src/operations/search/setDefaultLimit.js
@@ -7,9 +7,6 @@ const {limit, searchLimitForIds} = require('../../utils/searchForm.util');
  * @param {boolean} isStreaming
  */
 function setDefaultLimit(args, options, isStreaming) {
-    if (isStreaming) {
-        return; //don't set any limits when streaming since we send data as we get from mongo so no mem pressure
-    }
     // set a limit so the server does not come down due to volume of data
     if (!args['id'] && !args['_elements']) {
         options['limit'] = limit;
