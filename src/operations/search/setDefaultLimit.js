898ecec471c0359a47fc6dc74111ca78d5e787b9
imranq2 <imranq2@hotmail.com>
2022-05-26 17:58:02 -0700

    set default limit when streaming if no _count is passed

--- src/operations/search/setDefaultLimit.js
+++ src/operations/search/setDefaultLimit.js
@@ -6,7 +6,10 @@ const {limit, searchLimitForIds} = require('../../utils/searchForm.util');
  * @param {Object} options
  * @param {boolean} isStreaming
  */
-function setDefaultLimit(args, options, isStreaming) {
+function setDefaultLimit(args, options,
+                         // eslint-disable-next-line no-unused-vars
+                         isStreaming
+) {
     // set a limit so the server does not come down due to volume of data
     if (!args['id'] && !args['_elements']) {
         options['limit'] = limit;
