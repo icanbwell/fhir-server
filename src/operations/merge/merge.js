96edb2b7bb4172b723e53eaa321110cf38b42d69
imranq2 <imranq2@hotmail.com>
2022-05-30 11:42:31 -0700

    separate out mergeResource.js

--- src/operations/merge/merge.js
+++ src/operations/merge/merge.js
@@ -4,21 +4,11 @@ const {
     verifyHasValidScopes
 } = require('../security/scopes');
 const moment = require('moment-timezone');
-const {isTrue} = require('../../utils/isTrue');
-const env = require('var');
 const {validateResource} = require('../../utils/validator.util');
-const sendToS3 = require('../../utils/aws-s3');
-const globals = require('../../globals');
-const {CLIENT_DB, AUDIT_EVENT_CLIENT_DB, ATLAS_CLIENT_DB} = require('../../constants');
 const async = require('async');
-// const {check_fhir_mismatch} = require('../common/check_fhir_mismatch');
-const {logError} = require('../common/logging');
-const {getOrCreateCollection} = require('../../utils/mongoCollectionManager');
 const {logAuditEntryAsync} = require('../../utils/auditLogger');
 const {findDuplicateResources, findUniqueResources} = require('../../utils/list.util');
-const {preMergeChecks} = require('./preMergeChecks');
-const {mergeExisting} = require('./mergeExisting');
-const {mergeInsert} = require('./mergeInsert');
+const {merge_resource} = require('./mergeResource');
 
 // noinspection JSValidateTypes
 /**
@@ -87,125 +77,6 @@ module.exports.merge = async (requestInfo, args, resource_name, collection_name)
     logDebug(user, JSON.stringify(resources_incoming));
     logDebug(user, '-----------------');
 
-    // this function is called for each resource
-    // returns an OperationOutcome
-    /**
-     * Merges a single resource
-     * @param {Object} resource_to_merge
-     * @return {Promise<{operationOutcome: ?OperationOutcome, issue: {severity: string, diagnostics: string, code: string, expression: [string], details: {text: string}}, created: boolean, id: String, updated: boolean}>}
-     */
-    async function merge_resource(resource_to_merge) {
-        /**
-         * @type {string}
-         */
-        let id = resource_to_merge.id;
-
-        if (resource_to_merge.meta && resource_to_merge.meta.lastUpdated && typeof resource_to_merge.meta.lastUpdated !== 'string') {
-            resource_to_merge.meta.lastUpdated = new Date(resource_to_merge.meta.lastUpdated).toISOString();
-        }
-
-        if (env.LOG_ALL_SAVES) {
-            await sendToS3('logs',
-                resource_to_merge.resourceType,
-                resource_to_merge,
-                currentDate,
-                id,
-                'merge_' + requestId);
-        }
-
-        const preMergeCheckFailures = await preMergeChecks(resource_to_merge, resource_name, scopes, user, path, currentDate);
-        if (preMergeCheckFailures) {
-            return preMergeCheckFailures;
-        }
-
-        try {
-            logDebug(user, '-----------------');
-            logDebug(user, base_version);
-            logDebug(user, '--- body ----');
-            logDebug(user, JSON.stringify(resource_to_merge));
-
-            /**
-             * @type {boolean}
-             */
-            const useAtlas = (isTrue(env.USE_ATLAS));
-
-            // Grab an instance of our DB and collection
-            // noinspection JSValidateTypes
-            /**
-             * mongo db connection
-             * @type {import('mongodb').Db}
-             */
-            let db = (resource_to_merge.resourceType === 'AuditEvent') ?
-                globals.get(AUDIT_EVENT_CLIENT_DB) : (useAtlas && globals.has(ATLAS_CLIENT_DB)) ?
-                    globals.get(ATLAS_CLIENT_DB) : globals.get(CLIENT_DB);
-            /**
-             * @type {import('mongodb').Collection}
-             */
-            let collection = await getOrCreateCollection(db, `${resource_to_merge.resourceType}_${base_version}`);
-
-            // Query our collection for this id
-            /**
-             * @type {Object}
-             */
-            let data = await collection.findOne({id: id.toString()});
-
-            logDebug('test?', '------- data -------');
-            logDebug('test?', `${resource_to_merge.resourceType}_${base_version}`);
-            logDebug('test?', JSON.stringify(data));
-            logDebug('test?', '------- end data -------');
-
-            let res;
-
-            // check if resource was found in database or not
-            // noinspection JSUnusedLocalSymbols
-            if (data && data.meta) {
-                res = await mergeExisting(
-                    resource_to_merge, data, base_version, user, scope, collection_name, currentDate, requestId);
-            } else {
-                res = await mergeInsert(resource_to_merge, base_version, collection_name, user);
-            }
-
-            return res;
-        } catch (e) {
-            logError(`Error with merging resource ${resource_to_merge.resourceType}.merge with id: ${id} `, e);
-            const operationOutcome = {
-                resourceType: 'OperationOutcome',
-                issue: [
-                    {
-                        severity: 'error',
-                        code: 'exception',
-                        details: {
-                            text: 'Error merging: ' + JSON.stringify(resource_to_merge)
-                        },
-                        diagnostics: e.toString(),
-                        expression: [
-                            resource_to_merge.resourceType + '/' + id
-                        ]
-                    }
-                ]
-            };
-            await sendToS3('errors',
-                resource_to_merge.resourceType,
-                resource_to_merge,
-                currentDate,
-                id,
-                'merge');
-            await sendToS3('errors',
-                resource_to_merge.resourceType,
-                operationOutcome,
-                currentDate,
-                id,
-                'merge_error');
-            return {
-                id: id,
-                created: false,
-                updated: false,
-                issue: (operationOutcome.issue && operationOutcome.issue.length > 0) ? operationOutcome.issue[0] : null,
-                operationOutcome: operationOutcome
-            };
-        }
-    }
-
     /**
      * Tries to merge and retries if there is an error to protect against race conditions where 2 calls are happening
      *  in parallel for the same resource. Both of them see that the resource does not exist, one of them inserts it
@@ -214,7 +85,9 @@ module.exports.merge = async (requestInfo, args, resource_name, collection_name)
      * @return {Promise<{operationOutcome: ?OperationOutcome, issue: {severity: string, diagnostics: string, code: string, expression: [string], details: {text: string}}, created: boolean, id: String, updated: boolean}>}
      */
     async function merge_resource_with_retry(resource_to_merge) {
-        return await merge_resource(resource_to_merge);
+        return await merge_resource(resource_to_merge, resource_name,
+            scopes, user, path, currentDate,
+            requestId, base_version, scope, collection_name);
     }
 
     // if the incoming request is a bundle then unwrap the bundle
---
+++ src/operations/merge/mergeResource.js
@@ -0,0 +1,144 @@
+// this function is called for each resource
+// returns an OperationOutcome
+const env = require('var');
+const sendToS3 = require('../../utils/aws-s3');
+const {preMergeChecks} = require('./preMergeChecks');
+const {logDebug, logError} = require('../common/logging');
+const {isTrue} = require('../../utils/isTrue');
+const globals = require('../../globals');
+const {AUDIT_EVENT_CLIENT_DB, ATLAS_CLIENT_DB, CLIENT_DB} = require('../../constants');
+const {getOrCreateCollection} = require('../../utils/mongoCollectionManager');
+const {mergeExisting} = require('./mergeExisting');
+const {mergeInsert} = require('./mergeInsert');
+
+/**
+ * Merges a single resource
+ * @param {Object} resource_to_merge
+ * @return {Promise<{operationOutcome: ?OperationOutcome, issue: {severity: string, diagnostics: string, code: string, expression: [string], details: {text: string}}, created: boolean, id: String, updated: boolean}>}
+ * @param {string} resourceName
+ * @param {string[] | null} scopes
+ * @param {string|null} user
+ * @param {string} path
+ * @param {string} currentDate
+ * @param {string} requestId
+ * @param {string} baseVersion
+ * @param scope
+ * @param {string} collectionName
+ */
+async function merge_resource(resource_to_merge, resourceName,
+                              scopes, user, path, currentDate,
+                              requestId, baseVersion, scope, collectionName) {
+    /**
+     * @type {string}
+     */
+    let id = resource_to_merge.id;
+
+    if (resource_to_merge.meta && resource_to_merge.meta.lastUpdated && typeof resource_to_merge.meta.lastUpdated !== 'string') {
+        resource_to_merge.meta.lastUpdated = new Date(resource_to_merge.meta.lastUpdated).toISOString();
+    }
+
+    if (env.LOG_ALL_SAVES) {
+        await sendToS3('logs',
+            resource_to_merge.resourceType,
+            resource_to_merge,
+            currentDate,
+            id,
+            'merge_' + requestId);
+    }
+
+    const preMergeCheckFailures = await preMergeChecks(resource_to_merge, resourceName, scopes, user, path, currentDate);
+    if (preMergeCheckFailures) {
+        return preMergeCheckFailures;
+    }
+
+    try {
+        logDebug(user, '-----------------');
+        logDebug(user, baseVersion);
+        logDebug(user, '--- body ----');
+        logDebug(user, JSON.stringify(resource_to_merge));
+
+        /**
+         * @type {boolean}
+         */
+        const useAtlas = (isTrue(env.USE_ATLAS));
+
+        // Grab an instance of our DB and collection
+        // noinspection JSValidateTypes
+        /**
+         * mongo db connection
+         * @type {import('mongodb').Db}
+         */
+        let db = (resource_to_merge.resourceType === 'AuditEvent') ?
+            globals.get(AUDIT_EVENT_CLIENT_DB) : (useAtlas && globals.has(ATLAS_CLIENT_DB)) ?
+                globals.get(ATLAS_CLIENT_DB) : globals.get(CLIENT_DB);
+        /**
+         * @type {import('mongodb').Collection}
+         */
+        let collection = await getOrCreateCollection(db, `${resource_to_merge.resourceType}_${baseVersion}`);
+
+        // Query our collection for this id
+        /**
+         * @type {Object}
+         */
+        let data = await collection.findOne({id: id.toString()});
+
+        logDebug('test?', '------- data -------');
+        logDebug('test?', `${resource_to_merge.resourceType}_${baseVersion}`);
+        logDebug('test?', JSON.stringify(data));
+        logDebug('test?', '------- end data -------');
+
+        let res;
+
+        // check if resource was found in database or not
+        // noinspection JSUnusedLocalSymbols
+        if (data && data.meta) {
+            res = await mergeExisting(
+                resource_to_merge, data, baseVersion, user, scope, collectionName, currentDate, requestId);
+        } else {
+            res = await mergeInsert(resource_to_merge, baseVersion, collectionName, user);
+        }
+
+        return res;
+    } catch (e) {
+        logError(`Error with merging resource ${resource_to_merge.resourceType}.merge with id: ${id} `, e);
+        const operationOutcome = {
+            resourceType: 'OperationOutcome',
+            issue: [
+                {
+                    severity: 'error',
+                    code: 'exception',
+                    details: {
+                        text: 'Error merging: ' + JSON.stringify(resource_to_merge)
+                    },
+                    diagnostics: e.toString(),
+                    expression: [
+                        resource_to_merge.resourceType + '/' + id
+                    ]
+                }
+            ]
+        };
+        await sendToS3('errors',
+            resource_to_merge.resourceType,
+            resource_to_merge,
+            currentDate,
+            id,
+            'merge');
+        await sendToS3('errors',
+            resource_to_merge.resourceType,
+            operationOutcome,
+            currentDate,
+            id,
+            'merge_error');
+        return {
+            id: id,
+            created: false,
+            updated: false,
+            issue: (operationOutcome.issue && operationOutcome.issue.length > 0) ? operationOutcome.issue[0] : null,
+            operationOutcome: operationOutcome
+        };
+    }
+}
+
+module.exports = {
+    merge_resource
+};
