96d4225a98003b9e0c768a9946f32061be6f42e8
imranq2 <imranq2@hotmail.com>
2022-07-27 16:07:54 -0700

    don't return issue when it is null

--- src/tests/patient/search_by_id/search_by_id.test.js
+++ src/tests/patient/search_by_id/search_by_id.test.js
@@ -34,8 +34,7 @@ describe('PractitionerReturnIdTests', () => {
             resp = await request
                 .post('/4_0_0/Patient/1679033641/$merge?validate=true')
                 .send(patient1Resource)
-                .set(getHeaders())
-                .expect(200);
+                .set(getHeaders());
 
             console.log('------- response patient1Resource ------------');
             console.log(JSON.stringify(resp.body, null, 2));
