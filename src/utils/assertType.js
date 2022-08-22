e95ee860fcd9851b9e4ccac9b397a595119689c0
imranq2 <imranq2@hotmail.com>
2022-08-22 13:59:49 -0700

    update asserts

--- src/utils/assertType.js
+++ src/utils/assertType.js
@@ -1,21 +1,56 @@
 /**
+ * Class for assertion errors
+ */
+class AssertionError extends Error {
+}
+
+AssertionError.prototype.name = 'AssertionError';
+
+/**
  * asserts if the object is null/undefined or not an instance of the passed in type
  * @param {Object|null|undefined} obj
  * @param type
  * @param {string|undefined} [message]
+ * @throws {AssertionError}
  */
 function assertTypeEquals(obj, type, message) {
     if (!obj) {
-        throw new Error(message ? message : `obj of type ${type.name} is null or undefined`);
+        const assertionError = new AssertionError(
+            message ?
+                message :
+                `obj of type ${type.name} is null or undefined`
+        );
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(assertionError, assertTypeEquals);
+        }
+        throw assertionError;
     }
     if (!(obj instanceof type)) {
-        throw new Error(message ? message : `Type of obj ${typeof obj} is not the expected type ${type.name}`);
+        const assertionError = new AssertionError(
+            message ?
+                message :
+                `Type of obj ${typeof obj} is not the expected type ${type.name}`
+        );
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(assertionError, assertTypeEquals);
+        }
+        throw assertionError;
     }
 }
 
+/**
+ * assert is valid
+ * @param {Object|boolean} obj
+ * @param {string|undefined} [message]
+ * @throws {AssertionError}
+ */
 function assertIsValid(obj, message) {
     if (!obj) {
-        throw new Error(message ? message : 'obj is null or undefined');
+        const assertionError = new AssertionError(message ? message : 'obj is null or undefined');
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(assertionError, assertTypeEquals);
+        }
+        throw assertionError;
     }
 }
 
