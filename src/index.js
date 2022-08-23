efe3716d30850f4fd606acb4c52deb633fdd1c1f
imranq2 <imranq2@hotmail.com>
2022-08-23 15:54:17 -0700

    use dummyKafkaClient.js if env.ENABLE_EVENTS_KAFKA is not set

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@
  */
 const {createServer} = require('./server');
 const {createContainer} = require('./createContainer');
+const Sentry = require('@sentry/node');
+const {ErrorReporter} = require('./utils/slack.logger');
 
 const main = async function () {
-    await createServer(() => createContainer());
+    try {
+        await createServer(() => createContainer());
+    } catch (e) {
+        console.log(JSON.stringify({method: 'main', message: JSON.stringify(e)}));
+        Sentry.captureException(e);
+        await new ErrorReporter().reportErrorAsync('uncaughtException', e);
+    }
 };
 
 main().catch(reason => {
