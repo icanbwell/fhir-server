20ae363fc79902b5b4af8db656fc6ddc47b96895
imranq2 <imranq2@hotmail.com>
2022-08-28 19:20:55 -0700

    add support for returning merge as OperationOutcomes

--- src/tests/claims/merge_complex_bundle/claims.merge.complex.bundle.test.js
+++ src/tests/claims/merge_complex_bundle/claims.merge.complex.bundle.test.js
@@ -2,7 +2,8 @@ const explanationOfBenefitBundleResource = require('./fixtures/explanation_of_be
 const expectedExplanationOfBenefitBundleResource = require('./fixtures/expected_explanation_of_benefits.json');
 
 const {commonBeforeEach, commonAfterEach, getHeaders, createTestRequest} = require('../../common');
-const {describe, beforeEach, afterEach, expect} = require('@jest/globals');
+const {describe, beforeEach, afterEach} = require('@jest/globals');
+const {assertResourceCount, assertMerge, assertResponse} = require('../../fhirAsserts');
 
 describe('Claim Merge Tests', () => {
     beforeEach(async () => {
@@ -16,48 +17,22 @@ describe('Claim Merge Tests', () => {
     describe('Claim Merge Bundles', () => {
         test('Complex Claims with merge properly', async () => {
             const request = await createTestRequest();
-            let resp = await request
+            await request
                 .get('/4_0_0/ExplanationOfBenefit')
                 .set(getHeaders())
-                .expect(200);
-            expect(resp.body.length).toBe(0);
-            console.log('------- response 1 ------------');
-            console.log(JSON.stringify(resp.body, null, 2));
-            console.log('------- end response 1 ------------');
+                .expect(assertResourceCount(0));
 
-            resp = await request
+            await request
                 .post('/4_0_0/ExplanationOfBenefit/1/$merge')
                 .send(explanationOfBenefitBundleResource)
                 .set(getHeaders())
-                .expect(200);
-            console.log('------- response 2 ------------');
-            console.log(JSON.stringify(resp.body, null, 2));
-            console.log('------- end response 2  ------------');
+                .expect(assertMerge([{created: true}, {updated: true}]));
 
-            resp = await request
+            await request
                 .get('/4_0_0/ExplanationOfBenefit')
                 .set(getHeaders())
-                .expect(200);
-            // clear out the lastUpdated column since that changes
-            let body = resp.body;
-            console.log('------- response 5 ------------');
-            console.log(JSON.stringify(resp.body, null, 2));
-            console.log('------- end response 5  ------------');
-            expect(body.length).toBe(1);
-            body.forEach(element => {
-                delete element['meta']['lastUpdated'];
-            });
-            let expected = expectedExplanationOfBenefitBundleResource;
-            expected.forEach(element => {
-                if ('meta' in element) {
-                    delete element['meta']['lastUpdated'];
-                }
-                // element['meta'] = {'versionId': '1'};
-                if ('$schema' in element) {
-                    delete element['$schema'];
-                }
-            });
-            expect(body).toStrictEqual(expected);
+                .expect(assertResourceCount(1))
+                .expect(assertResponse(expectedExplanationOfBenefitBundleResource));
         });
     });
 });
