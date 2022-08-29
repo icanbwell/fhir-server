05892a62d2cbc5b499a98755b0e12ea8b45a5395
imranq2 <imranq2@hotmail.com>
2022-08-28 17:28:19 -0700

    add test for merge by parameters

--- src/tests/practitioner/graph/practitioner.graph.test.js
+++ src/tests/practitioner/graph/practitioner.graph.test.js
@@ -91,5 +91,72 @@ describe('Practitioner Graph Contained Tests', () => {
             expect(duplicates.map(a => `${a.resourceType}/${a.id}`)).toStrictEqual([]);
             assertCompareBundles(body, expected);
         });
+        test('Graph contained works properly with parameters', async () => {
+            const request = await createTestRequest();
+            await request
+                .get('/4_0_0/Practitioner')
+                .set(getHeaders())
+                .expect(assertStatusOk())
+                .expect(assertResourceCount(0));
+
+            await request
+                .post('/4_0_0/Practitioner/1679033641/$merge')
+                .send(practitionerResource)
+                .set(getHeaders())
+                .expect(assertMerge({created: true}));
+
+            await request
+                .post('/4_0_0/PractitionerRole/1/$merge')
+                .send(practitionerRoleResource)
+                .set(getHeaders())
+                .expect(assertMerge({created: true}));
+
+            await request
+                .post('/4_0_0/PractitionerRole/1/$merge')
+                .send(practitionerRoleDifferentSecurityTagResource)
+                .set(getHeaders())
+                .expect(assertMerge({created: true}));
+
+            await request
+                .post('/4_0_0/Organization/123456/$merge')
+                .send(organizationResource)
+                .set(getHeaders())
+                .expect(assertMerge({created: true}));
+
+            /**
+             * http://www.hl7.org/fhir/parameters-example.json.html
+             * @type {{parameter: [{resource: {resourceType: string, id: string, meta: {security: {}}, identifier: {}, active: boolean, name: {}, telecom: {}, address: {}, gender: string}, name: string}], resourceType: string}}
+             */
+            const parametersResource = {
+                resourceType: 'Parameters',
+                parameter: [{name: 'resource', resource: graphDefinitionResource}]
+            };
+
+            await request
+                .post('/4_0_0/Practitioner/$graph?id=1679033641&contained=true')
+                .send(parametersResource)
+                .set(getHeaders())
+                .expect(assertStatusOk())
+                .expect(assertResponse(expectedResource));
+
+            const resp = await request
+                .post('/4_0_0/Practitioner/$graph?id=1679033641&contained=true&_hash_references=true')
+                .send(parametersResource)
+                .set(getHeaders())
+                .expect(assertStatusOk())
+                .expect(assertResponse(expectedHashReferencesResource));
+
+            const body = resp.body;
+            const expected = expectedHashReferencesResource;
+            console.log('----- Received resources ----');
+            console.log(`${body.entry.map(e => e.resource).map(a => `${a.resourceType}/${a.id}`)}`);
+            console.log('----- End of Received resources ----');
+            // verify there are no duplicate ids
+            const duplicates = findDuplicateResources(
+                body.entry.map(e => e.resource)
+            );
+            expect(duplicates.map(a => `${a.resourceType}/${a.id}`)).toStrictEqual([]);
+            assertCompareBundles(body, expected);
+        });
     });
 });
