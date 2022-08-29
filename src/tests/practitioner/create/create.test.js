cbb9c5e9f63ae46848f0c82c9e720d18415f92d2
imranq2 <imranq2@hotmail.com>
2022-08-29 16:41:12 -0700

    fix test

--- src/tests/practitioner/create/create.test.js
+++ src/tests/practitioner/create/create.test.js
@@ -29,17 +29,22 @@ describe('Practitioner Tests', () => {
                 .expect(assertStatusCode(201));
 
             practitioner1Resource['active'] = false;
+            // const id1 = resp.headers['content-location'].split('/').splice(5, 1)[0];
 
+            // pause enough so the lastUpdated time is later on the second resource so our sorting works properly
+            await new Promise(resolve => setTimeout(resolve, 3000));
             await request
                 .post('/4_0_0/Practitioner')
                 .send(practitioner1Resource)
                 .set(getHeaders())
                 .expect(assertStatusCode(201));
 
+            // const id2 = resp.headers['content-location'].split('/').splice(5, 1)[0];
+
             // ACT & ASSERT
             // search by token system and code and make sure we get the right Practitioner back
             await request
-                .get('/4_0_0/Practitioner/?_bundle=1&sort=')
+                .get('/4_0_0/Practitioner?_bundle=1&_sort=lastUpdated')
                 .set(getHeaders())
                 .expect(assertResponse({
                             expected: expectedPractitionerResources,
