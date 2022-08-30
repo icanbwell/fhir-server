f3842fa2cf92c3242eb921c3133bdf5dcf960bee
Imran Qureshi <imranq2@hotmail.com>
2022-08-29 19:25:20 -0700

    Clean resources received in merge (#620)
    * clean resources received in merge
    
    * log in databaseBulkInserter.js
    
    * fix UI
    
    * fix UI
    
    * show id in UI
    
    * show id in UI
    
    * fix test
    
    * fix test
    
    * fix test
    
    * fix test
    
    * fix test
    
    * fix test
    
    * make test run deterministic

--- src/tests/practitioner/create/create.test.js
+++ src/tests/practitioner/create/create.test.js
@@ -22,36 +22,34 @@ describe('Practitioner Tests', () => {
             const request = await createTestRequest();
             // ARRANGE
             // add the resources to FHIR server
-            await request
+            let resp = await request
                 .post('/4_0_0/Practitioner/')
                 .send(practitioner1Resource)
                 .set(getHeaders())
                 .expect(assertStatusCode(201));
 
             practitioner1Resource['active'] = false;
-            // const id1 = resp.headers['content-location'].split('/').splice(5, 1)[0];
+            const id1 = resp.headers['content-location'].split('/').splice(5, 1)[0];
+            expectedPractitionerResources.entry[0].resource.id = id1;
 
             // pause enough so the lastUpdated time is later on the second resource so our sorting works properly
             await new Promise(resolve => setTimeout(resolve, 3000));
-            await request
+            resp = await request
                 .post('/4_0_0/Practitioner')
                 .send(practitioner1Resource)
                 .set(getHeaders())
                 .expect(assertStatusCode(201));
 
-            // const id2 = resp.headers['content-location'].split('/').splice(5, 1)[0];
+            const id2 = resp.headers['content-location'].split('/').splice(5, 1)[0];
+            expectedPractitionerResources.entry[1].resource.id = id2;
 
             // ACT & ASSERT
             // search by token system and code and make sure we get the right Practitioner back
             await request
-                .get('/4_0_0/Practitioner?_bundle=1&_sort=lastUpdated')
+                .get('/4_0_0/Practitioner?_bundle=1&_sort=meta.lastUpdated')
                 .set(getHeaders())
                 .expect(assertResponse({
-                            expected: expectedPractitionerResources,
-                            fnCleanResource: (r) => {
-                                delete r['id'];
-                                return r;
-                            }
+                            expected: expectedPractitionerResources
                         }
                     )
                 );
