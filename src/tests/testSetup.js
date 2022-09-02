d2fd71d9a273903fe06715be92fd6e64b546f1fa
imranq2 <imranq2@hotmail.com>
2022-09-01 20:29:00 -0700

    create custom matchers

--- src/tests/testSetup.js
+++ src/tests/testSetup.js
@@ -1,7 +1,13 @@
 // ./testSetup.js
-const {toHaveResponse, toHaveStatusCode, toHaveStatusOk} = require('./customMatchers');
+const {
+    toHaveResponse,
+    toHaveStatusCode,
+    toHaveStatusOk,
+    toHaveMergeResponse,
+    toHaveResourceCount
+} = require('./customMatchers');
 
-expect.extend({toHaveResponse, toHaveStatusCode, toHaveStatusOk});
+expect.extend({toHaveResponse, toHaveStatusCode, toHaveStatusOk, toHaveMergeResponse, toHaveResourceCount});
 
 const {toBeArray, toBeSealed} = require('jest-extended');
 expect.extend({toBeArray, toBeSealed});
