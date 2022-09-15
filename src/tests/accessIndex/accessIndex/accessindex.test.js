c8c3f1ab162fc2ad0ac6e422bcd4738d7d0a9b75
imranq2 <imranq2@hotmail.com>
2022-09-15 11:58:08 -0700

    add _access to generated resources

--- src/tests/accessIndex/accessIndex/accessindex.test.js
+++ src/tests/accessIndex/accessIndex/accessindex.test.js
@@ -3,6 +3,7 @@ const auditevent1Resource = require('./fixtures/AuditEvent/auditevent1.json');
 
 // expected
 const expectedAuditEventResources = require('./fixtures/expected/expected_AuditEvent.json');
+const expectedAuditEventResourcesAccessIndex = require('./fixtures/expected/expected_AuditEvent_access_index.json');
 
 const {commonBeforeEach, commonAfterEach, getHeaders, createTestRequest, getTestContainer} = require('../../common');
 const {describe, beforeEach, afterEach, test} = require('@jest/globals');
@@ -97,10 +98,10 @@ describe('AuditEvent Tests', () => {
 
             // search by token system and code and make sure we get the right AuditEvent back
             resp = await request
-                .get('/4_0_0/AuditEvent/?_bundle=1&_count=2&_getpagesoffset=0&_security=https://www.icanbwell.com/access%7Cmedstar&date=lt2021-09-22T00:00:00Z&date=ge2021-09-19T00:00:00Z&_useAccessIndex=1')
+                .get('/4_0_0/AuditEvent/?_bundle=1&_debug=1&_count=2&_getpagesoffset=0&_security=https://www.icanbwell.com/access%7Cmedstar&date=lt2021-09-22T00:00:00Z&date=ge2021-09-19T00:00:00Z&_useAccessIndex=1')
                 .set(getHeaders());
             // noinspection JSUnresolvedFunction
-            expect(resp).toHaveResponse(expectedAuditEventResources);
+            expect(resp).toHaveResponse(expectedAuditEventResourcesAccessIndex);
         });
     });
 });
