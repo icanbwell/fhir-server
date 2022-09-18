aea83e8b75ddeae5c4e4d397bd2db9203bf2b3b3
imranq2 <imranq2@hotmail.com>
2022-09-18 15:26:06 -0700

    move partitioners to partitioners folder

--- src/admin/scripts/baseBulkOperationRunner.js
+++ src/admin/scripts/baseBulkOperationRunner.js
@@ -76,7 +76,8 @@ class BaseBulkOperationRunner extends BaseScriptRunner {
         const cursor = await sourceCollection
             .find(query, {})
             .sort({id: 1})
-            .maxTimeMS(60 * 60 * 1000);
+            .maxTimeMS(60 * 60 * 1000)
+            .batchSize(1000);
 
         let count = 0;
         /**
