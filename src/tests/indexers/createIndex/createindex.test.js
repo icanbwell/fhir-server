65252435154de789f3e5106d03b99db80d33d0e3
imranq2 <imranq2@hotmail.com>
2022-09-17 17:16:40 -0700

    enable indexing to support all mongo options

--- src/tests/indexers/createIndex/createindex.test.js
+++ src/tests/indexers/createIndex/createindex.test.js
@@ -342,5 +342,130 @@ describe('Create Index Tests', () => {
                 }
             );
         });
+        test('createIndex works for AuditEvent twice', async () => {
+            await createTestRequest();
+            /**
+             * @type {SimpleContainer}
+             */
+            const container = getTestContainer();
+            /**
+             * @type {IndexManager}
+             */
+            const indexManager = container.indexManager;
+
+            // create collection
+            /**
+             * mongo auditEventDb connection
+             * @type {import('mongodb').Db}
+             */
+            const auditEventDb = globals.get(AUDIT_EVENT_CLIENT_DB);
+            const collectionName = 'AuditEvent_4_0_0';
+            /**
+             * mongo collection
+             * @type {import('mongodb').Collection}
+             */
+            const auditEventCollection = auditEventDb.collection(collectionName);
+            auditEventCollection.insertOne({id: '1', resourceType: 'AuditEvent'});
+            // run indexManager
+            await indexManager.indexCollectionAsync({
+                collectionName,
+                db: auditEventDb
+            });
+            // run indexManager
+            await indexManager.indexCollectionAsync({
+                collectionName,
+                db: auditEventDb
+            });
+            // check that indexes were created properly
+            /**
+             * @type {Object[]}
+             */
+            const indexes = await auditEventCollection.indexes();
+            expect(indexes.length).toBe(8);
+            expect(indexes[0]).toStrictEqual(
+                {
+                    'v': 2,
+                    'key': {
+                        '_id': 1
+                    },
+                    'name': '_id_'
+                }
+            );
+            expect(indexes[1]).toStrictEqual(
+                {
+                    'v': 2,
+                    'key': {
+                        'id': 1
+                    },
+                    'name': 'id_1',
+                    'unique': true
+                }
+            );
+            expect(indexes[2]).toStrictEqual(
+                {
+                    'v': 2,
+                    'key': {
+                        'meta.source': 1
+                    },
+                    'name': 'meta.source_1'
+                }
+            );
+            expect(indexes[3]).toStrictEqual(
+                {
+                    'v': 2,
+                    'key': {
+                        'meta.security.system': 1,
+                        'meta.security.code': 1
+                    },
+                    'name': 'security.system_code_1'
+                }
+            );
+            expect(indexes[4]).toStrictEqual(
+                {
+                    'v': 2,
+                    'key': {
+                        'meta.security.system': 1,
+                        'meta.security.code': 1,
+                        'id': 1,
+                        'meta.lastUpdated': 1
+                    },
+                    'name': 'helix_audit_event_security'
+                }
+            );
+            expect(indexes[5]).toStrictEqual(
+                {
+                    'v': 2,
+                    'key': {
+                        '_access.medstar': 1,
+                        'id': 1,
+                        'meta.lastUpdated': 1
+                    },
+                    'name': 'helix_auditEvent_security_access_medstar'
+                }
+            );
+            expect(indexes[6]).toStrictEqual(
+                {
+                    'v': 2,
+                    'key': {
+                        '_access.medstar': 1,
+                        'id': 1,
+                        'recorded': 1
+                    },
+                    'name': 'helix_auditEvent_recorded_access_medstar'
+                }
+            );
+            expect(indexes[7]).toStrictEqual(
+                {
+                    'v': 2,
+                    'key': {
+                        'meta.security.system': 1,
+                        'meta.security.code': 1,
+                        'id': 1,
+                        'recorded': 1
+                    },
+                    'name': 'helix_auditEvent_recorded'
+                }
+            );
+        });
     });
 });
