711dff0ebf462658f2b37b73caa8ff6c15023d06
imranq2 <imranq2@hotmail.com>
2022-10-01 12:29:06 -0700

    separate runners into their own folder

--- src/admin/scripts/partitionAuditEvent.js
+++ src/admin/scripts/partitionAuditEvent.js
@@ -32,6 +32,7 @@ async function main() {
     // set up all the standard services in the container
     const container = createContainer();
 
+    console.log(`Parameters: ${parameters}`);
     // now add our class
     container.register('processAuditEventRunner', (c) => new PartitionAuditEventRunner(
             {
