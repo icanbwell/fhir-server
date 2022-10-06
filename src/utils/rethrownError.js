810ad74bb9d633698298c824b6148dd6e225c4a5
imranq2 <imranq2@hotmail.com>
2022-10-05 20:50:38 -0700

    add try catch to graph

--- src/utils/rethrownError.js
+++ src/utils/rethrownError.js
@@ -17,11 +17,63 @@ class RethrownError extends Error {
         }
         this.original_error = error;
         this.stack_before_rethrow = this.stack;
-        const message_lines = (this.message.match(/\n/g) || []).length + 1;
-        this.stack = this.stack.split('\n').slice(0, message_lines + 1).join('\n') + '\n' +
-            error.stack;
+        // const message_lines = (this.message.match(/\n/g) || []).length + 1;
+        // this.stack = this.stack.split('\n').slice(0, message_lines + 1).join('\n') + '\n' +
+        //     error.stack;
         this.args = args;
         this.source = source;
+
+        this.nested = error;
+
+        if (message instanceof Error) {
+            error = message;
+        } else if (typeof message !== 'undefined') {
+            Object.defineProperty(this, 'message', {
+                value: message,
+                writable: true,
+                enumerable: false,
+                configurable: true
+            });
+        }
+        Error.captureStackTrace(this, this.constructor);
+        var oldStackDescriptor = Object.getOwnPropertyDescriptor(this, 'stack');
+        var stackDescriptor = this.buildStackDescriptor(oldStackDescriptor, error);
+        Object.defineProperty(this, 'stack', stackDescriptor);
+    }
+
+    /**
+     * builds stacks
+     * @param oldStackDescriptor
+     * @param {Error} nested
+     * @return {{value: string}|{get: (function(): string)}}
+     */
+    buildStackDescriptor(oldStackDescriptor, nested) {
+        if (oldStackDescriptor.get) {
+            return {
+                get: function () {
+                    var stack = oldStackDescriptor.get.call(this);
+                    return this.buildCombinedStacks(stack, this.nested);
+                }
+            };
+        } else {
+            var stack = oldStackDescriptor.value;
+            return {
+                value: this.buildCombinedStacks(stack, nested)
+            };
+        }
+    }
+
+    /**
+     * builds combined stacks
+     * @param {string} stack
+     * @param {Error} nested
+     * @return {string}
+     */
+    buildCombinedStacks(stack, nested) {
+        if (nested) {
+            stack += '\r\nCaused By: ' + nested.stack;
+        }
+        return stack;
     }
 }
 
