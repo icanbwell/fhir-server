b74974ba9dabff5b851ebf0573a289ec98181a58
imranq2 <imranq2@hotmail.com>
2022-10-31 18:13:58 -0700

    return original status code from RethrownError

--- src/tests/merge/mergeWith_id/mergewith_id.test.js
+++ src/tests/merge/mergeWith_id/mergewith_id.test.js
@@ -3,6 +3,8 @@ const person1Resource = require('./fixtures/Person/person1.json');
 
 // expected
 const expectedPersonResources = require('./fixtures/expected/expected_Person.json');
+const expectedMissingAccessScope = require('./fixtures/expected/expectedMissingAccessScope.json');
+const expectedWrongAccessScope = require('./fixtures/expected/expectedWrongAccessScope.json');
 
 const {
     commonBeforeEach,
@@ -22,7 +24,7 @@ describe('Person Tests', () => {
     });
 
     describe('Person mergeWith_id Tests', () => {
-        test('mergeWith_id works', async () => {
+        test('mergeWith_id works with access/*.*', async () => {
             const request = await createTestRequest();
             // ARRANGE
             // add the resources to FHIR server
@@ -44,13 +46,43 @@ describe('Person Tests', () => {
         test('mergeWith_id fails with missing permissions (create)', async () => {
             const request = await createTestRequest();
             // ARRANGE
-            // add the resources to FHIR server
+
+            // ACT & ASSERT
             let resp = await request
                 .post('/4_0_0/Person/1/$merge')
                 .send(person1Resource)
                 .set(getHeaders('user/*.read user/*.write'));
             // noinspection JSUnresolvedFunction
+            expect(resp).toHaveStatusCode(403);
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResponse(expectedMissingAccessScope, r => {
+                delete r['diagnostics'];
+            });
+        });
+        test('mergeWith_id fails with missing permissions (update)', async () => {
+            const request = await createTestRequest();
+            // ARRANGE
+            let resp = await request
+                .get('/4_0_0/Person/?_bundle=1')
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResourceCount(0);
+
+            // ACT & ASSERT
+            // add the resources to FHIR server
+            resp = await request
+                .post('/4_0_0/Person/1/$merge')
+                .send(person1Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
             expect(resp).toHaveMergeResponse({created: true});
+
+            // search by token system and code and make sure we get the right Person back
+            resp = await request
+                .get('/4_0_0/Person/?_bundle=1')
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResponse(expectedPersonResources);
 
             resp = await request
                 .post('/4_0_0/Person/1/$merge')
@@ -58,6 +90,48 @@ describe('Person Tests', () => {
                 .set(getHeaders('user/*.read user/*.write'));
             // noinspection JSUnresolvedFunction
             expect(resp).toHaveStatusCode(403);
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResponse(expectedMissingAccessScope, r => {
+                delete r['diagnostics'];
+            });
+
+        });
+        test('mergeWith_id fails with wrong access scope (create)', async () => {
+            const request = await createTestRequest();
+            // ARRANGE
+            // ACT & ASSERT
+            let resp = await request
+                .post('/4_0_0/Person/1/$merge')
+                .send(person1Resource)
+                .set(getHeaders('user/*.read user/*.write access/foo.*'));
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveStatusCode(403);
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResponse(expectedWrongAccessScope, r => {
+                delete r['diagnostics'];
+            });
+        });
+        test('mergeWith_id fails with wrong access scope (update)', async () => {
+            const request = await createTestRequest();
+            // ARRANGE
+            // add the resources to FHIR server
+            let resp = await request
+                .post('/4_0_0/Person/1/$merge')
+                .send(person1Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            resp = await request
+                .post('/4_0_0/Person/1/$merge')
+                .send(person1Resource)
+                .set(getHeaders('user/*.read user/*.write access/foo.*'));
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveStatusCode(403);
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResponse(expectedWrongAccessScope, r => {
+                delete r['diagnostics'];
+            });
 
             // ACT & ASSERT
             // search by token system and code and make sure we get the right Person back
