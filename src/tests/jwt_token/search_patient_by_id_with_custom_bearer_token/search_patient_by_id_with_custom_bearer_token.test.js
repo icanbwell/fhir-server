601c0d714fe11dafc8aa238c8640044ca7c24c8c
Hundley Crews <hcrews@gmail.com>
2022-11-03 16:01:24 -0400

    Add support for x-bwell-identity header

--- src/tests/jwt_token/search_patient_by_id_with_custom_bearer_token/search_patient_by_id_with_custom_bearer_token.test.js
+++ src/tests/jwt_token/search_patient_by_id_with_custom_bearer_token/search_patient_by_id_with_custom_bearer_token.test.js
@@ -8,7 +8,7 @@ const {
     commonBeforeEach,
     commonAfterEach,
     getHeadersWithCustomToken,
-    createTestRequest,
+    createTestRequest, getUnAuthenticatedHeaders, getFullAccessToken,
 } = require('../../common');
 const { describe, beforeEach, afterEach, expect, test } = require('@jest/globals');
 
@@ -49,6 +49,50 @@ describe('PatientReturnIdWithCustomBearerTokenTests', () => {
                 .get('/4_0_0/Patient/00100000000')
                 .set(getHeadersWithCustomToken())
                 .expect(200);
+            console.log('------- response Patient sorted ------------');
+            console.log(JSON.stringify(resp.body, null, 2));
+            console.log('------- end response sort ------------');
+            // clear out the lastUpdated column since that changes
+            let body = resp.body;
+            delete body['meta']['lastUpdated'];
+
+            let expected = expectedSinglePatientResource[0];
+            delete expected['meta']['lastUpdated'];
+            delete expected['$schema'];
+
+            expect(body).toStrictEqual(expected);
+        });
+    });
+
+    describe('Patient Search By Id Tests With x-well-identity Bearer Token', () => {
+        test('search by single id works', async () => {
+            const request = await createTestRequest();
+            const headers = { ...getUnAuthenticatedHeaders(), 'x-bwell-identity': getFullAccessToken()};
+            let resp = await request
+              .get('/4_0_0/Patient')
+              .set(headers)
+              .expect(200);
+            expect(resp.body.length).toBe(0);
+            console.log('------- response 1 ------------');
+            console.log(JSON.stringify(resp.body, null, 2));
+            console.log('------- end response 1 ------------');
+            resp = await request
+              .post('/4_0_0/Patient/1679033641/$merge?validate=true')
+              .send(patient1Resource)
+              .set(headers)
+              .expect(200);
+            console.log('------- response patient1Resource ------------');
+            console.log(JSON.stringify(resp.body, null, 2));
+            console.log('------- end response  ------------');
+            expect(resp.body['created']).toBe(true);
+            resp = await request.get('/4_0_0/Patient').set(getHeadersWithCustomToken()).expect(200);
+            console.log('------- response 3 ------------');
+            console.log(JSON.stringify(resp.body, null, 2));
+            console.log('------- end response 3 ------------');
+            resp = await request
+              .get('/4_0_0/Patient/00100000000')
+              .set(headers)
+              .expect(200);
             console.log('------- response Patient sorted ------------');
             console.log(JSON.stringify(resp.body, null, 2));
             console.log('------- end response sort ------------');
