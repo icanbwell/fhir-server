6766b00d928f165e3b4afaf75c0b2c599ac86e7a
imranq2 <imranq2@hotmail.com>
2022-11-05 19:00:00 -0700

    enable createAccessIndexField.js to also create _sourceId, _uuid and _sourceAssigningAuthority fields

--- src/admin/runners/createAccessIndexFieldRunner.js
+++ src/admin/runners/createAccessIndexFieldRunner.js
@@ -1,5 +1,6 @@
 const {BaseBulkOperationRunner} = require('./baseBulkOperationRunner');
 const {mongoConfig, auditEventMongoConfig} = require('../../config');
+const {generateUUID} = require('../../utils/uid.util');
 
 /**
  * @classdesc Copies documents from source collection into the appropriate partitioned collection
@@ -12,6 +13,7 @@ class CreateAccessIndexRunner extends BaseBulkOperationRunner {
      * @param {number} batchSize
      * @param {boolean} useAuditDatabase
      * @param {AdminLogger} adminLogger
+     * @param {MongoDatabaseManager} mongoDatabaseManager
      */
     constructor(
         {
@@ -19,12 +21,14 @@ class CreateAccessIndexRunner extends BaseBulkOperationRunner {
             collections,
             batchSize,
             useAuditDatabase,
-            adminLogger
+            adminLogger,
+            mongoDatabaseManager
         }) {
         super({
             mongoCollectionManager,
             batchSize,
-            adminLogger
+            adminLogger,
+            mongoDatabaseManager
         });
         /**
          * @type {string[]}
@@ -48,18 +52,56 @@ class CreateAccessIndexRunner extends BaseBulkOperationRunner {
      */
     async processRecordAsync(doc) {
         const operations = [];
-        if (!doc.meta || !doc.meta.security){
+        if (!doc.meta || !doc.meta.security) {
             return operations;
         }
+        // Step 1: Add any missing _access tags
         const accessCodes = doc.meta.security.filter(s => s.system === 'https://www.icanbwell.com/access').map(s => s.code);
+        // update only the necessary field in the document
+        const setCommand = {};
+        /**
+         * @type {boolean}
+         */
+        let hasUpdate = false;
         if (accessCodes.length > 0 && !doc['_access']) {
             const _access = {};
             for (const accessCode of accessCodes) {
                 _access[`${accessCode}`] = 1;
             }
-            // update only the necessary field in the document
-            const setCommand = {};
             setCommand['_access'] = _access;
+            hasUpdate = true;
+        }
+        // Step 2: add any missing _sourceAssigningAuthority tags
+        /**
+         * @type {string[]}
+         */
+        let sourceAssigningAuthorityCodes = doc.meta.security.filter(
+            s => s.system === 'https://www.icanbwell.com/sourceAssigningAuthority').map(s => s.code);
+        // if no sourceAssigningAuthorityCodes so fall back to owner tags
+        if (sourceAssigningAuthorityCodes.length === 0) {
+            sourceAssigningAuthorityCodes = doc.meta.security.filter(
+                s => s.system === 'https://www.icanbwell.com/owner').map(s => s.code);
+        }
+        if (sourceAssigningAuthorityCodes.length > 0 && !doc['_sourceAssigningAuthority']) {
+            const _sourceAssigningAuthority = {};
+            for (const sourceAssigningAuthorityCode of sourceAssigningAuthorityCodes) {
+                _sourceAssigningAuthority[`${sourceAssigningAuthorityCode}`] = 1;
+            }
+            setCommand['_sourceAssigningAuthority'] = _sourceAssigningAuthority;
+            hasUpdate = true;
+        }
+        // Step 3: add _sourceId
+        if (!doc['_sourceId']) {
+            setCommand['_sourceId'] = doc.id;
+            hasUpdate = true;
+        }
+        // Step 4: add _uuid
+        if (!doc['_uuid']) {
+            setCommand['_uuid'] = `urn:uuid:${generateUUID()}`;
+            hasUpdate = true;
+        }
+        // if there are any updates to be done
+        if (hasUpdate) {
             /**
              * @type {import('mongodb').BulkWriteOperation<import('mongodb').DefaultSchema>}
              */
@@ -75,6 +117,7 @@ class CreateAccessIndexRunner extends BaseBulkOperationRunner {
      * @returns {Promise<void>}
      */
     async processAsync() {
+        // noinspection JSValidateTypes
         try {
             if (this.collections.length > 0 && this.collections[0] === 'all') {
                 this.collections = await this.getAllCollectionNamesAsync(
@@ -95,11 +138,18 @@ class CreateAccessIndexRunner extends BaseBulkOperationRunner {
                 const query = {
                     _access: null
                 };
+                // noinspection JSValidateTypes
+                /**
+                 * @type {import('mongodb').Collection<import('mongodb').Document>}
+                 */
                 const projection = {
                     'id': 1,
                     'meta.security.system': 1,
                     'meta.security.code': 1,
-                    '_access': 1
+                    '_access': 1,
+                    '_sourceAssigningAuthority': 1,
+                    '_sourceId': 1,
+                    '_uuid': 1
                 };
                 try {
                     await this.runForQueryBatchesAsync(
