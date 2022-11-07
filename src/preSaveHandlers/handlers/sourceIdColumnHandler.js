15b623fc71670e91c56c7b71d781c19e82e5eb28
imranq2 <imranq2@hotmail.com>
2022-11-07 09:58:49 -0800

    set sourceId and uuid

--- src/preSaveHandlers/handlers/sourceIdColumnHandler.js
+++ src/preSaveHandlers/handlers/sourceIdColumnHandler.js
@@ -1,14 +1,21 @@
 const {PreSaveHandler} = require('./preSaveHandler');
 const {IdentifierSystem} = require('../../utils/identifierSystem');
+const {getFirstElementOrNull} = require('../../utils/list.util');
 
 class SourceIdColumnHandler extends PreSaveHandler {
     async preSaveAsync({resource}) {
-        // if an identifier with system=https://www.icanbwell.com/sourceId exists then use that
-        if (resource.identifier && resource.identifier.some(s => s.system === IdentifierSystem.sourceId)) {
-            resource._sourceId = resource.meta.security.filter(s => s.system === IdentifierSystem.sourceId)[0].value;
-        } else if (!resource._sourceId) {
+        if (!resource._sourceId) {
+            // if an identifier with system=https://www.icanbwell.com/sourceId exists then use that
+            if (resource.identifier && resource.identifier.some(s => s.system === IdentifierSystem.sourceId)) {
+                resource._sourceId = getFirstElementOrNull(
+                    resource.meta.security.filter(s => s.system === IdentifierSystem.sourceId).map(s => s.value));
+            }
+        }
+        if (!resource._sourceId) {
             resource._sourceId = resource.id;
         }
+
+        // if an identifier with system=https://www.icanbwell.com/sourceId does not exist then create it
         if (resource.identifier && !resource.identifier.some(s => s.system === IdentifierSystem.sourceId)) {
             resource.identifier.push(
                 {
--- src/preSaveHandlers/handlers/uuidColumnHandler.js
+++ src/preSaveHandlers/handlers/uuidColumnHandler.js
@@ -1,19 +1,25 @@
 const {PreSaveHandler} = require('./preSaveHandler');
 const {generateUUID, isUuid} = require('../../utils/uid.util');
 const {IdentifierSystem} = require('../../utils/identifierSystem');
+const {getFirstElementOrNull} = require('../../utils/list.util');
 
 class UuidColumnHandler extends PreSaveHandler {
     async preSaveAsync({resource}) {
         if (!resource._uuid) {
-            // if an identifier with system=https://www.icanbwell.com/uuid exists then use that
+            // if an identifier with system=https://www.icanbwell.com/sourceId exists then use that
             if (resource.identifier && resource.identifier.some(s => s.system === IdentifierSystem.uuid)) {
-                resource._uuid = resource.meta.security.filter(s => s.system === IdentifierSystem.uuid)[0].value;
-            } else if (isUuid(resource.id)) {
+                resource._uuid = getFirstElementOrNull(
+                    resource.meta.security.filter(s => s.system === IdentifierSystem.uuid).map(s => s.value));
+            }
+        }
+        if (!resource._uuid) {
+            if (isUuid(resource.id)) {
                 resource._uuid = resource.id;
             } else {
                 resource._uuid = `${generateUUID()}`;
             }
         }
+
         if (resource.identifier && !resource.identifier.some(s => s.system === IdentifierSystem.uuid)) {
             resource.identifier.push(
                 {
