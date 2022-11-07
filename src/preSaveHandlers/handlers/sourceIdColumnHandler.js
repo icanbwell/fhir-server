4ca55f49094f801468b3d1e76d45f1f60dfc56e1
imranq2 <imranq2@hotmail.com>
2022-11-07 09:23:59 -0800

    set sourceId and uuid

--- src/preSaveHandlers/handlers/sourceIdColumnHandler.js
+++ src/preSaveHandlers/handlers/sourceIdColumnHandler.js
@@ -2,8 +2,19 @@ const {PreSaveHandler} = require('./preSaveHandler');
 
 class SourceIdColumnHandler extends PreSaveHandler {
     async preSaveAsync({resource}) {
-        if (!resource._sourceId) {
+        // if an identifier with system=https://www.icanbwell.com/sourceId exists then use that
+        if (resource.identifier && resource.identifier.some(s => s.system === 'https://www.icanbwell.com/sourceId')) {
+            resource._sourceId = resource.meta.security.filter(s => s.system === 'https://www.icanbwell.com/sourceId')[0].value;
+        } else if (!resource._sourceId) {
             resource._sourceId = resource.id;
+        }
+        if (resource.identifier && !resource.identifier.some(s => s.system === 'https://www.icanbwell.com/sourceId')) {
+            resource.identifier.push(
+                {
+                    'system': 'https://www.icanbwell.com/sourceId',
+                    'value': resource._sourceId
+                }
+            );
         }
         return resource;
     }
--- src/preSaveHandlers/handlers/uuidColumnHandler.js
+++ src/preSaveHandlers/handlers/uuidColumnHandler.js
@@ -4,12 +4,24 @@ const {generateUUID, isUuid} = require('../../utils/uid.util');
 class UuidColumnHandler extends PreSaveHandler {
     async preSaveAsync({resource}) {
         if (!resource._uuid) {
-            if (isUuid(resource.id)) {
+            // if an identifier with system=https://www.icanbwell.com/uuid exists then use that
+            if (resource.identifier && resource.identifier.some(s => s.system === 'https://www.icanbwell.com/uuid')) {
+                resource._uuid = resource.meta.security.filter(s => s.system === 'https://www.icanbwell.com/uuid')[0].value;
+            } else if (isUuid(resource.id)) {
                 resource._uuid = resource.id;
             } else {
                 resource._uuid = `${generateUUID()}`;
             }
         }
+        if (resource.identifier && !resource.identifier.some(s => s.system === 'https://www.icanbwell.com/uuid')) {
+            resource.identifier.push(
+                {
+                    'system': 'https://www.icanbwell.com/uuid',
+                    'value': resource._uuid
+                }
+            );
+        }
+
         return resource;
     }
 }
