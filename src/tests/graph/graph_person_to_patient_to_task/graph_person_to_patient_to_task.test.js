f54196b367ba6d3cede010e21cd1392fa6026f86
imranq2 <imranq2@hotmail.com>
2022-11-07 20:41:28 -0800

    add test for graph of tasks

---
+++ src/tests/graph/graph_person_to_patient_to_task/graph_person_to_patient_to_task.test.js
@@ -0,0 +1,89 @@
+// test file
+const person1Resource = require('./fixtures/Person/person1.json');
+
+const patient1Resource = require('./fixtures/Patient/patient1.json');
+const patient2Resource = require('./fixtures/Patient/patient2.json');
+
+const task1Resource = require('./fixtures/Task/task1.json');
+const task2Resource = require('./fixtures/Task/task2.json');
+const task3Resource = require('./fixtures/Task/task3.json');
+
+// graph
+const graphDefinitionResource = require('./fixtures/graph/my_graph.json');
+
+// expected
+const expectedResource = require('./fixtures/expected/expected.json');
+
+const {commonBeforeEach, commonAfterEach, getHeaders, createTestRequest} = require('../../common');
+const {describe, beforeEach, afterEach, test, expect} = require('@jest/globals');
+
+describe('Person Tests', () => {
+    beforeEach(async () => {
+        await commonBeforeEach();
+    });
+
+    afterEach(async () => {
+        await commonAfterEach();
+    });
+
+    describe('Person graph_person_to_patient_to_task Tests', () => {
+        test('graph_person_to_patient_to_task works', async () => {
+            const request = await createTestRequest();
+            // ARRANGE
+            // add the resources to FHIR server
+            let resp = await request
+                .post('/4_0_0/Person/1/$merge?validate=true')
+                .send(person1Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            // add patients
+            resp = await request
+                .post('/4_0_0/Patient/1/$merge?validate=true')
+                .send(patient1Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+            resp = await request
+                .post('/4_0_0/Patient/1/$merge?validate=true')
+                .send(patient2Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            // add tasks
+            resp = await request
+                .post('/4_0_0/Task/1/$merge?validate=true')
+                .send(task1Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            resp = await request
+                .post('/4_0_0/Task/1/$merge?validate=true')
+                .send(task2Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            resp = await request
+                .post('/4_0_0/Task/1/$merge?validate=true')
+                .send(task3Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            // ACT & ASSERT
+            resp = await request
+                .post(
+                    '/4_0_0/ExplanationOfBenefit/$graph?id=WPS-Claim-230916613369,WPS-Claim-230916613368&contained=true'
+                )
+                .set(getHeaders())
+                .send(graphDefinitionResource);
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResponse(expectedResource);
+
+        });
+    });
+});
