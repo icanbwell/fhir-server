7ac7e9ee3896108e68eac216e353dc916eb104d0
imranq2 <imranq2@hotmail.com>
2022-11-16 17:31:24 -0800

    support search by uuid

---
+++ src/tests/searchParameters/search_by_uuid/search_by_uuid.test.js
@@ -0,0 +1,45 @@
+// test file
+const practitioner1Resource = require('./fixtures/Practitioner/practitioner1.json');
+
+// expected
+const expectedPractitionerResources = require('./fixtures/expected/expected_Practitioner.json');
+
+const {
+    commonBeforeEach,
+    commonAfterEach,
+    getHeaders,
+    createTestRequest,
+} = require('../../common');
+const {describe, beforeEach, afterEach, test } = require('@jest/globals');
+
+describe('Practitioner Tests', () => {
+    beforeEach(async () => {
+        await commonBeforeEach();
+    });
+
+    afterEach(async () => {
+        await commonAfterEach();
+    });
+
+    describe('Practitioner search by uuid Tests', () => {
+        test('search by uuid works', async () => {
+            const request = await createTestRequest();
+            // ARRANGE
+            // add the resources to FHIR server
+            let resp = await request
+                .post('/4_0_0/Practitioner/1/$merge?validate=true')
+                .send(practitioner1Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            // ACT & ASSERT
+            // search by token system and code and make sure we get the right Practitioner back
+            resp = await request
+                .get('/4_0_0/Practitioner/?_bundle=1&id=1679033641')
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResponse(expectedPractitionerResources);
+        });
+    });
+});
