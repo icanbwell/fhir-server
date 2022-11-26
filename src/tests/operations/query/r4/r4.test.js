acc97f59969174cf96345a28e227e079e03cbdc3
imranq2 <imranq2@hotmail.com>
2022-11-25 19:51:00 -0800

    add support for ":not" in query parameters

--- src/tests/operations/query/r4/r4.test.js
+++ src/tests/operations/query/r4/r4.test.js
@@ -290,5 +290,49 @@ describe('r4 search Tests', () => {
                 '$gt': 'john-muir-health-e.k-4ea143ZrQGvdUvf-b2y.tdyiVMBWgblY4f6y2zis3'
             });
         });
+        test('r4 works with :not for id', async () => {
+            await createTestRequest((container) => {
+                container.register('configManager', () => new MockConfigManager());
+                container.register('indexProvider', (c) => new MockIndexProvider({
+                    configManager: c.configManager
+                }));
+                container.register('accessIndexManager', (c1) => new MockAccessIndexManager({
+                    configManager: c1.configManager,
+                    indexProvider: c1.indexProvider
+                }));
+                return container;
+            });
+            const container = getTestContainer();
+            const r4SearchQueryCreator = container.r4SearchQueryCreator;
+            const args = {
+                '_id:not': 'john-muir-health-e.k-4ea143ZrQGvdUvf-b2y.tdyiVMBWgblY4f6y2zis3',
+            };
+            const result = r4SearchQueryCreator.buildR4SearchQuery({
+                resourceType: 'Patient', args
+            });
+            expect(result.query.$and['0'].$not.id).toStrictEqual('john-muir-health-e.k-4ea143ZrQGvdUvf-b2y.tdyiVMBWgblY4f6y2zis3');
+        });
+        test('r4 works with :not for _security', async () => {
+            await createTestRequest((container) => {
+                container.register('configManager', () => new MockConfigManager());
+                container.register('indexProvider', (c) => new MockIndexProvider({
+                    configManager: c.configManager
+                }));
+                container.register('accessIndexManager', (c1) => new MockAccessIndexManager({
+                    configManager: c1.configManager,
+                    indexProvider: c1.indexProvider
+                }));
+                return container;
+            });
+            const container = getTestContainer();
+            const r4SearchQueryCreator = container.r4SearchQueryCreator;
+            const args = {
+                '_security:not': 'https://www.icanbwell.com/access%7Cbwell',
+            };
+            const result = r4SearchQueryCreator.buildR4SearchQuery({
+                resourceType: 'Patient', args
+            });
+            expect(result.query.$and['0'].$not['meta.security.code']).toStrictEqual('https://www.icanbwell.com/access%7Cbwell');
+        });
     });
 });
