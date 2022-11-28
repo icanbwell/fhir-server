f4c577514501429958f23460d36cfe92b0dba2a4
imranq2 <imranq2@hotmail.com>
2022-11-27 17:30:38 -0800

    add test for persondata.test.js

---
+++ src/tests/admin/patientData/persondata.test.js
@@ -0,0 +1,174 @@
+// test file
+const patient1Resource = require('./fixtures/Patient/patient1.json');
+const patient2Resource = require('./fixtures/Patient/patient2.json');
+
+const observation1Resource = require('./fixtures/Observation/observation1.json');
+
+const personResource = require('./fixtures/Person/person.json');
+const topLevelPersonResource = require('./fixtures/Person/topLevelPerson.json');
+
+
+// expected
+const expectedPersonResources = require('./fixtures/expected/expected_Person.json');
+const expectedPersonDeletionResources = require('./fixtures/expected/expected_Person_deletion.json');
+
+const {
+    commonBeforeEach,
+    commonAfterEach,
+    getHeaders,
+    createTestRequest,
+    getHeadersWithCustomToken
+} = require('../../common');
+const {describe, beforeEach, afterEach, test} = require('@jest/globals');
+
+describe('Person Tests', () => {
+    beforeEach(async () => {
+        await commonBeforeEach();
+    });
+
+    afterEach(async () => {
+        await commonAfterEach();
+    });
+
+    describe('Person personData Tests', () => {
+        test('personData fails without admin permissions', async () => {
+            const request = await createTestRequest();
+            // ARRANGE
+            // add the resources to FHIR server
+            let resp = await request
+                .post('/4_0_0/Patient/1/$merge?validate=true')
+                .send(patient1Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            resp = await request
+                .post('/4_0_0/Observation/1/$merge?validate=true')
+                .send(observation1Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            await request
+                .post('/4_0_0/Person/1/$merge?validate=true')
+                .send(personResource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            // ACT & ASSERT
+            resp = await request
+                .get('/admin/showPersonDataGraph?personId=person1')
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp.body.message).toBe('Missing scopes for admin/*.read in user/*.read user/*.write access/*.*');
+        });
+        test('personData works with admin permissions', async () => {
+            const request = await createTestRequest();
+
+            // ARRANGE
+            // add the resources to FHIR server
+            let resp = await request
+                .post('/4_0_0/Patient/1/$merge?validate=true')
+                .send(patient1Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            resp = await request
+                .post('/4_0_0/Observation/1/$merge?validate=true')
+                .send(observation1Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            await request
+                .post('/4_0_0/Patient/1/$merge?validate=true')
+                .send(patient2Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            await request
+                .post('/4_0_0/Person/1/$merge?validate=true')
+                .send(personResource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            await request
+                .post('/4_0_0/Person/1/$merge?validate=true')
+                .send(topLevelPersonResource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            // ACT & ASSERT
+            resp = await request
+                .get('/admin/showPersonDataGraph?personId=person1')
+                .set(getHeadersWithCustomToken('user/*.read admin/*.*'));
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResponse(expectedPersonResources);
+        });
+        test('personData delete works with admin permissions', async () => {
+            const request = await createTestRequest();
+
+            // ARRANGE
+            // add the resources to FHIR server
+            let resp = await request
+                .post('/4_0_0/Patient/1/$merge?validate=true')
+                .send(patient1Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            resp = await request
+                .post('/4_0_0/Observation/1/$merge?validate=true')
+                .send(observation1Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            await request
+                .post('/4_0_0/Patient/1/$merge?validate=true')
+                .send(patient2Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            await request
+                .post('/4_0_0/Person/1/$merge?validate=true')
+                .send(personResource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            await request
+                .post('/4_0_0/Person/1/$merge?validate=true')
+                .send(topLevelPersonResource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            // ACT & ASSERT
+            resp = await request
+                .get('/admin/deletePersonDataGraph?personId=person1')
+                .set(getHeadersWithCustomToken('user/*.* admin/*.*'));
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResponse(expectedPersonDeletionResources);
+
+            resp = await request
+                .get('/admin/showPersonDataGraph?personId=person1')
+                .set(getHeadersWithCustomToken('user/*.read admin/*.*'));
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResourceCount(0);
+
+            // make sure top level person is NOT deleted
+            resp = await request
+                .get('/4_0_0/Person/topLevelPerson')
+                .set(getHeadersWithCustomToken('user/*.read admin/*.*'));
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResourceCount(1);
+        });
+    });
+});
