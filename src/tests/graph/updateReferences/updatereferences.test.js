58b3e347630dc5bda4c42ed518d5418462e9ba31
imranq2 <imranq2@hotmail.com>
2022-12-01 17:36:30 -0800

    add test

---
+++ src/tests/graph/updateReferences/updatereferences.test.js
@@ -0,0 +1,53 @@
+// test file
+const observation1Resource = require('./fixtures/Observation/observation1.json');
+
+// expected
+const expectedObservationResources = require('./fixtures/expected/expected_Observation.json');
+
+const {commonBeforeEach, commonAfterEach, getHeaders, createTestRequest} = require('../../common');
+const {describe, beforeEach, afterEach, test, expect} = require('@jest/globals');
+const Observation = require('../../../fhir/classes/4_0_0/resources/observation');
+
+describe('Observation Tests', () => {
+    beforeEach(async () => {
+        await commonBeforeEach();
+    });
+
+    afterEach(async () => {
+        await commonAfterEach();
+    });
+
+    describe('Observation updateReferences Tests', () => {
+        test('updateReferences works', async () => {
+            const request = await createTestRequest();
+            let resp = await request
+                .get('/4_0_0/Observation')
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResourceCount(0);
+
+            const observation = new Observation(
+                observation1Resource
+            );
+
+            /**
+             * @param {Reference} reference
+             */
+            function fnUpdateReference(reference) {
+                console.log(reference.toJSON());
+                if (reference.reference && reference.reference.startsWith('Patient/')) {
+                    reference.reference = 'Patient/ProxyPatient';
+                }
+                if (reference.reference && reference.reference.startsWith('Task/')) {
+                    reference.reference = 'Task/ProxyTask';
+                }
+            }
+
+            observation.updateReferences({
+                fnUpdateReference
+            });
+
+            expect(observation.toJSON()).toStrictEqual(expectedObservationResources);
+        });
+    });
+});
