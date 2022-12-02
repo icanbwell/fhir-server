83b2fee6b87d04734b09d26015d3d1517af5673f
imranq2 <imranq2@hotmail.com>
2022-12-02 13:21:58 -0800

    add test

--- src/tests/graphql/explanationOfBenefit/graphql.explanationOfBenefits.test.js
+++ src/tests/graphql/explanationOfBenefit/graphql.explanationOfBenefits.test.js
@@ -1,5 +1,6 @@
 const explanationOfBenefitBundleResource = require('./fixtures/explanation_of_benefits.json');
 const expectedGraphQLResponse = require('./fixtures/expected_graphql_response.json');
+const expectedGraphQLWithExplainResponse = require('./fixtures/expected_graphql_response_with_explain.json');
 
 const patientBundleResource = require('./fixtures/patients.json');
 const organizationBundleResource = require('./fixtures/organizations.json');
@@ -11,6 +12,12 @@ const path = require('path');
 // eslint-disable-next-line security/detect-non-literal-fs-filename
 const explanationOfBenefitQuery = fs.readFileSync(
     path.resolve(__dirname, './fixtures/query.graphql'),
+    'utf8'
+);
+
+// eslint-disable-next-line security/detect-non-literal-fs-filename
+const explanationOfBenefitWithExplainQuery = fs.readFileSync(
+    path.resolve(__dirname, './fixtures/query_explain.graphql'),
     'utf8'
 );
 
@@ -110,6 +117,65 @@ describe('GraphQL ExplanationOfBenefit Tests', () => {
 
                 return r;
             });
+        });
+        test('GraphQL ExplanationOfBenefit with explain', async () => {
+            const request = await createTestRequest();
+            const graphqlQueryText = explanationOfBenefitWithExplainQuery.replace(/\\n/g, '');
+            let resp = await request
+                .get('/4_0_0/ExplanationOfBenefit')
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResourceCount(0);
+
+            resp = await request
+                .post('/4_0_0/Patient/1/$merge')
+                .send(patientBundleResource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            resp = await request
+                .post('/4_0_0/Organization/1/$merge')
+                .send(organizationBundleResource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            resp = await request
+                .post('/4_0_0/Coverage/1/$merge')
+                .send(coverageBundleResource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            resp = await request
+                .post('/4_0_0/ExplanationOfBenefit/1/$merge')
+                .send(explanationOfBenefitBundleResource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            resp = await request.get('/4_0_0/Patient/').set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResourceCount(2);
+
+            resp = await request.get('/4_0_0/ExplanationOfBenefit/').set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResourceCount(2);
+
+            resp = await request
+                // .get('/graphql/?query=' + graphqlQueryText)
+                // .set(getHeaders())
+                .post('/graphqlv2')
+                .send({
+                    operationName: null,
+                    variables: {},
+                    query: graphqlQueryText,
+                })
+                .set(getGraphQLHeaders());
+
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResponse(expectedGraphQLWithExplainResponse);
         });
     });
 });
