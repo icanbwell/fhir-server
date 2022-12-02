943e561b7b535ecb21663f5b7a10f1cfe4692739
imranq2 <imranq2@hotmail.com>
2022-12-01 18:00:48 -0800

    add test

--- src/tests/graph/updateReferences/updatereferences.test.js
+++ src/tests/graph/updateReferences/updatereferences.test.js
@@ -9,6 +9,8 @@ const expectedObservationWithoutProxyPatientResources = require('./fixtures/expe
 const {commonBeforeEach, commonAfterEach, getHeaders, createTestRequest} = require('../../common');
 const {describe, beforeEach, afterEach, test, expect} = require('@jest/globals');
 const Observation = require('../../../fhir/classes/4_0_0/resources/observation');
+const personResource = require('../../searchParameters/search_by_proxy_patient/search_by_proxy_patient/fixtures/Person/person.json');
+const observation2Resource = require('../../searchParameters/search_by_proxy_patient/search_by_proxy_patient/fixtures/Observation/observation2.json');
 
 describe('UpdateReferences Tests', () => {
     beforeEach(async () => {
@@ -51,23 +53,74 @@ describe('UpdateReferences Tests', () => {
 
             expect(observation.toJSON()).toStrictEqual(expectedObservationResources);
         });
-        test('updateReferences works via GET', async () => {
+        test('updateReferences works via GET without proxy patient', async () => {
             const request = await createTestRequest();
+
             let resp = await request
-                .get('/4_0_0/Observation')
+                .post('/4_0_0/Person/1/$merge?validate=true')
+                .send(personResource)
                 .set(getHeaders());
             // noinspection JSUnresolvedFunction
-            expect(resp).toHaveResourceCount(0);
+            expect(resp).toHaveMergeResponse({created: true});
 
             resp = await request
-                .post('/4_0_0/Observation/2354-InAgeCohort/$merge')
+                .post('/4_0_0/Observation/1/$merge?validate=true')
                 .send(observation1Resource)
                 .set(getHeaders());
             // noinspection JSUnresolvedFunction
             expect(resp).toHaveMergeResponse({created: true});
 
             resp = await request
+                .post('/4_0_0/Observation/1/$merge?validate=true')
+                .send(observation2Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            resp = await request
                 .get('/4_0_0/Observation/2354-InAgeCohort')
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResponse(expectedObservationWithoutProxyPatientResources.entry[0].resource);
+
+            resp = await request
+                .get('/4_0_0/Observation/?_bundle=1&patient=Patient/00100000000')
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResponse(expectedObservationWithoutProxyPatientResources);
+        });
+        test('updateReferences works via GET with proxy patient', async () => {
+            const request = await createTestRequest();
+
+            let resp = await request
+                .post('/4_0_0/Person/1/$merge?validate=true')
+                .send(personResource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            resp = await request
+                .post('/4_0_0/Observation/1/$merge?validate=true')
+                .send(observation1Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            resp = await request
+                .post('/4_0_0/Observation/1/$merge?validate=true')
+                .send(observation2Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            resp = await request
+                .get('/4_0_0/Observation/?patient=Patient/00100000000')
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResponse(expectedObservationWithoutProxyPatientResources);
+
+            resp = await request
+                .get('/4_0_0/Observation/?_bundle=1&patient=Patient/person.m65633')
                 .set(getHeaders());
             // noinspection JSUnresolvedFunction
             expect(resp).toHaveResponse(expectedObservationWithoutProxyPatientResources);
