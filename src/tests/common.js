39056d4c24aab597f7c72e10b610248fcad11cef
imranq2 <imranq2@hotmail.com>
2022-12-07 14:25:24 -0800

    do manual setup and teardown of mongo

--- src/tests/common.js
+++ src/tests/common.js
@@ -134,7 +134,7 @@ module.exports.commonAfterEach = async () => {
         server = null;
     }
     tester = null;
-    // app = null;
+    app = null;
     // global.gc();
     // globals.clear();
 };
