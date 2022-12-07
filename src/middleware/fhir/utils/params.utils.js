30ac436b0185f786704eea875fe0db5c92134a16
imranq2 <imranq2@hotmail.com>
2022-12-07 10:20:06 -0800

    set max old size

--- src/middleware/fhir/utils/params.utils.js
+++ src/middleware/fhir/utils/params.utils.js
@@ -37,8 +37,8 @@ const getSearchParameters = (profile, version, customArgsModule) => {
     // args for this particular route instead of the default arguments included
 
     if (customArgsModule) {
-        // eslint-disable-next-line security/detect-non-literal-require
         // noinspection JSCheckFunctionSignatures
+        // eslint-disable-next-line security/detect-non-literal-require
         let paramsAsArray = require(String(customArgsModule)).makeResource(Object.assign({}, {
             base_version: version,
             key: lowercaseProfileName
