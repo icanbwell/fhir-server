66dc89a8516eb68f62b36fb8b4084c1265c22425
Imran Qureshi <imranq2@hotmail.com>
2022-12-19 09:55:46 -0800

    cache AWS secret for performance (#769)
    * cache AWS secret for performance
    
    * cache AWS secret for performance

--- src/utils/awsSecretsManager.js
+++ src/utils/awsSecretsManager.js
@@ -1,6 +1,10 @@
 const {GetSecretValueCommand} = require('@aws-sdk/client-secrets-manager');
 const {assertTypeEquals} = require('./assertType');
 const {AwsSecretsClientFactory} = require('./awsSecretsClientFactory');
+const {RethrownError} = require('./rethrownError');
+
+const Mutex = require('async-mutex').Mutex;
+const mutex = new Mutex();
 
 class AwsSecretsManager {
     /**
@@ -17,25 +21,56 @@ class AwsSecretsManager {
          */
         this.secretsManagerClientFactory = secretsManagerClientFactory;
         assertTypeEquals(secretsManagerClientFactory, AwsSecretsClientFactory);
+
+        /**
+         * This is a Map where the key is secret name and the value is result
+         * @type {Map<string, string>}
+         */
+        this.mapCache = new Map();
     }
 
     /**
      * @param {string} secretName
      */
     async getSecretValueAsync({secretName}) {
-        var input = {SecretId: secretName};
-        const command = new GetSecretValueCommand(input);
-        /**
-         * @type {import('@aws-sdk/client-secrets-manager').SecretsManagerClient}
-         */
-        const secretsManagerClient = await this.secretsManagerClientFactory.createSecretsClientAsync();
-        /**
-         * @type {import('@aws-sdk/client-secrets-manager').GetSecretValueCommandOutput}
-         */
-        const response = await secretsManagerClient.send(command);
-        const secretString = response.SecretString;
-        const {username, password} = JSON.parse(secretString);
-        return {username, password};
+        const release = await mutex.acquire();
+        try {
+            /**
+             * @type {string}
+             */
+            let secretString;
+            if (this.mapCache.has(secretName)) {
+                secretString = this.mapCache.get(secretName);
+            } else {
+                var input = {SecretId: secretName};
+                const command = new GetSecretValueCommand(input);
+                /**
+                 * @type {import('@aws-sdk/client-secrets-manager').SecretsManagerClient}
+                 */
+                const secretsManagerClient = await this.secretsManagerClientFactory.createSecretsClientAsync();
+                /**
+                 * @type {import('@aws-sdk/client-secrets-manager').GetSecretValueCommandOutput}
+                 */
+                const response = await secretsManagerClient.send(command);
+                secretString = response.SecretString;
+                this.mapCache.set(secretName, secretString);
+            }
+            const {username, password} = JSON.parse(secretString);
+            return {username, password};
+        } catch (e) {
+            throw new RethrownError(
+                {
+                    message: `Error retrieving AWS secret ${secretName}`,
+                    error: e,
+                    args: {
+                        secret: secretName
+                    },
+                    source: 'AwsSecretManager.getSecretValueAsync'
+                }
+            );
+        } finally {
+            release();
+        }
     }
 }
 
