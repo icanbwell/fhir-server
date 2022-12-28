481c2ae33b72e07ebc1d280de12f389f33e84924
imranq2 <imranq2@hotmail.com>
2022-12-27 17:49:32 -0800

    add test

--- src/tests/dataLayer/concurrency_issue/concurrency_issue.test.js
+++ src/tests/dataLayer/concurrency_issue/concurrency_issue.test.js
@@ -3,9 +3,11 @@ const codesystem1Resource = require('./fixtures/CodeSystem/codesystem.json');
 
 // expected
 const expectedCodeSystemResources = require('./fixtures/expected/expected_codesystem.json');
+const expectedCodeSystemsFromDatabase = require('./fixtures/expected/expected_codesystem_from_database.json');
 
-const {commonBeforeEach, commonAfterEach, getHeaders, createTestRequest} = require('../../common');
+const {commonBeforeEach, commonAfterEach, getHeaders, createTestRequest, getTestContainer} = require('../../common');
 const {describe, beforeEach, afterEach, test} = require('@jest/globals');
+const CodeSystem = require('../../../fhir/classes/4_0_0/resources/codeSystem');
 
 describe('CodeSystem Tests', () => {
     beforeEach(async () => {
@@ -31,10 +33,56 @@ describe('CodeSystem Tests', () => {
             // ACT & ASSERT
             // search by token system and code and make sure we get the right CodeSystem back
             resp = await request
-                .get('/4_0_0/CodeSystem/?_bundle=1&[write_query_here]')
+                .get('/4_0_0/CodeSystem/?_bundle=1&id=medline-loinc-labs')
                 .set(getHeaders());
             // noinspection JSUnresolvedFunction
             expect(resp).toHaveResponse(expectedCodeSystemResources);
+        });
+        test('concurrency_issue works with databaseUpdateManager', async () => {
+            await createTestRequest();
+            /**
+             * @type {SimpleContainer}
+             */
+            const container = getTestContainer();
+
+            /**
+             * @type {DatabaseUpdateFactory}
+             */
+            const databaseUpdateFactory = container.databaseUpdateFactory;
+            expect(databaseUpdateFactory).toBeDefined();
+
+            /**
+             * @type {DatabaseUpdateManager}
+             */
+            const databaseUpdateManager = databaseUpdateFactory.createDatabaseUpdateManager({
+                resourceType: 'CodeSystem',
+                base_version: '4_0_0'
+            });
+
+            for (const codeSystem of codesystem1Resource){
+                await databaseUpdateManager.replaceOneAsync({doc: new CodeSystem(codeSystem)});
+            }
+
+            /**
+             * @type {DatabaseQueryFactory}
+             */
+            const databaseQueryFactory = container.databaseQueryFactory;
+            expect(databaseQueryFactory).toBeDefined();
+
+            const databaseQueryManager = databaseQueryFactory.createQuery({
+                resourceType: 'CodeSystem',
+                base_version: '4_0_0'
+            });
+            /**
+             * @type {Resource|null}
+             */
+            const resource = await databaseQueryManager.findOneAsync(
+                {
+                    query: {'id': 'medline-loinc-labs'}
+                }
+            );
+            resource.meta.lastUpdated = null;
+            expect(resource.toJSON()).toStrictEqual(expectedCodeSystemsFromDatabase);
         });
     });
 });
