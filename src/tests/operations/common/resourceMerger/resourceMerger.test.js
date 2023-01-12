8369d63a9d36b70bebc03aec9b929999f3cbdca8
imranq2 <imranq2@hotmail.com>
2023-01-11 18:28:48 -0800

    add test to repro the problem

--- src/tests/operations/common/resourceMerger/resourceMerger.test.js
+++ src/tests/operations/common/resourceMerger/resourceMerger.test.js
@@ -11,6 +11,8 @@ const expectedPersonResource = require('./fixtures/expected/expected_person.json
 
 const {ResourceMerger} = require('../../../../operations/common/resourceMerger');
 const Person = require('../../../../fhir/classes/4_0_0/resources/person');
+const deepmerge = require('deepmerge');
+const {mergeObject} = require('../../../../utils/mergeHelper');
 
 describe('ResourceMerger Tests', () => {
     beforeEach(async () => {
@@ -31,13 +33,29 @@ describe('ResourceMerger Tests', () => {
             const resourceMerger = new ResourceMerger({
                 preSaveManager: new PreSaveManager({preSaveHandlers: []})
             });
+            const currentResource = new Person(person1Resource);
+            const resourceToMerge = new Person(personMergeResource);
+
+            // first try merge of the raw json
+            let mergedObject = deepmerge(person1Resource, personMergeResource);
+            expect(mergedObject.gender).toStrictEqual('male');
+
+            // then try merge of the resources
+            mergedObject = deepmerge(currentResource.toJSON(), resourceToMerge.toJSON());
+            expect(mergedObject.gender).toStrictEqual('male');
+
+            // now try merge with our options
+            mergedObject = mergeObject(currentResource.toJSON(), resourceToMerge.toJSON());
+            expect(mergedObject.gender).toStrictEqual('male');
+
+
             /**
              * @type {{updatedResource: (Resource|null), patches: (MergePatchEntry[]|null)}}
              */
             const result = await resourceMerger.mergeResourceAsync(
                 {
-                    currentResource: new Person(person1Resource),
-                    resourceToMerge: new Person(personMergeResource),
+                    currentResource: currentResource,
+                    resourceToMerge: resourceToMerge,
                 }
             );
             expect(result.updatedResource).toStrictEqual(expectedPersonResource);
