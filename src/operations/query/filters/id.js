2ac8f273a67f5d5587adcab468d51c9f8f6709af
imranq2 <imranq2@hotmail.com>
2023-01-15 11:03:07 -0800

    add code for uuid

--- src/operations/query/filters/id.js
+++ src/operations/query/filters/id.js
@@ -1,5 +1,7 @@
 const {isUuid} = require('../../../utils/uid.util');
 
+const uuidFieldName = '_uuid';
+
 /**
  * Filters by id
  * https://www.hl7.org/fhir/search.html#id
@@ -13,29 +15,68 @@ function filterById({queryParameterValue, propertyObj, columns}) {
      * @type {Object[]}
      */
     const and_segments = [];
+    if (queryParameterValue === undefined || queryParameterValue === null || queryParameterValue.length === 0) {
+        return and_segments;
+    }
     /**
      * @type {string}
      */
     let field = propertyObj.field;
 
+    /**
+     * @param {string[]} idAndUuidList
+     */
+    function getIdFilterForArray({idAndUuidList}) {
+        // see which ids are uuid
+        /**
+         * @type {string[]}
+         */
+        const uuidList = idAndUuidList.filter(i => isUuid(i));
+        /**
+         * @type {string[]}
+         */
+        const idList = idAndUuidList.filter(i => !uuidList.includes(i));
+
+        if (idList.length > 0 && uuidList.length > 0) {
+            and_segments.push({
+                $or: [
+                    {
+                        [`${field}`]: {
+                            $in: idList,
+                        }
+                    },
+                    {
+                        [`${uuidFieldName}`]: {
+                            $in: uuidList,
+                        }
+                    }
+                ]
+            });
+        } else if (idList.length > 0) {
+            and_segments.push({
+                [`${field}`]: {
+                    $in: idList,
+                },
+            });
+        } else if (uuidList.length > 0) {
+            and_segments.push({
+                [`${uuidFieldName}`]: {
+                    $in: uuidList,
+                },
+            });
+        }
+    }
+
     if (Array.isArray(queryParameterValue)) {
+        getIdFilterForArray({idAndUuidList: queryParameterValue});
         // if array is passed then check in array
-        and_segments.push({
-            [`${field}`]: {
-                $in: queryParameterValue,
-            },
-        });
     } else if (queryParameterValue.includes(',')) {
         // see if this is a comma separated list
         const value_list = queryParameterValue.split(',');
-        and_segments.push({
-            [`${field}`]: {
-                $in: value_list,
-            },
-        });
+        getIdFilterForArray({idAndUuidList: value_list});
     } else {
         if (isUuid(queryParameterValue)) {
-            field = '_uuid';
+            field = uuidFieldName;
         }
         // single value is passed
         and_segments.push({
