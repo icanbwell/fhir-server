1cb72a828b87a568bab27874f700556d23d7e606
Mintu Sah <mintu.sah@icanbwell.com>
2023-01-23 17:25:44 +0530

    CVP-1623 updated the testcases

--- src/app.test.js
+++ src/app.test.js
@@ -7,21 +7,23 @@ const {Kafka} = require('kafkajs');
 const { describe, expect, test } = require('@jest/globals');
 const env = require('var');
 
+// Mocking connect and disconnect methods of prooducer class
+const mockProducerMethods = {
+    connect: jest.fn().mockImplementation(() => { return Promise.resolve('connected');}),
+    disconnect: jest.fn().mockImplementation(() => { return Promise.resolve('disconnected');})
+}
+
+// Mocking the producer method of Kafka class
+const mockProducer = {
+    producer: jest.fn(() => mockProducerMethods)
+};
+
 // Mocking kafkajs library
 jest.mock('kafkajs', () => {
-    //Mock the connect, disconnect method of the producer
+    //Mock the Kafka class 
     return {
         __esmodule: true,
-        Kafka: jest.fn().mockImplementation(() => {
-            return {
-                producer: jest.fn().mockImplementationOnce(() => {
-                    return {
-                        connect: jest.fn().mockImplementationOnce(() => { return Promise.resolve('connected');}),
-                        disconnect: jest.fn().mockImplementationOnce(() => { return Promise.resolve('disconnected');})
-                    };
-                })
-            };
-        })
+        Kafka: jest.fn(() => mockProducer)
     };
 });
 
@@ -51,25 +53,36 @@ describe('#app', () => {
         expect(response.status).toBe(200);
         // If Kafka is healthy status returned is ok
         expect(response.body).toMatchObject({ status: 'ok' });
+        // Kafka class has been called
         expect(Kafka).toHaveBeenCalledTimes(1);
+        // A connection request is being made
+        expect(mockProducerMethods.connect).toHaveBeenCalledTimes(1);
 
         // Kafka Health Connection should not be checked within an interval of 30 seconds
+        // Clearing all the mocks
+        jest.clearAllMocks()
         request = await createTestRequest();
         response = await request.get('/health');
         expect(response.status).toBe(200);
         // If Kafka is healthy status returned is ok
         expect(response.body).toMatchObject({ status: 'ok' });
-        expect(Kafka).toHaveBeenCalledTimes(1);
+        // Since the request has been made with 30 seconds of the previous request. Kafka class won't be called 
+        expect(Kafka).toHaveBeenCalledTimes(0);
+        expect(mockProducerMethods.connect).toHaveBeenCalledTimes(0);
 
         // Kafka Health Connection should be checked but all the values should be stored in variables.
-        // So kafka shouldn't be called.
+        // So kafka shouldn't be called, and restoring all the mocks
+        jest.restoreAllMocks();
         jest.advanceTimersByTime(40000);
         request = await createTestRequest();
         response = await request.get('/health');
         expect(response.status).toBe(200);
         // If Kafka is healthy status returned is ok
         expect(response.body).toMatchObject({ status: 'ok' });
-        expect(Kafka).toHaveBeenCalledTimes(1);
+        // Since the KafkaClient is being stored in a variable. Kafka class won't be called.
+        expect(Kafka).toHaveBeenCalledTimes(0);
+        // Ensuring the connect() is being called.
+        expect(mockProducerMethods.connect).toHaveBeenCalledTimes(1);
 
         env.ENABLE_EVENTS_KAFKA = enableKafkaEvents;
     });
