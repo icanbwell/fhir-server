9e212e050f21b309b5666b0cd677f1122efd42f3
Mintu Sah <mintu.sah@icanbwell.com>
2023-01-23 14:46:54 +0530

    CVP-1623 updated testcase to use 'var' to set env variables

--- src/app.test.js
+++ src/app.test.js
@@ -2,8 +2,26 @@
  * simple test for the app
  */
 const { createTestRequest } = require('./tests/common');
-
 const { describe, expect, test } = require('@jest/globals');
+const env = require('var');
+
+// Mocking kafkajs library
+jest.mock('kafkajs', () => {
+    //Mock the connect, disconnect method of the producer
+    return {
+        __esmodule: true,
+        Kafka: jest.fn().mockImplementation(() => {
+            return {
+                producer: jest.fn().mockImplementationOnce(() => {
+                    return {
+                        connect: jest.fn().mockImplementationOnce(() => { return Promise.resolve('connected');}),
+                        disconnect: jest.fn().mockImplementationOnce(() => { return Promise.resolve('disconnected');})
+                    };
+                })
+            };
+        })
+    };
+});
 
 describe('#app', () => {
     test('it should startup and return health check status ok', async () => {
@@ -14,27 +32,15 @@ describe('#app', () => {
     });
 
     test('it should startup check kafka health and return health check status ok', async () => {
-        // Mocking kafkajs library
-        jest.mock('kafkajs', () => {
-            //Mock the connect, disconnect method of the producer
-            return {
-                producer: jest.fn().mockImplementationOnce(() => {
-                    return {
-                        connect: jest.fn().mockImplementationOnce(() => { return Promise.resolve('connected');}),
-                        disconnect: jest.fn().mockImplementationOnce(() => { return Promise.resolve('disconnected');})
-                    };
-                })
-            };
-        });
-
+        const enableKafkaEvents = env.ENABLE_EVENTS_KAFKA;
         // Setting ENABLE_EVENTS_KAFKA to '1' which implies kafka is being used.
-        process.env.ENABLE_EVENTS_KAFKA = '1';
+        env.ENABLE_EVENTS_KAFKA = '1';
 
         let request = await createTestRequest();
         let response = await request.get('/health');
         expect(response.status).toBe(200);
         // If Kafka is healthy status returned is ok
         expect(response.body).toMatchObject({ status: 'ok' });
-        process.env.ENABLE_EVENTS_KAFKA = '0';
+        env.ENABLE_EVENTS_KAFKA = enableKafkaEvents;
     });
 });
