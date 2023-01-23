05d3233f7a422c928d42310cb83d6a371871136d
Mintu Sah <mintu.sah@icanbwell.com>
2023-01-23 17:30:44 +0530

    CVP-1623 linting fixes

--- src/app.test.js
+++ src/app.test.js
@@ -3,27 +3,31 @@
  */
 const { createTestRequest } = require('./tests/common');
 
-const {Kafka} = require('kafkajs');
+const { Kafka } = require('kafkajs');
 const { describe, expect, test } = require('@jest/globals');
 const env = require('var');
 
 // Mocking connect and disconnect methods of prooducer class
 const mockProducerMethods = {
-    connect: jest.fn().mockImplementation(() => { return Promise.resolve('connected');}),
-    disconnect: jest.fn().mockImplementation(() => { return Promise.resolve('disconnected');})
-}
+    connect: jest.fn().mockImplementation(() => {
+        return Promise.resolve('connected');
+    }),
+    disconnect: jest.fn().mockImplementation(() => {
+        return Promise.resolve('disconnected');
+    }),
+};
 
 // Mocking the producer method of Kafka class
 const mockProducer = {
-    producer: jest.fn(() => mockProducerMethods)
+    producer: jest.fn(() => mockProducerMethods),
 };
 
 // Mocking kafkajs library
 jest.mock('kafkajs', () => {
-    //Mock the Kafka class 
+    //Mock the Kafka class
     return {
         __esmodule: true,
-        Kafka: jest.fn(() => mockProducer)
+        Kafka: jest.fn(() => mockProducer),
     };
 });
 
@@ -60,13 +64,13 @@ describe('#app', () => {
 
         // Kafka Health Connection should not be checked within an interval of 30 seconds
         // Clearing all the mocks
-        jest.clearAllMocks()
+        jest.clearAllMocks();
         request = await createTestRequest();
         response = await request.get('/health');
         expect(response.status).toBe(200);
         // If Kafka is healthy status returned is ok
         expect(response.body).toMatchObject({ status: 'ok' });
-        // Since the request has been made with 30 seconds of the previous request. Kafka class won't be called 
+        // Since the request has been made with 30 seconds of the previous request. Kafka class won't be called
         expect(Kafka).toHaveBeenCalledTimes(0);
         expect(mockProducerMethods.connect).toHaveBeenCalledTimes(0);
 
