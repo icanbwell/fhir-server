1014d501433f418f9087d4af7a57996e01a29a6e
imranq2 <imranq2@hotmail.com>
2023-01-30 08:12:03 -0800

    fix tests

--- src/operations/query/r4.js
+++ src/operations/query/r4.js
@@ -262,6 +262,7 @@ class R4SearchQueryCreator {
      * @return {import('mongodb').Filter<import('mongodb').DefaultSchema>}
      */
     simplifyFilter({filter}) {
+        // simplify $or
         if (filter.$or && filter.$or.length > 1) {
             filter.$or = removeDuplicatesWithLambda(filter.$or,
                 (a, b) => JSON.stringify(a) === JSON.stringify(b)
@@ -273,6 +274,16 @@ class R4SearchQueryCreator {
         if (filter.$or && filter.$or.length === 1) {
             filter = filter.$or[0];
         }
+        // simplify $nor
+        if (filter.$nor && filter.$nor.length > 1) {
+            filter.$nor = removeDuplicatesWithLambda(filter.$nor,
+                (a, b) => JSON.stringify(a) === JSON.stringify(b)
+            );
+        }
+        if (filter.$nor && filter.$nor.length > 0) {
+            filter.$nor = filter.$nor.map(f => this.simplifyFilter({filter: f}));
+        }
+        // simplify $and
         if (filter.$and && filter.$and.length > 1) {
             filter.$and = removeDuplicatesWithLambda(filter.$and,
                 (a, b) => JSON.stringify(a) === JSON.stringify(b)
@@ -284,6 +295,7 @@ class R4SearchQueryCreator {
         if (filter.$and && filter.$and.length === 1) {
             filter = filter.$and[0];
         }
+        // simplify $in
         if (filter.$in && filter.$in.length === 1) {
             filter = filter.$in[0];
         }
