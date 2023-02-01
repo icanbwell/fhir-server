2c87dbc10188fac96b74d04aa6931a635648f53e
Sunny Singhal <sunny.singhal@icanbwell.com>
2023-02-01 16:22:40 +0530

    CVP-1668: Added get CodeSystem codes graphql query for optimized mongo query

--- src/graphql/v2/dataSource.js
+++ src/graphql/v2/dataSource.js
@@ -320,9 +320,10 @@ class FhirDataSource extends DataSource {
      * @param {GraphQLContext} context
      * @param {Object} info
      * @param {string} resourceType
+     * @param {boolean} useAggregationPipeline
      * @return {Promise<Bundle>}
      */
-    async getResourcesBundle(parent, args, context, info, resourceType) {
+    async getResourcesBundle(parent, args, context, info, resourceType, useAggregationPipeline = false) {
         this.createDataLoader(args);
         // https://www.apollographql.com/blog/graphql/filtering/how-to-search-and-filter-results-with-graphql/
 
@@ -334,7 +335,8 @@ class FhirDataSource extends DataSource {
                     _bundle: '1',
                     ...args
                 },
-                resourceType
+                resourceType,
+                useAggregationPipeline
             }
         );
         if (bundle.meta) {
