8780b3941d7ed0924646890c2e5c1478b966a8b6
imranq2 <imranq2@hotmail.com>
2023-02-05 17:59:35 -0800

    fix prefer:global_id

--- src/enrich/providers/globalIdEnrichmentProvider.js
+++ src/enrich/providers/globalIdEnrichmentProvider.js
@@ -3,6 +3,7 @@ const {assertTypeEquals} = require('../../utils/assertType');
 const {DatabaseQueryFactory} = require('../../dataLayer/databaseQueryFactory');
 const {isUuid} = require('../../utils/uid.util');
 const {isTrue} = require('../../utils/isTrue');
+const {IdentifierSystem} = require('../../utils/identifierSystem');
 
 /**
  * Abstract base class for an enrichment provider.  Inherit from this to create a new enrichment provider
@@ -31,18 +32,66 @@ class GlobalIdEnrichmentProvider extends EnrichmentProvider {
      */
     // eslint-disable-next-line no-unused-vars
     async enrichAsync({resources, parsedArgs}) {
-        if (parsedArgs.headers && isTrue(parsedArgs['prefer:global_id'])) {
-            for (const resource of resources) {
-                if (resource.id && !isUuid(resource.id)) {
-                    const uuid = resource._uuid;
-                    if (uuid) {
-                        resource.id = uuid;
+        if (parsedArgs.headers && parsedArgs.headers['prefer']) {
+            /**
+             * @type {string}
+             */
+            const preferHeader = parsedArgs.headers['prefer'];
+            const parts = preferHeader.split('=');
+            if (parts[0] === 'global_id' && parts.slice(-1)[0] === 'true') {
+                for (const resource of resources) {
+                    if (resource.id && !isUuid(resource.id)) {
+                        const uuid = resource._uuid;
+                        if (uuid) {
+                            resource.id = uuid;
+                        }
                     }
+                    // update references
+                    resource.updateReferences(
+                        {
+                            fnUpdateReference: (reference) => this.updateReference(
+                                {
+                                    reference
+                                }
+                            )
+                        }
+                    );
                 }
             }
         }
 
         return resources;
+    }
+
+    /**
+     * updates references
+     * @param {Reference} reference
+     * @return {Reference}
+     */
+    updateReference({reference}) {
+        if (reference.reference) {
+            const parts = reference.reference.split('/');
+            const id = parts.slice(-1)[0];
+            const resourceType = parts[0];
+            if (!isUuid(id) && reference.extension && reference.extension.length > 0) {
+                /**
+                 * @type {Extension|undefined}
+                 */
+                const uuidExtension = reference.extension.find(e => e.url === IdentifierSystem.uuid);
+                if (uuidExtension) {
+                    /**
+                     * @type {string|undefined}
+                     */
+                    const uuid = uuidExtension.valueString;
+                    if (resourceType) {
+                        reference.reference = `${resourceType}/${uuid}`;
+                    } else {
+                        reference.reference = uuid;
+                    }
+                }
+            }
+        }
+        return reference;
     }
 
     /**
