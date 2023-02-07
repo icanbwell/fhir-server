a5041f4d23f3caf5d049331e9734eae5b4e71570
imranq2 <imranq2@hotmail.com>
2023-02-07 11:32:48 -0800

    add test for references

--- src/tests/global_id/search_prefer_global_id/search_prefer_global_id.test.js
+++ src/tests/global_id/search_prefer_global_id/search_prefer_global_id.test.js
@@ -89,5 +89,28 @@ describe('Observation Tests', () => {
                 expect(referenceId).toStrictEqual(expectedReferenceId);
             }
         });
+        test('search_prefer_global_id works with reference', async () => {
+            const request = await createTestRequest();
+            // ARRANGE
+            // add the resources to FHIR server
+            let resp = await request
+                .post('/4_0_0/Observation/1/$merge?validate=true')
+                .send(observation1Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            // ACT & ASSERT
+            // search by token system and code and make sure we get the right Observation back
+            const headers = getHeaders();
+            headers['Prefer'] = 'global_id=true';
+
+            const patientId = generateUUIDv5('2354|A');
+            resp = await request
+                .get(`/4_0_0/Observation/?_bundle=1&patient=${patientId}&_debug=1`)
+                .set(headers);
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResponse(expectedObservationResources);
+        });
     });
 });
