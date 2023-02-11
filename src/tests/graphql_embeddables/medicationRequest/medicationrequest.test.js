713ceddf93499b963518dde7a1bcd851e8c815ef
imranq2 <imranq2@hotmail.com>
2023-02-11 15:21:03 -0800

    add graphql test

--- src/tests/graphql_embeddables/medicationRequest/medicationrequest.test.js
+++ src/tests/graphql_embeddables/medicationRequest/medicationrequest.test.js
@@ -2,6 +2,11 @@
 const medicationrequest1Resource = require('./fixtures/MedicationRequest/medicationrequest1.json');
 const medicationrequest2Resource = require('./fixtures/MedicationRequest/medicationrequest2.json');
 const medicationrequestOtherPatientResource = require('./fixtures/MedicationRequest/medicationrequest_other_patient.json');
+
+const medication1Resource = require('./fixtures/Medication/medication1.json');
+const medicationDispense1Resource = require('./fixtures/MedicationDispense/medicationdispense1.json');
+const medicationStatement1Resource = require('./fixtures/MedicationStatement/medicationstatement1.json');
+
 const patientBundleResource = require('./fixtures/Patient/patient1.json');
 const personBundleResource = require('./fixtures/Person/person1.json');
 
@@ -54,6 +59,27 @@ describe('GraphQL MedicationRequest Tests', () => {
             resp = await request
                 .post('/4_0_0/MedicationRequest/1/$merge?validate=true')
                 .send(medicationrequestOtherPatientResource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            resp = await request
+                .post('/4_0_0/Medication/1/$merge?validate=true')
+                .send(medication1Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            resp = await request
+                .post('/4_0_0/MedicationDispense/1/$merge?validate=true')
+                .send(medicationDispense1Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            resp = await request
+                .post('/4_0_0/MedicationStatement/1/$merge?validate=true')
+                .send(medicationStatement1Resource)
                 .set(getHeaders());
             // noinspection JSUnresolvedFunction
             expect(resp).toHaveMergeResponse({created: true});
