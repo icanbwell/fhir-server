07ee1d008a30780109e9d896c348e8a082153500
imranq2 <imranq2@hotmail.com>
2023-02-11 13:53:36 -0800

    add file templates

--- .idea/fileTemplates/GraphqlTest.js
+++ .idea/fileTemplates/GraphqlTest.js
@@ -1,26 +1,30 @@
+#set($ResourceNameLower = $ResourceName.toLowerCase())
+#set($dollar = "$")
+
 // test file
-const condition1Resource = require('./fixtures/Condition/condition1.json');
+const ${ResourceNameLower}1Resource = require('./fixtures/${ResourceName}/${ResourceNameLower}1.json');
 const patientBundleResource = require('./fixtures/Patient/patient1.json');
 const personBundleResource = require('./fixtures/Person/person1.json');
 
 // expected
-const expectedConditionResources = require('./fixtures/expected/expected_condition.json');
+const expected${ResourceName}Resources = require('./fixtures/expected/expected_${ResourceNameLower}.json');
 
 const fs = require('fs');
 const path = require('path');
 
 // eslint-disable-next-line security/detect-non-literal-fs-filename
-const conditionQuery = fs.readFileSync(path.resolve(__dirname, './fixtures/query.graphql'), 'utf8');
+const ${ResourceNameLower}Query = fs.readFileSync(path.resolve(__dirname, './fixtures/query.graphql'), 'utf8');
 
 const {
     commonBeforeEach,
     commonAfterEach,
     getHeaders,
-    createTestRequest, getGraphQLHeadersWithPerson,
+    getGraphQLHeadersWithPerson,
+    createTestRequest,
 } = require('../../common');
-const {describe, beforeEach, afterEach, test} = require('@jest/globals');
+const { describe, beforeEach, afterEach, test } = require('@jest/globals');
 
-describe('GraphQL Condition Tests', () => {
+describe('GraphQL ${ResourceName} Tests', () => {
     beforeEach(async () => {
         await commonBeforeEach();
     });
@@ -29,33 +33,33 @@ describe('GraphQL Condition Tests', () => {
         await commonAfterEach();
     });
 
-    describe('GraphQL Condition condition Tests', () => {
-        test('GraphQL condition works', async () => {
+    describe('GraphQL ${ResourceName} ${NAME} Tests', () => {
+        test('GraphQL ${NAME} works', async () => {
             const request = await createTestRequest();
             // ARRANGE
             // add the resources to FHIR server
             let resp = await request
-                .post('/4_0_0/Condition/1/$merge?validate=true')
-                .send(condition1Resource)
+                .post('/4_0_0/${ResourceName}/1/${dollar}merge?validate=true')
+                .send(${ResourceNameLower}1Resource)
                 .set(getHeaders());
             // noinspection JSUnresolvedFunction
-            expect(resp).toHaveMergeResponse({created: true});
-
-            resp = await request
-                .post('/4_0_0/Patient/1/$merge?validate=true')
+            expect(resp).toHaveMergeResponse({ created: true });
+            
+           resp = await request
+                .post('/4_0_0/Patient/1/${dollar}merge?validate=true')
                 .send(patientBundleResource)
                 .set(getHeaders());
             // noinspection JSUnresolvedFunction
             expect(resp).toHaveMergeResponse({created: true});
 
             resp = await request
-                .post('/4_0_0/Person/1/$merge?validate=true')
+                .post('/4_0_0/Person/1/${dollar}merge?validate=true')
                 .send(personBundleResource)
                 .set(getHeaders());
             // noinspection JSUnresolvedFunction
             expect(resp).toHaveMergeResponse({created: true});
-
-            const graphqlQueryText = conditionQuery.replace(/\\n/g, '');
+            
+            const graphqlQueryText = ${ResourceNameLower}Query.replace(/\\n/g, '');
             // ACT & ASSERT
             resp = await request
                 // .get('/graphql/?query=' + graphqlQueryText)
@@ -63,13 +67,15 @@ describe('GraphQL Condition Tests', () => {
                 .post('/graphqlv2')
                 .send({
                     operationName: null,
-                    variables: {},
+                    variables: {
+                        FHIR_DEFAULT_COUNT: 10
+                    },
                     query: graphqlQueryText,
                 })
                 .set(getGraphQLHeadersWithPerson('79e59046-ffc7-4c41-9819-c8ef83275454'));
 
             // noinspection JSUnresolvedFunction
-            expect(resp).toHaveResponse(expectedConditionResources);
+            expect(resp).toHaveResponse(expected${ResourceName}Resources);
         });
     });
 });
