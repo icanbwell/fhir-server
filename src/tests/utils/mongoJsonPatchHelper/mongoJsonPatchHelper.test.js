22d1e1331cddcd4903f52bf8514e4528912eefc1
imranq2 <imranq2@hotmail.com>
2023-03-04 23:12:10 -0800

    add mongoJsonPatchHelper.js

--- src/tests/utils/mongoJsonPatchHelper/mongoJsonPatchHelper.test.js
+++ src/tests/utils/mongoJsonPatchHelper/mongoJsonPatchHelper.test.js
@@ -65,7 +65,7 @@ describe('mongoJsonPatchHelper Tests', () => {
              * @type {import('mongodb').Collection<import('mongodb').Document>}
              */
             const collection = db.collection(`${resourceType}_${base_version}`);
-            await collection.insertOne(doc1);
+            await collection.insertOne(doc1.toJSONInternal());
 
             const doc2 = new Person({
                 'resourceType': 'Person',
@@ -180,6 +180,89 @@ describe('mongoJsonPatchHelper Tests', () => {
             ];
             const result = await collection.bulkWrite(operations);
             expect(result.modifiedCount).toStrictEqual(1);
+
+            const docFromDatabase = await collection.findOne({}, {projection: {_id: 0}});
+            expect(docFromDatabase).toStrictEqual({
+                '_sourceAssigningAuthority': 'myAccess',
+                '_sourceId': '1',
+                '_uuid': '87ec3599-51e3-510c-9bf4-537608fbaf7e',
+                'id': '1',
+                'identifier': [
+                    {
+                        'id': 'sourceId',
+                        'system': 'https://www.icanbwell.com/sourceId',
+                        'value': '1'
+                    },
+                    {
+                        'id': 'uuid',
+                        'system': 'https://www.icanbwell.com/uuid',
+                        'value': '87ec3599-51e3-510c-9bf4-537608fbaf7e'
+                    }
+                ],
+                'link': [
+                    {
+                        'target': {
+                            '_sourceAssigningAuthority': 'myAccess',
+                            '_sourceId': 'Patient/2',
+                            '_uuid': 'Patient/413ed4ad-0c9c-584f-a9b5-a3cb42aa036e',
+                            'extension': [
+                                {
+                                    'id': 'sourceId',
+                                    'url': 'https://www.icanbwell.com/sourceId',
+                                    'valueString': 'Patient/2'
+                                },
+                                {
+                                    'id': 'uuid',
+                                    'url': 'https://www.icanbwell.com/uuid',
+                                    'valueString': 'Patient/413ed4ad-0c9c-584f-a9b5-a3cb42aa036e'
+                                },
+                                {
+                                    'id': 'sourceAssigningAuthority',
+                                    'url': 'https://www.icanbwell.com/sourceAssigningAuthority',
+                                    'valueString': 'myAccess'
+                                }
+                            ],
+                            'reference': 'Patient/2'
+                        }
+                    },
+                    {
+                        'target': {
+                            'extension': [
+                                {
+                                    'id': 'sourceId',
+                                    'url': 'https://www.icanbwell.com/sourceId',
+                                    'valueString': 'Patient/3'
+                                },
+                                {
+                                    'id': 'uuid',
+                                    'url': 'https://www.icanbwell.com/uuid',
+                                    'valueString': 'Patient/21cd2633-d630-55f4-9cd9-dc1282bd199e'
+                                },
+                                {
+                                    'id': 'sourceAssigningAuthority',
+                                    'url': 'https://www.icanbwell.com/sourceAssigningAuthority',
+                                    'valueString': 'myAccess'
+                                }
+                            ],
+                            'reference': 'Patient/3'
+                        }
+                    }
+                ],
+                'meta': {
+                    'security': [
+                        {
+                            'code': 'myAccess',
+                            'system': 'https://www.icanbwell.com/owner'
+                        },
+                        {
+                            'code': 'myAccess',
+                            'system': 'https://www.icanbwell.com/sourceAssigningAuthority'
+                        }
+                    ],
+                    'versionId': '1'
+                },
+                'resourceType': 'Person'
+            });
         });
     });
 });
