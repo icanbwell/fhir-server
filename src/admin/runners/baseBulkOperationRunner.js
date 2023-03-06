3305ddaa48c8742633b612e224e11232478a2b22
imranq2 <imranq2@hotmail.com>
2023-03-06 10:06:03 -0800

    add transactionOptions

--- src/admin/runners/baseBulkOperationRunner.js
+++ src/admin/runners/baseBulkOperationRunner.js
@@ -241,7 +241,7 @@ class BaseBulkOperationRunner extends BaseScriptRunner {
             skip
         }) {
         const maxTimeMS = 20 * 60 * 60 * 1000;
-        const numberOfSecondsBetweenSessionRefreshes = 30;
+        const numberOfSecondsBetweenSessionRefreshes = 10 * 60;
         let loopRetryNumber = 0;
         const maxLoopRetries = 5;
         let continueLoop = true;
