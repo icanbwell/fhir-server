b69333a736dfa828601381903b5e98eaae41bbf5
imranq2 <imranq2@hotmail.com>
2023-03-11 18:29:58 -0800

    improve mongoQuerySimplifier.js

--- src/operations/query/r4ArgsParser.js
+++ src/operations/query/r4ArgsParser.js
@@ -138,7 +138,7 @@ class R4ArgsParser {
                     queryParameter,
                     queryParameterValue: new QueryParameterValue({
                         value: queryParameterValue,
-                        operator: Array.isArray(queryParameterValue) ? 'and' : undefined
+                        operator: '$and'
                     }),
                     propertyObj,
                     modifiers
