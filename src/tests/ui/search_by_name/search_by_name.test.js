9f71ca6c160e5fd393226c3d4128919e147fcae3
imranq2 <imranq2@hotmail.com>
2023-03-14 15:00:55 -0700

    add test for parsing empty parameters

--- src/tests/ui/search_by_name/search_by_name.test.js
+++ src/tests/ui/search_by_name/search_by_name.test.js
@@ -44,9 +44,9 @@ describe('Patient UI Tests', () => {
             // noinspection JSUnresolvedFunction
             expect(resp).toHaveStatusOk();
 
-            logInfo('------- response Patient sorted ------------');
+            logInfo('------- response Patient sorted ------------', {});
             logInfo('', {resp});
-            logInfo('------- end response sort ------------');
+            logInfo('------- end response sort ------------', {});
             expect(resp.type).toStrictEqual('text/html');
             expect(resp.body).toStrictEqual({});
             expect(resp.text).not.toBeNull();
@@ -75,6 +75,49 @@ describe('Patient UI Tests', () => {
             resp = await request
                 .post('/4_0_0/Patient/_search')
                 .send(body)
+                .set(getHtmlHeadersWithForm());
+
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveStatusOk();
+
+            logInfo('------- response Patient sorted ------------', {});
+            logInfo('', {resp});
+            logInfo('------- end response sort ------------', {});
+            expect(resp.type).toStrictEqual('text/html');
+            expect(resp.body).toStrictEqual({});
+            expect(resp.text).not.toBeNull();
+            expect(resp.text).toMatch(new RegExp('SHYLA'));
+
+            const form = 'address%3Acontains=&address-city%3Acontains=&address-country%3Acontains=&address-postalcode%3Acontains=&address-state%3Acontains=&name%3Acontains=&phonetic%3Acontains=&_lastUpdated=&_lastUpdated=&given=SHYLA&family=PATIENT1&email=&_security=&id=&identifier=&_source%3Acontains=&identifier=&_getpagesoffset=&_sort=&_count=100';
+            resp = await request
+                .post('/4_0_0/Patient/_search')
+                .send(form)
+                .set(getHtmlHeadersWithForm());
+
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveStatusOk();
+
+            logInfo('------- response Patient sorted ------------', {});
+            logInfo('', {resp});
+            logInfo('------- end response sort ------------', {});
+            expect(resp.type).toStrictEqual('text/html');
+            expect(resp.body).toStrictEqual({});
+            expect(resp.text).not.toBeNull();
+            expect(resp.text).toMatch(new RegExp('SHYLA'));
+        });
+        test('search by single name works with form', async () => {
+            const request = await createTestRequest();
+            let resp = await request
+                .post('/4_0_0/Patient/1679033641/$merge?validate=true')
+                .send(patient1Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            const form = 'address%3Acontains=&address-city%3Acontains=&address-country%3Acontains=&address-postalcode%3Acontains=&address-state%3Acontains=&name%3Acontains=&phonetic%3Acontains=&_lastUpdated=&_lastUpdated=&given=SHYLA&family=PATIENT1&email=&_security=&id=&identifier=&_source%3Acontains=&identifier=&_getpagesoffset=&_sort=&_count=100';
+            resp = await request
+                .post('/4_0_0/Patient/_search')
+                .send(form)
                 .set(getHtmlHeadersWithForm());
 
             // noinspection JSUnresolvedFunction
