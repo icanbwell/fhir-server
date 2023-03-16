23fad9c9d90d46e68294c7cd5982bcf5e0324477
Mintu Sah <mintu.sah@icanbwell.com>
2023-03-16 22:59:18 +0530

    EFS-220 Created script to update documents in two different clusters.

---
+++ src/admin/runners/updateCollectionsRunner.js
@@ -0,0 +1,222 @@
+const env = require('var');
+const { assertTypeEquals } = require('../../utils/assertType');
+const moment = require('moment-timezone');
+const { MongoCollectionManager } = require('../../utils/mongoCollectionManager');
+const { MongoDatabaseManager } = require('../../utils/mongoDatabaseManager');
+const { AdminLogger } = require('../adminLogger');
+
+/**
+ * @classdesc Copies documents from one collection into the other collection in different clusters
+ */
+class UpdateCollectionsRunner {
+    /**
+     * Constructor
+     * @param {MongoDatabaseManager} mongoDatabaseManager
+     * @param {MongoCollectionManager} mongoCollectionManager
+     * @param {moment.Moment} updatedAfter
+     * @param {number} readBatchSize
+     * @param {number} writeBatchSize
+     * @param {Object|string|undefined} readOnlyCertainCollections
+     * @param {Object|string|undefined} excludeCollection
+     * @param {AdminLogger} adminLogger
+     */
+    constructor({
+        mongoDatabaseManager,
+        mongoCollectionManager,
+        updatedAfter,
+        readBatchSize,
+        writeBatchSize,
+        readOnlyCertainCollections,
+        excludeCollection,
+        adminLogger,
+    }) {
+        /**
+         * @type {moment.Moment}
+         */
+        this.updatedAfter = updatedAfter;
+        assertTypeEquals(updatedAfter, moment);
+
+        /**
+         * @type {number}
+         */
+        this.readBatchSize = readBatchSize;
+
+        /**
+         * @type {number}
+         */
+        this.writeBatchSize = writeBatchSize;
+
+        /**
+         * @type {object|string|undefined}
+         */
+        this.readOnlyCertainCollections = readOnlyCertainCollections;
+
+        /**
+         * @type {object|string|undefined}
+         */
+        this.excludeCollection = excludeCollection;
+
+        /**
+         * @type {MongoDatabaseManager}
+         */
+        this.mongoDatabaseManager = mongoDatabaseManager;
+        assertTypeEquals(mongoDatabaseManager, MongoDatabaseManager);
+
+        /**
+         * @type {MongoCollectionManager}
+         */
+        this.mongoCollectionManager = mongoCollectionManager;
+        assertTypeEquals(mongoCollectionManager, MongoCollectionManager);
+
+        /**
+         * @type {AdminLogger}
+         */
+        this.adminLogger = adminLogger;
+        assertTypeEquals(adminLogger, AdminLogger);
+    }
+
+    /**
+     * @description Creates config for the target cluster using connection string
+     * @returns {Object}
+     */
+    getTargetClusterConfig() {
+        // const mongoUrl = encodeURI(`mongodb+srv://${env.MONGO_USERNAME}:${env.MONGO_PASSWORD}@`)
+        // const db_name = "cl-dev-fhir-pl-0.vpsmx.mongodb.net"
+
+        const mongoUrl = encodeURI(`mongodb://localhost:27017/`);
+        const db_name = 'fhir';
+        const options = {
+            retryWrites: true,
+            w: 'majority',
+        };
+        return { connection: mongoUrl, db_name: db_name, options: options };
+    }
+
+    /**
+     * @description Creates config for the source cluster using connection string
+     * @returns {Object}
+     */
+    getSourceClusterConfig() {
+        // const mongoUrl = encodeURI(`mongodb+srv://${env.MONGO_USERNAME}:${env.MONGO_PASSWORD}`)
+        // const db_name = "@cl-dev-fhir-v3-pl-0.vpsmx.mongodb.net"
+
+        const mongoUrl = encodeURI(`mongodb://localhost:27017/`);
+        const db_name = 'fhir_v3';
+        const options = {
+            retryWrites: true,
+            w: 'majority',
+        };
+        return { connection: mongoUrl, db_name: db_name, options: options };
+    }
+    /**
+     * Runs a loop to process all the documents.
+     */
+    async processAsync() {
+        // Creating config specific to each cluster
+        const targetClusterConfig = this.getTargetClusterConfig();
+        const sourceClusterConfig = this.getSourceClusterConfig();
+        let targetClient, sourceClient;
+
+        try {
+            // Creating a connection between the cluster and the application
+            targetClient = await this.mongoDatabaseManager.createClientAsync(targetClusterConfig);
+            sourceClient = await this.mongoDatabaseManager.createClientAsync(sourceClusterConfig);
+
+            this.adminLogger.logInfo('Client connected successfully to both the clusters.');
+            // Createing a new db instance for both the clusters
+            const targetDatabase = targetClient.db(targetClusterConfig.db_name);
+            const sourceDatabase = sourceClient.db(sourceClusterConfig.db_name);
+
+            const sourceCollections = await this.mongoCollectionManager.getAllCollectionNames({
+                db: sourceDatabase,
+            });
+            this.adminLogger.logInfo(
+                `Total collections present in ${sourceClusterConfig.db_name}: ${sourceCollections.length}`
+            );
+            for (const collection of sourceCollections) {
+                this.adminLogger.logInfo(`========= Iterating through ${collection} =========`);
+                let updatedCount = 0;
+                let skippedCount = 0;
+
+                if (
+                    !this.readOnlyCertainCollections &&
+                    this.excludeCollection &&
+                    this.excludeCollection.includes(collection)
+                ) {
+                    // As the collection is to be excluded we move to the next collection
+                    this.adminLogger.logInfo(
+                        `${collection} is being excluded from further data transfer`
+                    );
+                    continue;
+                }
+                if (
+                    this.readOnlyCertainCollections &&
+                    !this.readOnlyCertainCollections.includes(collection)
+                ) {
+                    // As we need to iterate only a few collection we skip collections that are not present in the list.
+                    this.adminLogger.logInfo(
+                        `Omitting ${collection} as it is not present in the list of collections to be iterated`
+                    );
+                    continue;
+                }
+                const sourceDatabaseCollection = sourceDatabase.collection(collection);
+                const targetDatabaseCollection = targetDatabase.collection(collection);
+
+                // Projection is used so that we don't fetch _id. Thus preventing it from being updated while updating document.
+                // Returns a list of documents from sourceDatabaseCollection collection with specified batch size
+                const cursor = sourceDatabaseCollection.find().batchSize(this.readBatchSize);
+                while (await cursor.hasNext()) {
+                    const sourceDocument = await cursor.next();
+                    // Fetching document from active db having same id.
+                    const targetDocument = await targetDatabaseCollection.findOne({
+                        _id: sourceDocument._id,
+                    });
+                    // Skip target/source documents in which lastUpdated is not present.
+                    if (
+                        targetDocument?.meta?.lastUpdated === undefined ||
+                        sourceDocument?.meta?.lastUpdated === undefined
+                    ) {
+                        this.adminLogger.logInfo(
+                            `Document in ${collection} with id:${sourceDocument.id} skipped as the targetDocument or sourceDocument is missing lastUpdated`
+                        );
+                        continue;
+                    }
+                    //  Active (Source)db lastUpdated <  this.updatedAfter and targetDatabase lastUpdate > fhirDb lastUpdate
+                    if (
+                        targetDocument &&
+                        targetDocument.meta.lastUpdated <= this.updatedAfter &&
+                        targetDocument.meta.lastUpdated > sourceDocument.meta.lastUpdated
+                    ) {
+                        // The document already exists in fhir and has been updated more recently than updatedAfter
+                        this.adminLogger.logInfo(
+                            `Document in ${collection} with id:${sourceDocument.id} found in target db but omitting as lastUpdated > ${this.updatedAfter}`
+                        );
+                        skippedCount++;
+                        continue;
+                    }
+                    const result = await targetDatabaseCollection.updateOne(
+                        { _id: sourceDocument._id },
+                        {
+                            $set: sourceDocument,
+                        }
+                    );
+                    updatedCount += result.modifiedCount;
+                }
+                this.adminLogger.logInfo(
+                    `===== For ${collection} total updated documents: ${updatedCount} and total documents skipped: ${skippedCount} `
+                );
+            }
+        } catch (e) {
+            this.adminLogger.logError('ERROR', { error: e });
+        } finally {
+            await targetClient.close();
+            await sourceClient.close();
+            this.adminLogger.logInfo('Closed connectinon for both the cluster');
+            this.adminLogger.logInfo('Finished Script');
+        }
+    }
+}
+
+module.exports = {
+    UpdateCollectionsRunner,
+};
---
+++ src/admin/scripts/updateCollections.js
@@ -0,0 +1,75 @@
+// load config from .env.  Should be first thing so env vars are available to rest of the code
+const path = require('path');
+const dotenv = require('dotenv');
+const pathToEnv = path.resolve(__dirname, '.env');
+dotenv.config({
+    path: pathToEnv,
+});
+const { logInfo } = require('../../operations/common/logging');
+const { createContainer } = require('../../createContainer');
+const { CommandLineParser } = require('./commandLineParser');
+const moment = require('moment-timezone');
+const { UpdateCollectionsRunner } = require('../runners/updateCollectionsRunner.js');
+const { AdminLogger } = require('../adminLogger');
+
+/**
+ * main function
+ * @returns {Promise<void>}
+ */
+async function main() {
+    /**
+     * @type {Object}
+     */
+    const parameters = CommandLineParser.parseCommandLine();
+    const updatedAfter = parameters.updatedAfter
+        ? new Date(`${parameters.updatedAfter}T00:00:00Z`)
+        : new Date(2023, 3 - 1, 14);
+    const readBatchSize = parameters.readBatchSize || process.env.BULK_BUFFER_SIZE || 10000;
+    const writeBatchSize = parameters.writeBatchSize || process.env.BULK_BUFFER_SIZE || 10000;
+    const readOnlyCertainCollections = parameters.readOnlyCertainCollections
+        ? parameters.readOnlyCertainCollections.split(',')
+        : undefined;
+    const excludeCollection = parameters.excludeCollection
+        ? parameters.excludeCollection.split(',')
+        : undefined;
+    console.log(`Running script to update data with last_updated greater than ${updatedAfter}`);
+
+    // set up all the standard services in the container
+    const container = createContainer();
+
+    logInfo('Parameters', { parameters });
+    // now add our class
+    container.register(
+        'processUpdateFhirRunner',
+        (c) =>
+            new UpdateCollectionsRunner({
+                mongoDatabaseManager: c.mongoDatabaseManager,
+                mongoCollectionManager: c.mongoCollectionManager,
+                updatedAfter: moment.utc(updatedAfter),
+                readBatchSize,
+                writeBatchSize,
+                readOnlyCertainCollections,
+                excludeCollection,
+                adminLogger: new AdminLogger(),
+            })
+    );
+
+    /**
+     * @type {PartitionAuditEventRunner}
+     */
+    const processUpdateFhirRunner = container.processUpdateFhirRunner;
+    await processUpdateFhirRunner.processAsync();
+
+    console.log('Exiting process');
+    process.exit(0);
+}
+
+/**
+ * To run this:
+ * nvm use 18.14.2
+ * node src/admin/scripts/updateCollections.js --updatedAfter=2023-03-14 --readbatchSize=10000 --writeBatchSize=10000
+ * node src/admin/scripts/updateCollections.js --updatedAfter=2023-03-14 --readOnlyCertainCollections="value1,value2" --excludeCollection="valueX,valueY"
+ */
+main().catch((reason) => {
+    console.error(reason);
+});
