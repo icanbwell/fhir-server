a98e89ec71d01d3145b4f5a3a46af1bb7517c04c
Mintu Sah <mintu.sah@icanbwell.com>
2023-03-21 17:26:44 +0530

    EFS-220 updated logInfo and filtered collections if collection is provided.

--- src/admin/runners/updateCollectionsRunner.js
+++ src/admin/runners/updateCollectionsRunner.js
@@ -78,10 +78,9 @@ class UpdateCollectionsRunner {
      * @returns {Object}
      */
     getTargetClusterConfig() {
-        const mongoUrl = `mongodb+srv://${process.env.TARGET_CLUSTER_USERNAME}:${process.env.TARGET_CLUSTER_PASSWORD}@${process.env.TARGET_CLUSTER_MONGO_URL}`;
-        this.adminLogger.logInfo(`Connecting to target cluster with mongo url: ${mongoUrl}`);
+        const mongoUrl = encodeURI(`mongodb+srv://${process.env.TARGET_CLUSTER_USERNAME}:${process.env.TARGET_CLUSTER_PASSWORD}@${process.env.TARGET_CLUSTER_MONGO_URL}`);
         const db_name = process.env.TARGET_DB_NAME;
-        this.adminLogger.logInfo(`The target database to be used is: ${db_name}`);
+        this.adminLogger.logInfo(`Connecting to target cluster with mongo url: ${process.env.TARGET_CLUSTER_MONGO_URL} and db_name: ${db_name}`);
         const options = {
             retryWrites: true,
             w: 'majority',
@@ -94,16 +93,16 @@ class UpdateCollectionsRunner {
      * @returns {Object}
      */
     getSourceClusterConfig() {
-        const mongoUrl = `mongodb+srv://${process.env.SOURCE_CLUSTER_USERNAME}:${process.env.SOURCE_CLUSTER_PASSWORD}@${process.env.SOURCE_CLUSTER_MONGO_URL}`;
-        this.adminLogger.logInfo(`Connecting to source cluster with mongo url: ${mongoUrl}`);
+        const mongoUrl = encodeURI(`mongodb+srv://${process.env.SOURCE_CLUSTER_USERNAME}:${process.env.SOURCE_CLUSTER_PASSWORD}@${process.env.SOURCE_CLUSTER_MONGO_URL}`);
         const db_name = process.env.SOURCE_DB_NAME;
-        this.adminLogger.logInfo(`The source database to be used is: ${db_name}`);
+        this.adminLogger.logInfo(`Connecting to target cluster with mongo url: ${process.env.SOURCE_CLUSTER_MONGO_URL} and db_name: ${db_name}`);
         const options = {
             retryWrites: true,
             w: 'majority',
         };
         return { connection: mongoUrl, db_name: db_name, options: options };
     }
+
     /**
      * Runs a loop to process all the documents.
      */
@@ -135,11 +134,16 @@ class UpdateCollectionsRunner {
             const sourceDatabase = sourceClient.db(sourceClusterConfig.db_name);
 
             // Fetch all the collection names for the source database.
-            const sourceCollections = await this.mongoCollectionManager.getAllCollectionNames({
+            let sourceCollections = await this.mongoCollectionManager.getAllCollectionNames({
                 db: sourceDatabase,
             });
+
+            if (this.collections) {
+                // If collections is specified filter out the collections that needs to be iterated.
+                sourceCollections = sourceCollections.filter(collection => this.collections.includes(collection));
+            }
             this.adminLogger.logInfo(
-                `Total collections present in ${sourceClusterConfig.db_name}: ${sourceCollections.length}`
+                `Total filtered collections are ${sourceCollections}`
             );
 
             // Creating batches of collections depending on the concurrency parameter passed.
@@ -182,6 +186,8 @@ class UpdateCollectionsRunner {
                     // Fetching the collection from the database for both source and target
                     const sourceDatabaseCollection = sourceDatabase.collection(collection);
                     const targetDatabaseCollection = targetDatabase.collection(collection);
+
+                    // Add the extra data here.
 
                     // Cursor options. As we are also provide _idAbove we need to get results in sorted manner
                     const cursorOptions = {
