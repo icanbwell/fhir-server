f58c8d0feae98fb7674da225e0a03a6381b05483
Mintu Sah <mintu.sah@icanbwell.com>
2023-04-01 23:17:04 +0530

    EFS-238 Removed unused variables

--- src/config.js
+++ src/config.js
@@ -88,21 +88,15 @@ let auditEventReadOnlyMongoConfig;
 
 if (env.AUDIT_EVENT_ONLINE_ARCHIVE_CLUSTER_MONGO_URL) {
     let auditEventReadOnlyMongoUrl = env.AUDIT_EVENT_ONLINE_ARCHIVE_CLUSTER_MONGO_URL;
-    if (env.AUDIT_EVENT_ONLINE_ARCHIVE_MONGO_USERNAME !== undefined) {
+    if (env.AUDIT_EVENT_MONGO_USERNAME !== undefined) {
         auditEventReadOnlyMongoUrl = auditEventReadOnlyMongoUrl.replace(
             'mongodb://',
-            `mongodb://${env.AUDIT_EVENT_ONLINE_ARCHIVE_MONGO_USERNAME}:${env.AUDIT_EVENT_ONLINE_ARCHIVE_MONGO_PASSWORD}@`
-        );
-        auditEventReadOnlyMongoUrl = auditEventReadOnlyMongoUrl.replace(
-            'mongodb+srv://',
-            `mongodb+srv://${env.AUDIT_EVENT_ONLINE_ARCHIVE_MONGO_USERNAME}:${env.AUDIT_EVENT_ONLINE_ARCHIVE_MONGO_PASSWORD}@`
+            `mongodb://${env.AUDIT_EVENT_MONGO_USERNAME}:${env.AUDIT_EVENT_MONGO_PASSWORD}@`
         );
     }
-    // url-encode the url
-    auditEventReadOnlyMongoUrl = auditEventReadOnlyMongoUrl ? encodeURI(auditEventReadOnlyMongoUrl) : auditEventReadOnlyMongoUrl;
     auditEventReadOnlyMongoConfig = {
-        connection: auditEventReadOnlyMongoUrl,
-        db_name: String(env.AUDIT_EVENT_ONLINE_ARCHIVE_MONGO_DB_NAME),
+        connection: encodeURI(auditEventReadOnlyMongoUrl),
+        db_name: String(env.AUDIT_EVENT_MONGO_DB_NAME),
         options: options,
     };
 } else {
