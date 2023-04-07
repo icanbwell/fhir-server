f128afb2a42407fb9d669946a8d7824f3880e02e
Mintu Sah <mintu.sah@icanbwell.com>
2023-04-07 11:58:05 +0530

    EFS-252 update display

--- src/admin/scripts/fix_patient_security_tag.py
+++ src/admin/scripts/fix_patient_security_tag.py
@@ -87,7 +87,7 @@ for doc in person_docs:
                 print(doc_id, 'copied access to owner', access['code'])
                 owner['code'] = access['code']
                 if 'display' in owner:
-                    owner['owner'] = access['code']
+                    owner['display'] = access['code']
                 is_updated = True
             if access['code'] != sourceAssigningAuthority['code']:
                 print(doc_id, 'copied access to sourceAssigningAuthority', access['code'])
