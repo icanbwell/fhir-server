3718e95b21fa2ef39ca03a0195d3acfc86202d46
Mintu Sah <mintu.sah@icanbwell.com>
2023-04-10 18:35:50 +0530

    EFS-250 removed unused vars

--- src/admin/scripts/dbStats.js
+++ src/admin/scripts/dbStats.js
@@ -1,5 +1,3 @@
-// load config from .env.  Should be first thing so env vars are available to rest of the code
-const path = require('path');
 const { logInfo } = require('../../operations/common/logging');
 const { createContainer } = require('../../createContainer');
 const { DatabaseStats } = require('../runners/dbStatsRunner.js');
