928f72877193d7c26d78d178566046b3fa9c86fb
Sunny Singhal <sunny.singhal@icanbwell.com>
2023-04-12 14:49:23 +0530

    EFS-249 Add mongo Object ID check

---
+++ src/tests/utils/mongoIdValidator/mongoIdValidator.test.js
@@ -0,0 +1,12 @@
+const { isValidMongoObjectId } = require('../../../utils/mongoIdValidator');
+const { describe, expect, test } = require('@jest/globals');
+
+describe('mongoIdValidator Tests', () => {
+    describe('mongoIdValidator Tests', () => {
+        test('mongoIdValidator works', () => {
+            expect(isValidMongoObjectId('6307c87743c5f18242934e05')).toStrictEqual(true);
+            expect(isValidMongoObjectId('nppes-1891352894')).toStrictEqual(false);
+            expect(isValidMongoObjectId('toptoptoptop')).toStrictEqual(false);
+        });
+    });
+});
