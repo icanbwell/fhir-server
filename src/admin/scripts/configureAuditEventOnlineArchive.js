663b698e0f816961af632b776b89421d1c815eed
Mintu Sah <mintu.sah@icanbwell.com>
2023-04-14 14:58:36 +0530

    EFS-250 Updated admin cmd doc string

--- src/admin/scripts/configureAuditEventOnlineArchive.js
+++ src/admin/scripts/configureAuditEventOnlineArchive.js
@@ -39,11 +39,17 @@ async function main() {
 /**
  * To run this:
  * nvm use 18.14.2
-src/admin/runners/configureAuditEventOnlineArchiveRunner.js * required env variables
- * for audit event cluster - AUDIT_EVENT_MONGO_URL, AUDIT_EVENT_MONGO_DB_NAME
- * CREATE_AUDIT_EVENT_ONLINE_ARCHIVE_ENDPOINT, ONLINE_ARCHIVE_AUTHENTICATION_PUBLIC_KEY, ONLINE_ARCHIVE_AUTHENTICATION_PRIVATE_KEY
- * node src/admin/scripts/configureAuditEventOnlineArchive.js
- * node src/admin/scripts/configureAuditEventOnlineArchive.js --collections="AuditEvent_4_0_0_2023_09_09"
+ * npm install
+ * Create .env file in root directory with these variables
+ * AUDIT_EVENT_MONGO_URL=
+ * AUDIT_EVENT_MONGO_DB_NAME=
+ * AUDIT_EVENT_MONGO_USERNAME=
+ * AUDIT_EVENT_MONGO_PASSWORD=
+ * AUDIT_EVENT_ONLINE_ARCHIVE_MANAGEMENT_API=
+ * ONLINE_ARCHIVE_AUTHENTICATION_PUBLIC_KEY=
+ * ONLINE_ARCHIVE_AUTHENTICATION_PRIVATE_KEY=
+ * Command: node -r dotenv/config src/admin/scripts/configureAuditEventOnlineArchive.js
+ * Command: node -r dotenv/config src/admin/scripts/configureAuditEventOnlineArchive.js --collections="AuditEvent_4_0_0_2023_09_09"
  */
 main().catch((reason) => {
     console.error(reason);
