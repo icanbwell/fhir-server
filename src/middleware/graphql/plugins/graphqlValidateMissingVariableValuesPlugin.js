9039895589683418d30f0d28d8a8a82506e8d7e4
007sunny <sunny.singhal@icanbwell.com>
2023-04-19 12:30:20 +0530

    EFS-216 Add GraphQL missing query variable value plugin (#969)
    * EFS-216 Add GraphQL missing query variable value plugin
    
    * EFS-216 Add default values
    
    * EFS-216 Fix TCs

---
+++ src/middleware/graphql/plugins/graphqlValidateMissingVariableValuesPlugin.js
@@ -0,0 +1,50 @@
+const { ApolloServerErrorCode } = require('@apollo/server/errors');
+
+const { GraphQLError } = require('graphql');
+
+class ValidateMissingVariableValuesPlugin /*extends ApolloServerPlugin*/ {
+    /**
+     * This plugin validates that all the GraphQL Variables have corresponding values provided
+     */
+    constructor() {
+        // ok to not specify
+    }
+
+    // eslint-disable-next-line no-unused-vars
+    async requestDidStart(requestContext1) {
+        return {
+            didResolveOperation({ request, document }) {
+                let missingVariables = [];
+                const queryVariables = request.variables;
+                (document.definitions || []).forEach(definition => {
+                    (definition.variableDefinitions || []).forEach(variableDefinition => {
+                        if (!queryVariables[variableDefinition.variable.name.value] && !variableDefinition.defaultValue) {
+                            missingVariables.push(variableDefinition.variable.name.value);
+                        }
+                    });
+                });
+                if (missingVariables.length) {
+                    throw new GraphQLError(`Missing variable values: ${missingVariables.join(',')}`, {
+                        extensions: { code: ApolloServerErrorCode.GRAPHQL_VALIDATION_FAILED },
+                    });
+                }
+            },
+            async willSendResponse({ response }) {
+                if (response.body.kind === 'single' &&
+                    response.body.singleResult.errors?.[0]?.extensions?.code === ApolloServerErrorCode.GRAPHQL_VALIDATION_FAILED) {
+                    response.http.status = 200;
+                }
+            },
+        };
+    }
+}
+
+const getValidateMissingVariableValuesPlugin = () => {
+    return new ValidateMissingVariableValuesPlugin();
+};
+
+module.exports = {
+    getValidateMissingVariableValuesPlugin,
+};
+
+
