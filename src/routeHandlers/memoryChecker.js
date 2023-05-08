253bf58f1df9c713fd20da226363726cbc42397f
Sunny Singhal <sunny.singhal@icanbwell.com>
2023-05-08 18:32:05 +0530

    EFS-285 Fixed syntax issue

--- src/routeHandlers/memoryChecker.js
+++ src/routeHandlers/memoryChecker.js
@@ -9,7 +9,7 @@ module.exports.handleMemoryCheck = (req, res, memoryThresholdOffset) => {
     const heapStats = v8.getHeapStatistics();
     const memoryUsedMB = heapStats.used_heap_size / 1024 / 1024;
     const memoryAllocatedMB = heapStats.heap_size_limit / 1024 / 1024;
-    const preDefinedMemoryMB = env.SERVER_RESTART_MEM_LIMIT ? parseInt(env.env.SERVER_RESTART_MEM_LIMIT) : null;
+    const preDefinedMemoryMB = env.SERVER_RESTART_MEM_LIMIT ? parseInt(env.SERVER_RESTART_MEM_LIMIT) : null;
     const memoryUsedPercentage = Math.ceil((memoryUsedMB / (preDefinedMemoryMB || memoryAllocatedMB)) * 100);
     let memThreshold = env.SERVER_RESTART_MEM_THRESHOLD ? parseInt(env.SERVER_RESTART_MEM_THRESHOLD) : 95;
     // Reduce threshold so that requests stop coming before the server is restarted in liveness check
