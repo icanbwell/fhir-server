512fee6bc08b1e8ef1c1da8454378ef67b9b28b7
Mintu Sah <mintu.sah@icanbwell.com>
2023-05-23 12:49:38 +0530

    EFS-342 Updated test case to use var module for updating DEFAULT_SORT_ID env value.

--- src/tests/searchParameters/searchByUuidAsDefaultSort/searchByUuidAsDefaultSort.test.js
+++ src/tests/searchParameters/searchByUuidAsDefaultSort/searchByUuidAsDefaultSort.test.js
@@ -1,3 +1,5 @@
+const env = require('var');
+
 // test file
 const person1Resource = require('./fixtures/Person/person1.json');
 const person2Resource = require('./fixtures/Person/person2.json');
@@ -26,8 +28,8 @@ describe('Person Tests', () => {
 
     describe('Person search using uuid as default sort id Tests', () => {
         test('Search person using uuid as default sort id ', async () => {
-            const DEFAULT_SORT_ID = process.env.DEFAULT_SORT_ID;
-            process.env.DEFAULT_SORT_ID = '_uuid';
+            const DEFAULT_SORT_ID = env.DEFAULT_SORT_ID;
+            env.DEFAULT_SORT_ID = '_uuid';
 
             const request = await createTestRequest();
             // ARRANGE
@@ -57,19 +59,19 @@ describe('Person Tests', () => {
 
             // Fetch two resources at a time.
             let response = await request
-                .get('/4_0_0/Person/?_count=2&_total=accurate&_bundle=1')
+                .get('/4_0_0/Person/?_count=2&_total=accurate&_bundle=1&_debug=1')
                 .set(getHeaders());
             // noinspection JSUnresolvedFunction
             expect(response).toHaveResponse(expectedPersonResources1);
 
             // Get the next set of resources based on id:above logic.
             resp = await request
-                .get('/4_0_0/Person/?_count=2&_total=accurate&_bundle=1&id%3Aabove=9b3326ba-2421-4b9a-9d57-1eba0481cbd4')
+                .get('/4_0_0/Person/?_count=2&_total=accurate&_bundle=1&id%3Aabove=9b3326ba-2421-4b9a-9d57-1eba0481cbd4&_debug=1')
                 .set(getHeaders());
             // noinspection JSUnresolvedFunction
             expect(resp).toHaveResponse(expectedPersonResources2);
 
-            process.env.DEFAULT_SORT_ID = DEFAULT_SORT_ID;
+            env.DEFAULT_SORT_ID = DEFAULT_SORT_ID;
         });
     });
 });
