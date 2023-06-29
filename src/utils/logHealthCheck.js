0ac16c403a278cf91c8214d646984929b70af642
drcate <drcate@pobox.com>
2023-06-29 11:52:39 -0700

    EFS-362 checkpoint

--- src/utils/logHealthCheck.js
+++ src/utils/logHealthCheck.js
@@ -1,9 +1,21 @@
-/* eslint-disable no-unused-vars */
-const { logInfo } = require('../operations/common/logging');
+const { logInfo, logSystemEventAsync} = require('../operations/common/logging');
 const { AdminLogger } = require('../admin/adminLogger');
 
 module.exports.handleLogHealthCheck = async () => {
-    logInfo('Log HealthCheck', {});
-    const adminLogger = new AdminLogger();
-    await adminLogger.logInfo('Admin Log HealthCheck');
+    let healthy = true;
+    try {
+        logInfo('Log HealthCheck', {});
+        const adminLogger = new AdminLogger();
+        await adminLogger.logInfo('Admin Log HealthCheck');
+        await logSystemEventAsync(
+            {
+                event: 'healthCheck',
+                message: 'Performing system healthcheck',
+                args: {}
+            }
+        );
+    } catch (e) {
+        healthy = false;
+    }
+    return healthy;
 };
