05f48fa9cbe5686957de7a35c2e1afa77aa0acf7
imranq2 <imranq2@hotmail.com>
2023-07-01 18:55:48 -0700

    format output from fhir query

--- src/tests/chatgpt/chatgpt.test.js
+++ src/tests/chatgpt/chatgpt.test.js
@@ -83,6 +83,48 @@ describe('ChatGPT Tests', () => {
             const result = await chain.call({query: 'List 5 countries.'});
             console.log(JSON.stringify(result.records, null, 2));
         });
+        test('ChatGPT works with English FHIR query and structured output', async () => {
+            // https://js.langchain.com/docs/getting-started/guide-llm
+            // https://blog.langchain.dev/going-beyond-chatbots-how-to-make-gpt-4-output-structured-data-using-langchain/
+            // https://nathankjer.com/introduction-to-langchain/
+
+            const outputParser = StructuredOutputParser.fromZodSchema(
+                z.array(
+                    z.object({
+                        fields: z.object({
+                            url: z.string().describe('url')
+                        })
+                    })
+                ).describe('An array of Airtable records, each representing a url')
+            );
+            const model = new OpenAI({
+                // modelName: 'gpt-4', // Or gpt-3.5-turbo
+                temperature: 0 // For best results with the output fixing parser
+            });
+            const outputFixingParser = OutputFixingParser.fromLLM(
+                model,
+                outputParser
+            );
+            const template = 'You are a software program. You are talking to a FHIR server. \n{format_instructions}\n The base url is fhir.icanbwell.com/4_0_0.  Patient id is {patientId}. how would I query for all FHIR {resource} that belong to this patient?';
+
+            // const template = 'Answer the user\'s question as best you can:\n{format_instructions}\n{query}';
+            const prompt = new PromptTemplate({
+                template: template,
+                inputVariables: ['patientId', 'resource'],
+                partialVariables: {
+                    format_instructions: outputFixingParser.getFormatInstructions()
+                }
+            });
+            console.log(prompt);
+            const chain = new LLMChain(
+                {
+                    llm: model, prompt: prompt,
+                    outputKey: 'records', // For readability - otherwise the chain output will default to a property named "text"
+                    outputParser: outputFixingParser
+                });
+            const result = await chain.call({patientId: 'imran', resource: 'condition'});
+            console.log(JSON.stringify(result.records, null, 2));
+        });
     });
 });
 
