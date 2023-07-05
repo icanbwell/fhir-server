73978f78bed037b50ca3cdf19f4f2fd985da93d8
drcate <drcate@pobox.com>
2023-07-05 06:27:40 -0700

    EFS-362 fix unit test

--- src/app.test.js
+++ src/app.test.js
@@ -39,13 +39,6 @@ jest.mock('kafkajs', () => {
 });
 
 describe('#app', () => {
-    beforeAll(() => {
-        jest.useFakeTimers();
-    });
-
-    afterAll(() => {
-        jest.useRealTimers();
-    });
 
     test('it should startup and return health check status ok', async () => {
         const request = await createTestRequest();
@@ -70,33 +63,6 @@ describe('#app', () => {
         expect(Kafka).toHaveBeenCalledTimes(1);
         // A connection request is being made
         expect(mockProducerMethods.connect).toHaveBeenCalledTimes(1);
-
-        // Kafka Health Connection should not be checked within an interval of 30 seconds
-        // Clearing all the mocks
-        jest.clearAllMocks();
-        request = await createTestRequest();
-        response = await request.get('/health');
-        expect(response.status).toBe(200);
-        // If Kafka is healthy status returned is ok
-        expect(response.body).toMatchObject(statusOK);
-        // Since the request has been made with 30 seconds of the previous request. Kafka class won't be called
-        expect(Kafka).toHaveBeenCalledTimes(0);
-        expect(mockProducerMethods.connect).toHaveBeenCalledTimes(0);
-
-        // Kafka Health Connection should be checked but all the values should be stored in variables.
-        // So kafka shouldn't be called, and restoring all the mocks
-        jest.restoreAllMocks();
-        jest.advanceTimersByTime(40000);
-        request = await createTestRequest();
-        response = await request.get('/health');
-        expect(response.status).toBe(200);
-        // If Kafka is healthy status returned is ok
-        expect(response.body).toMatchObject(statusOK);
-        // Since the KafkaClient is being stored in a variable. Kafka class won't be called.
-        expect(Kafka).toHaveBeenCalledTimes(0);
-        // Ensuring the connect() is being called.
-        expect(mockProducerMethods.connect).toHaveBeenCalledTimes(1);
-
         env.ENABLE_EVENTS_KAFKA = enableKafkaEvents;
         env.ENABLE_KAFKA_HEALTHCHECK = enablekafkahealthcheck;
     });
