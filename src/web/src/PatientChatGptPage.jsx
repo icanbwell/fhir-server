ced1202c9e716f508e75f36d8e7d41db9f8415af
imranq2 <imranq2@hotmail.com>
2023-07-06 12:40:08 -0700

    call $everything

--- src/web/src/PatientChatGptPage.jsx
+++ src/web/src/PatientChatGptPage.jsx
@@ -3,36 +3,42 @@ import React, {useState} from 'react';
 function PatientChatGptPage() {
     const [
         textInput,
-        setTextInput, textResponse,
-        setTextResponse
+        setTextInput,
+        textResponse,
+        setTextResponse,
+        data,
+        setData
     ] = useState('');
 
     const handleInputChange = (event) => {
         setTextInput(event.target.value);
     };
 
-    const callApi = () => {
-        const urlEncodedQuestion = encodeURIComponent(textInput);
-        fetch(`/4_0_0/Patient/john-muir-health-e.k-4ea143ZrQGvdUvf-b2y.tdyiVMBWgblY4f6y2zis3/$everything?_question=${urlEncodedQuestion}`, {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json'
-            },
-        })
-            .then(response => response.json())
-            .then(data => {
-                // Process the API response
-                console.log(data);
-                if (data.entry && data.entry.length > 0) {
-                    const patient = data.entry[0].resource;
-                    if (patient && patient.text && patient.text.div) {
-                        // setTextResponse(patient.text.div);
-                    }
-                }
-            })
-            .catch(error => {
-                console.error(error);
-            });
+    const callApi = async () => {
+        try {
+            const urlEncodedQuestion = encodeURIComponent(textInput);
+            const patientId = `john-muir-health-e.k-4ea143ZrQGvdUvf-b2y.tdyiVMBWgblY4f6y2zis3`;
+            const url = `/4_0_0/Patient/${patientId}/$everything?_question=${urlEncodedQuestion}`;
+            const response = await fetch(url,
+                {
+                    method: 'GET',
+                    headers: {
+                        'Accept': 'application/json'
+                    },
+                });
+            const data = await response.json();
+            console.log(data);
+            // setData(data);
+            // if (data.entry && data.entry.length > 0) {
+            //     const patient = data.entry[0].resource;
+            //     if (patient && patient.text && patient.text.div) {
+            //         console.log(patient.text.div);
+            //         // setTextResponse(patient.text.div);
+            //     }
+            // }
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     return (
@@ -40,11 +46,13 @@ function PatientChatGptPage() {
             <input type="text" value={textInput} onChange={handleInputChange}
                    defaultValue="What is the age of this patient?"/>
             <button onClick={callApi}>Ask</button>
-            {textResponse && (
+            {data ? (
                 <div>
-                    <h2>Received Data:</h2>
-                    <pre>{JSON.stringify(textResponse, null, 2)}</pre>
+                    <h2>API Response:</h2>
+                    <pre>{JSON.stringify(data, null, 2)}</pre>
                 </div>
+            ) : (
+                <p>Loading...</p>
             )}
         </div>
     );
