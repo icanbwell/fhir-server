84aa8e2922054d1e39133fddd863d24a975713c1
imranq2 <imranq2@hotmail.com>
2023-07-06 10:18:59 -0700

    fix routing

--- src/web/src/App.js
+++ src/web/src/App.js
@@ -1,28 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+
 import HomePage from './HomePage';
-import PatientChatGptPage from './PatientChatGptPage';
+// import PatientChatGptPage from './PatientChatGptPage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Routes>
+                    <Route exact path="/" component={HomePage}/>
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
