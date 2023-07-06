e710a985c5e3525817e1bf103354bdbaa7eb499a
imranq2 <imranq2@hotmail.com>
2023-07-06 09:45:41 -0700

    add react router

--- src/web/src/App.js
+++ src/web/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import HomePage from './HomePage';
+import PatientChatGptPage from './PatientChatGptPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1>My App</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/chatgpt">ChatGpt</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route exact path="/" component={HomePage} />
+        <Route path="/chatgpt" component={PatientChatGptPage} />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/web/src/PatientChatGptPage.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+
+function PatientChatGptPage() {
+    const [textInput, setTextInput] = useState('');
+
+    const handleInputChange = (event) => {
+        setTextInput(event.target.value);
+    };
+
+    const callApi = () => {
+        fetch('https://api.example.com/endpoint', {
+            method: 'GET',
+            // Additional headers or body if needed
+        })
+            .then(response => response.json())
+            .then(data => {
+                // Process the API response
+                console.log(data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    };
+
+    return (
+        <div>
+            <input type="text" value={textInput} onChange={handleInputChange}/>
+            <button onClick={callApi}>Ask</button>
+        </div>
+    );
+}
+
+export default PatientChatGptPage;
+
