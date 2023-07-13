c86233f1c702f5e139405ccba11622c3f170c049
drcate <drcate@pobox.com>
2023-07-13 07:16:01 -0700

    EFS-434 clean up script

--- src/admin/runners/dumpPersonsRunner.js
+++ src/admin/runners/dumpPersonsRunner.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const {finished} = require('stream/promises');
-// const { assertIsValid } = require('../../utils/assertType');
 const moment = require('moment-timezone');
 const { BaseBulkOperationRunner } = require('./baseBulkOperationRunner');
 
@@ -10,8 +9,6 @@ class DumpPersonsRunner extends BaseBulkOperationRunner {
      * @param {AdminLogger} adminLogger
      * @param {MongoDatabaseManager} mongoDatabaseManager
      * @param {MongoCollectionManager} mongoCollectionManager
-     * @param {number} limit
-     * @param {number} skip
      * @param {number} batchSize
      * @param {string} accessCode
      * @param {string} beforeDate
@@ -22,8 +19,6 @@ class DumpPersonsRunner extends BaseBulkOperationRunner {
             adminLogger,
             mongoDatabaseManager,
             mongoCollectionManager,
-            limit,
-            skip,
             batchSize,
             accessCode,
             beforeDate,
@@ -41,16 +36,6 @@ class DumpPersonsRunner extends BaseBulkOperationRunner {
          * @type {number}
          */
         this.batchSize = batchSize;
-
-        /**
-         * @type {number|undefined}
-         */
-        this.limit = limit;
-
-        /**
-         * @type {number|undefined}
-         */
-        this.skip = skip;
 
         /**
          * @type {string}
@@ -85,19 +70,6 @@ class DumpPersonsRunner extends BaseBulkOperationRunner {
         return newDoc;
     }
 
-
-    // async processBatch(docList) {
-    //     const query = {};
-    //     try {
-    //         this.adminLogger.logInfo(`Total resources being processed: ${docList.length}`);
-    //         for (const doc in docList) {
-    //             this.outputStream.write(JSON.stringify(doc));
-    //             this.outputStream.write(',');
-    //         }
-    //     } catch (e) {
-    //         this.adminLogger.logError(`Got error ${e}.  At ${this.startFromIdContainer.startFromId}`);
-    //     }
-    // }
     /**
      * Runs a loop to access all the documents and dump into output file
      * @returns {Promise<void>}
@@ -114,7 +86,7 @@ class DumpPersonsRunner extends BaseBulkOperationRunner {
                     writer.once('drain', resolve);
                 });
             }
-        }
+        };
         const firstWrite = writeStream(this.outputStream, '{ "entry" : [');
         if (firstWrite) {
             if (firstWrite) {
@@ -125,7 +97,6 @@ class DumpPersonsRunner extends BaseBulkOperationRunner {
         let config = await this.mongoDatabaseManager.getClientConfigAsync();
         let sourceClient = await this.mongoDatabaseManager.createClientAsync(config);
         let session = sourceClient.startSession();
-        // console.log(session);
         let sessionId = session.serverSession.id;
         const sourceDb = sourceClient.db(config.db_name);
         const sourceCollection = await this.mongoCollectionManager.getOrCreateCollectionAsync(
@@ -135,13 +106,6 @@ class DumpPersonsRunner extends BaseBulkOperationRunner {
         );
         const options = {session: session, timeout: false, noCursorTimeout: true, maxTimeMS: this.maxTimeMS};
         let refreshTimestamp = moment(); // take note of time at operation start
-        // const db = await this.mongoDatabaseManager.getClientDbAsync();
-        // console.log('getting collection');
-        // const dbCollection = await this.mongoCollectionManager.getOrCreateCollectionAsync(
-        //     {
-        //         db: db, collectionName: this.collectionName
-        //     }
-        // );
         // Filter to process only certain documents depending on the owner code passed.
         const accessFilter = this.accessCode ?
             { 'meta.security': { $elemMatch: { 'system': 'https://www.icanbwell.com/access', 'code': this.accessCode }} } :
@@ -156,12 +120,11 @@ class DumpPersonsRunner extends BaseBulkOperationRunner {
         const result = await sourceCollection.find({
             ...accessFilter,
             ...beforeDateQuery
-        }, options).batchSize(1000)
+        }, options).batchSize(this.batchSize)
             .maxTimeMS(this.maxTimeMS) // 20 hours
             .addCursorFlag('noCursorTimeout', true);
         // Format document and write to output file
         for await (let doc of result) {
-            //console.log('inside loop');
             doc = await this.formatDocument(doc);
             const docWrite = writeStream(this.outputStream, JSON.stringify(doc));
             if (docWrite) {
@@ -184,7 +147,6 @@ class DumpPersonsRunner extends BaseBulkOperationRunner {
         }
         const closeBracket = writeStream(this.outputStream, ']}');
         if (closeBracket) {
-            console.log('closeBracket');
             await closeBracket;
         }
         await session.endSession();
--- src/admin/scripts/dumpPersons.js
+++ src/admin/scripts/dumpPersons.js
@@ -12,7 +12,7 @@ async function main() {
      * @type {Object}
      */
     const parameters = CommandLineParser.parseCommandLine();
-    const batchSize = parameters.batchSize || process.env.BULK_BUFFER_SIZE || 100;
+    const batchSize = parameters.batchSize || process.env.BULK_BUFFER_SIZE || 1000;
     const accessCode = parameters.accessCode;
     const beforeDate = parameters.beforeDate;
     const outputFile = parameters.outputFile;
@@ -29,8 +29,6 @@ async function main() {
             adminLogger: new AdminLogger(),
             mongoDatabaseManager: c.mongoDatabaseManager,
             mongoCollectionManager: c.mongoCollectionManager,
-            limit: parameters.limit,
-            skip: parameters.skip,
             batchSize: batchSize,
             accessCode: accessCode,
             beforeDate: beforeDate,
@@ -48,14 +46,11 @@ async function main() {
 
 /**
  * To run this:
- * If running the script using EC2, the command will be: node -r dotenv/config src/admin/scripts/dumpPersons
+ * If running the script using EC2, the command will be: node -r dotenv/config src/admin/scripts/dumpPersons.js
  * Required env variables:
  * MONGO_URL, MONGO_USERNAME, MONGO_PASSWORD, MONGO_DB_NAME(default: fhir)
- * node src/admin/scripts/dumpPersons
- * node src/admin/scripts/dumpPersons --uuidGreaterThan="60185667-f6c5-5534-8980-90448606be94"
- * node src/admin/scripts/removeDuplicatePersonLinks --minLinks=2 --limit=10 --batchSize=1000
- * node src/admin/scripts/removeDuplicatePersonLinks --ownerCode="bwell"
- * node src/admin/scripts/removeDuplicatePersonLinks --personUuids="60185667-f6c5-5534-8980-90448606be94,f31e6f0a-a0fc-500d-8e6a-e017d633391d"
+ * node src/admin/scripts/dumpPersons.js
+ * node src/admin/scripts/dumpPersons.js --beforeDate=2023-04-22T00:00:00Z --accessCode=bWell --batchSize=1000 --outputFile=dump.json
  */
 main().catch(reason => {
     console.error(reason);
