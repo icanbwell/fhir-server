c0aefbf2ee7742841374a457106d7ac93e1aa9a9
drcate <drcate@pobox.com>
2023-07-21 15:01:25 -0700

    EFS-401 add setup for quantity-type search

--- src/tests/graphql/observation/observation.test.js
+++ src/tests/graphql/observation/observation.test.js
@@ -15,6 +15,7 @@ const path = require('path');
 const observationQuery = fs.readFileSync(path.resolve(__dirname, './fixtures/query.graphql'), 'utf8');
 const observationSubjectQuery = fs.readFileSync(path.resolve(__dirname, './fixtures/query_subject.graphql'), 'utf8');
 const observationNotSubjectQuery = fs.readFileSync(path.resolve(__dirname, './fixtures/query_not_subject.graphql'), 'utf8');
+// const observationNotQuantityQuery = fs.readFileSync(path.resolve(__dirname, './fixtures/query_not_quantity.graphql'), 'utf8');
 
 const {
     commonBeforeEach,
@@ -164,5 +165,49 @@ describe('GraphQL Observation Tests', () => {
             // noinspection JSUnresolvedFunction
             expect(resp).toHaveGraphQLResponse(expectedObservationNotSubjectResources, 'observation');
         });
+        // blocked by bug where quantity searchs are ignored
+        /* test('GraphQL notEquals Quantity type', async () => {
+            const request = await createTestRequest();
+            // ARRANGE
+            // add the resources to FHIR server
+            let resp = await request
+                .post('/4_0_0/Observation/1/$merge?validate=true')
+                .send(observation1Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            resp = await request
+                .post('/4_0_0/Patient/1/$merge?validate=true')
+                .send(patientBundleResource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            resp = await request
+                .post('/4_0_0/Person/1/$merge?validate=true')
+                .send(personBundleResource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            const graphqlQueryText = observationNotQuantityQuery.replace(/\\n/g, '');
+            // ACT & ASSERT
+            resp = await request
+                // .get('/graphql/?query=' + graphqlQueryText)
+                // .set(getHeaders())
+                .post('/graphqlv2')
+                .send({
+                    operationName: null,
+                    variables: {
+                        FHIR_DEFAULT_COUNT: 10
+                    },
+                    query: graphqlQueryText,
+                })
+               .set(getGraphQLHeadersWithPerson('79e59046-ffc7-4c41-9819-c8ef83275454'));
+
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveGraphQLResponse(expectedObservationNotSubjectResources, 'observation');
+        }); */
     });
 });
