b57e402f11051a48fdb57d7f2d5fcad12d5e5c25
imranq2 <imranq2@hotmail.com>
2023-07-26 19:10:01 -0700

    use async _read

--- src/operations/streaming/mongoStreamReader.js
+++ src/operations/streaming/mongoStreamReader.js
@@ -26,18 +26,42 @@ class MongoReadableStream extends Readable {
     ) {
         super({objectMode: true, highWaterMark: highWaterMark});
 
+        /**
+         * @type {DatabasePartitionedCursor}
+         */
         this.cursor = cursor;
 
+        /**
+         * @type {AbortSignal}
+         */
         this.signal = signal;
 
+        /**
+         * @type {DatabaseAttachmentManager}
+         */
         this.databaseAttachmentManager = databaseAttachmentManager;
+
+        /**
+         * @type {boolean}
+         */
+        this.isFetchingData = false; // Track if data is currently being fetched
     }
 
     // eslint-disable-next-line no-unused-vars
-    _read(size) {
-        (async () => {
-            await this.readAsync();
-        })();
+    async _read(size) {
+        // Ensure we are not already fetching data
+        if (!this.isFetchingData) {
+            this.isFetchingData = true;
+            try {
+                await this.readAsync();
+            } catch (error) {
+                // Handle any errors that may occur during data retrieval
+                this.emit('error', error);
+                this.push(null); // Signal the end of the stream
+                return;
+            }
+            this.isFetchingData = false;
+        }
     }
 
     async readAsync() {
