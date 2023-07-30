b76b4f71ce5b2d15dcc0d6c8968f7150c6e924c5
imranq2 <imranq2@hotmail.com>
2023-07-29 19:53:42 -0700

    add table style

--- src/web/src/partials/Identifier.jsx
+++ src/web/src/partials/Identifier.jsx
@@ -1,56 +1,66 @@
 import React from 'react';
+import {makeStyles} from '@mui/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-function Identifier({ identifiers, name, resourceType }) {
-    if (!Array.isArray(identifiers)) {
-        identifiers = [identifiers];
-    }
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+});
 
-    if (identifiers && identifiers.length > 0 && identifiers[0]) {
-        return (
-            <div>
-                <h4>{name}</h4>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Id</th>
-                            <th scope="col">Value</th>
-                            <th scope="col">System</th>
-                            <th scope="col">Type Code</th>
-                            <th scope="col">Type System</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {identifiers.map(identifier => {
-                            if (identifier) {
-                                return (
-                                    <tr key={identifier.id}>
-                                        <td>{identifier.id}</td>
-                                        <td>
-                                            <a title={`Search for ${identifier.value}`}
-                                               href={`/4_0_0/${resourceType}?identifier=${identifier.system}|${identifier.value}`}>{identifier.value}</a>
-                                        </td>
-                                        <td>{identifier.system}</td>
-                                        <td>
-                                            {identifier.type && identifier.type.coding &&
-                                                identifier.type.coding.map(coding => <span key={coding.code}>{coding.code} &nbsp;</span>)
-                                            }
-                                        </td>
-                                        <td>
-                                            {identifier.type && identifier.type.coding &&
-                                                identifier.type.coding.map(coding => <span key={coding.system}>{coding.system} &nbsp;</span>)
-                                            }
-                                        </td>
-                                    </tr>
-                                );
-                            }
-                            return null;
-                        })}
-                    </tbody>
-                </table>
-            </div>
-        );
-    }
-    return null;
+function Identifier({identifiers, resourceType}) {
+    const classes = useStyles();
+
+    return (
+        <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="identifier table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Id</TableCell>
+                        <TableCell>Value</TableCell>
+                        <TableCell>System</TableCell>
+                        <TableCell>Type Code</TableCell>
+                        <TableCell>Type System</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {identifiers.map(identifier => {
+                        if (identifier) {
+                            return (
+                                <TableRow key={identifier.id}>
+                                    <TableCell>{identifier.id}</TableCell>
+                                    <TableCell>
+                                        <a title={`Search for ${identifier.value}`}
+                                           href={`/4_0_0/${resourceType}?identifier=${identifier.system}|${identifier.value}`}>{identifier.value}</a>
+                                    </TableCell>
+                                    <TableCell>{identifier.system}</TableCell>
+                                    <TableCell>
+                                        {identifier.type && identifier.type.coding &&
+                                            identifier.type.coding.map(coding => <span
+                                                key={coding.code}>{coding.code} &nbsp;</span>)
+                                        }
+                                    </TableCell>
+                                    <TableCell>
+                                        {identifier.type && identifier.type.coding &&
+                                            identifier.type.coding.map(coding => <span
+                                                key={coding.system}>{coding.system} &nbsp;</span>)
+                                        }
+                                    </TableCell>
+                                </TableRow>
+                            );
+                        }
+                        return null;
+                    })}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 }
 
 export default Identifier;
