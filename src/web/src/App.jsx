ee54c2534ef2baa0ce43cfca5c1dd6204853e3e1
imranq2 <imranq2@hotmail.com>
2023-07-31 15:00:24 -0700

    switch to createBrowserRouter

--- src/web/src/App.jsx
+++ src/web/src/App.jsx
@@ -23,31 +23,35 @@ import Root from './pages/Root';
 
 function App() {
     const router = createBrowserRouter(
-        createRoutesFromElements(
-            <Route path="/" element={<HomePage/>}/>
-        ),
-        // createRoutesFromElements(
-        //     <Routes>
-        //         <Route path="/" element={<HomePage/>}/>
-        //         {/*<Route path="about" element={<AboutPage/>}/>*/}
-        //         {/*<Route path="patient" element={<PatientChatGptPage/>}/>*/}
-        //         {/*<Route path="pat2/:id" element={<PatientChatGptPage/>}/>*/}
-        //         {/*<Route path="patientTimeline" element={<PatientTimeline/>}/>*/}
-        //         {/*<Route path="observationGraph" element={<ObservationGraph/>}/>*/}
-        //         {/*<Route path="observationTimeline" element={<ObservationTimeline/>}/>*/}
-        //         {/*<Route path="4_0_0/Patient/id:?" element={<IndexPage/>}/>*/}
-        //         {/*<Route path="4_0_0/Practitioner/" element={<IndexPage/>}*/}
-        //         {/*       errorElement={<p>Oops! Something Went Wrong</p>}/>*/}
-        //         {/*<Route path="/4_0_0/Practitioner/id:?" element={<IndexPage/>}/>*/}
-        //     </Routes>
-        // ),
+        [
+            {path: "*", Component: Root},
+        ],
         {basename: "/web"}
     );
+
+    // 1️⃣ Changed from App to Root
+    function Root() {
+        // 2️⃣ `BrowserRouter` component removed, but the <Routes>/<Route>
+        // component below are unchanged
+        return (
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/about" element={<AboutPage/>}/>
+                <Route path="/patient" element={<PatientChatGptPage/>}/>
+                <Route path="/pat2/:id" element={<PatientChatGptPage/>}/>
+                <Route path="/patientTimeline" element={<PatientTimeline/>}/>
+                <Route path="/observationGraph" element={<ObservationGraph/>}/>
+                <Route path="/observationTimeline" element={<ObservationTimeline/>}/>
+                <Route path="/4_0_0/Patient/id:?" element={<IndexPage/>}/>
+                <Route path="/4_0_0/Practitioner/" element={<IndexPage/>}/>
+                <Route path="/4_0_0/Practitioner/id:?" element={<IndexPage/>}/>
+            </Routes>
+        );
+    }
+
     return (
         // https://reactrouter.com/en/main/start/overview
-        <React.StrictMode>
-            <RouterProvider router={router}/>
-        </React.StrictMode>
+        <RouterProvider router={router}/>
     );
 }
 
