b096d799f81a7e6b8e228257a8d33bd0c1aeb33c
imranq2 <imranq2@hotmail.com>
2023-08-01 15:03:12 -0700

    add ResourceCard.jsx

--- src/web/src/pages/ResourceCard.jsx
+++ src/web/src/pages/ResourceCard.jsx
@@ -3,11 +3,23 @@ import ResourceHeader from '../partials/ResourceHeader';
 import ResourceItem from './ResourceItem';
 import Json from '../partials/Json';
 import React from 'react';
+import {makeStyles} from '@mui/styles';
+
+const useStyles = makeStyles({
+    header: {
+        backgroundColor: 'lightgray', // Light gray background color
+        color: 'darkslategray',
+        fontSize: '2em',          // Text size
+        // add more styles as you wish
+    },
+});
 
 const ResourceCard = ({index, resource}) => {
+    const classes = useStyles();
     return (
         <Card key={index}>
-            <CardHeader title={`(${index + 1}) ${resource.resourceType}/${resource.id}`}>
+            <CardHeader title={`(${index + 1}) ${resource.resourceType}/${resource.id}`}
+                        classes={{title: classes.header}}>
             </CardHeader>
             <CardContent>
                 <ResourceHeader resource={resource}/>
