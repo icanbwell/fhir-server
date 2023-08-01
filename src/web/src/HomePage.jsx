1764d88fec3749bc6f05a5dc31b74511a8d9fe70
imranq2 <imranq2@hotmail.com>
2023-08-01 12:41:13 -0700

    set route to home page

--- src/web/src/HomePage.jsx
+++ src/web/src/HomePage.jsx
@@ -1,8 +1,57 @@
 // HomePage.js
-import React from 'react';
+import {resourceDefinitions} from './utils/reactResourceDefinitions';
+import React, {useEffect} from 'react';
+import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow, IconButton} from '@mui/material';
+import DescriptionIcon from '@mui/icons-material/Description';
 
-function HomePage() {
-  return <h1>Welcome to the Home Page</h1>;
-}
+const Home = ({resources, url, currentYear, meta}) => {
+    const searchResource = (resourceName) => {
+        // implementation of searchResource
+    };
 
-export default HomePage;
+    const openDox = (event, resourceUrl) => {
+        // prevent row click event
+        event.stopPropagation();
+        // implementation of openDox
+    };
+
+    useEffect(() => {
+    }, []);
+
+    return (
+        <div>
+            <TableContainer>
+                <Table stickyHeader className="sticky-table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Resource</TableCell>
+                            <TableCell>Description</TableCell>
+                            <TableCell></TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {resourceDefinitions.map((resource) => (
+                            <TableRow key={resource.name} onClick={() => searchResource(resource.name)}
+                                      className="row-click">
+                                <TableCell>{resource.name}</TableCell>
+                                <TableCell className="pe-5 position-relative">
+                                    {resource.description}
+                                </TableCell>
+                                <TableCell>
+                                    <IconButton
+                                        onClick={(event) => openDox(event, resource.url)}
+                                        title={`FHIR Specification for ${resource.name}`}
+                                    >
+                                        <DescriptionIcon/>
+                                    </IconButton>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+    );
+};
+
+export default Home;
