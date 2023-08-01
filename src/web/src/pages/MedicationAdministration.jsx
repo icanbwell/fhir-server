dc4f03d13db6d6403ef588e8fba5f9c2ae582805
imranq2 <imranq2@hotmail.com>
2023-08-01 11:00:19 -0700

    add Medication.jsx

---
+++ src/web/src/pages/MedicationAdministration.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Typography, Link, Box} from '@mui/material';
+
+const MedicationAdministration = ({resource, admin, index}) => {
+
+    return (
+        <Box>
+        </Box>
+    );
+};
+
+export default MedicationAdministration;
---
+++ src/web/src/pages/MedicationDispense.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Typography, Link, Box} from '@mui/material';
+
+const MedicationDispense = ({resource, admin, index}) => {
+
+    return (
+        <Box>
+        </Box>
+    );
+};
+
+export default MedicationDispense;
