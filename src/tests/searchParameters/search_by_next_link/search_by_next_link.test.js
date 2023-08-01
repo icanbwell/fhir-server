5077b4197586adbc920c27be230a773619ea7a3a
007sunny <sunny.singhal@joshtechnologygroup.com>
2023-08-01 14:09:45 +0530

    EFS-441 Added testcase for _elements param

--- src/tests/searchParameters/search_by_next_link/search_by_next_link.test.js
+++ src/tests/searchParameters/search_by_next_link/search_by_next_link.test.js
@@ -98,5 +98,75 @@ describe('Next link Tests', () => {
             // revert DEFAULT_SORT_ID to original value
             env.DEFAULT_SORT_ID = defaultSortId;
         });
+
+        test('next link is present and works', async () => {
+            // change DEFAULT_SORT_ID to _uuid
+            const defaultSortId = env.DEFAULT_SORT_ID;
+            env.DEFAULT_SORT_ID = '_uuid';
+
+            const request = await createTestRequest();
+
+            // add the resources to FHIR server
+            let resp = await request
+                .post('/4_0_0/Person/1/$merge')
+                .send(person1Resource)
+                .set(getHeaders());
+
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            resp = await request
+                .post('/4_0_0/Person/2/$merge')
+                .send(person2Resource)
+                .set(getHeaders());
+
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
+
+            // response should have person2 as the only resource and nextLink should be present
+            resp = await request
+                .get('/4_0_0/Person?_count=1&_bundle=1&_elements=id')
+                .set(getHeaders());
+
+            expect(resp.body.entry).toBeDefined();
+            expect(resp.body.entry.length).toEqual(1);
+            const person2 = resp.body.entry[0].resource;
+            expect(person2).toEqual({id: '2', resourceType: 'Person'});
+
+            expect(resp.body.link).toBeDefined();
+            expect(resp.body.link.length).toEqual(2);
+
+            let nextLink = resp.body.link[1].url.replace('http://localhost:3000', '');
+            expect(isUuid(nextLink.split('=').pop())).toBeTrue();
+
+            // response should have person1 as the only resource and nextLink should be present
+            resp = await request
+                .get(nextLink)
+                .set(getHeaders());
+
+            expect(resp.body.entry).toBeDefined();
+            expect(resp.body.entry.length).toEqual(1);
+            const person1 = resp.body.entry[0].resource;
+            expect(person1).toEqual({id: '1', resourceType: 'Person'});
+
+            expect(resp.body.link).toBeDefined();
+            expect(resp.body.link.length).toEqual(2);
+            nextLink = resp.body.link[1].url.replace('http://localhost:3000', '');
+            expect(isUuid(nextLink.split('=').pop())).toBeTrue();
+
+            // response should not have any resource and nextLink should not be present
+            resp = await request
+                .get(nextLink)
+                .set(getHeaders());
+
+            expect(resp.body.entry).toBeDefined();
+            expect(resp.body.entry.length).toEqual(0);
+
+            expect(resp.body.link).toBeDefined();
+            expect(resp.body.link.length).toEqual(1);
+
+            // revert DEFAULT_SORT_ID to original value
+            env.DEFAULT_SORT_ID = defaultSortId;
+        });
     });
 });
