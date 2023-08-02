ada7aadfe64b11727c630fa8c7bf586fd83e1a90
imranq2 <imranq2@hotmail.com>
2023-08-01 18:37:28 -0700

    Allow switching back and forth for beta

--- src/web/src/partials/Header.jsx
+++ src/web/src/partials/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {AppBar, Toolbar, Typography, IconButton, Button} from '@mui/material';
+import {AppBar, Toolbar, Typography, IconButton, Button, Box, Alert} from '@mui/material';
 import InfoIcon from '@mui/icons-material/Info';
 import LogoutIcon from '@mui/icons-material/Logout';
 import HomeIcon from '@mui/icons-material/Home';
@@ -15,28 +15,69 @@ class Header extends React.Component {
         };
     }
 
+    setCookie(name, value, days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        const expires = "expires=" + date.toUTCString();
+        document.cookie = name + "=" + value + ";" + expires + ";path=/";
+    }
+
+    getCookie(name) {
+        const cookieName = name + "=";
+        const decodedCookie = decodeURIComponent(document.cookie);
+        const cookies = decodedCookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = cookies[i];
+            while (cookie.charAt(0) === ' ') {
+                cookie = cookie.substring(1);
+            }
+            if (cookie.indexOf(cookieName) === 0) {
+                return cookie.substring(cookieName.length, cookie.length);
+            }
+        }
+        return "";
+    }
+
+    deleteCookie(name) {
+        console.log(`deleteCookie(${name})`);
+        document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    }
+
+    switchToOldUI = () => {
+        this.deleteCookie("web2");
+        window.location.href = "/";
+    };
+
+
     render() {
         const {deployEnvironment, environment, resources} = this.state;
         return (
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton color="inherit" aria-label="home" id="home" component={Link} to="/">
-                        <HomeIcon/>
-                    </IconButton>
+            <React.Fragment>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton color="inherit" aria-label="home" id="home" component={Link} to="/">
+                            <HomeIcon/>
+                        </IconButton>
 
-                    <Typography variant="h6" style={{flexGrow: 1}}>
-                        Helix FHIR Server
-                    </Typography>
-                    <IconButton color="inherit" aria-label="information" id="appInfo">
-                        <InfoIcon/>
-                    </IconButton>
-                    {environment &&
-                        <Button color="inherit" startIcon={<LogoutIcon/>} id="btnLogout" href="/logout_action">
-                            Logout
-                        </Button>
-                    }
-                </Toolbar>
-            </AppBar>
+                        <Typography variant="h6" style={{flexGrow: 1}}>
+                            Helix FHIR Server
+                        </Typography>
+                        <IconButton color="inherit" aria-label="information" id="appInfo">
+                            <InfoIcon/>
+                        </IconButton>
+                        {environment &&
+                            <Button color="inherit" startIcon={<LogoutIcon/>} id="btnLogout" href="/logout_action">
+                                Logout
+                            </Button>
+                        }
+                    </Toolbar>
+                </AppBar>
+                <Alert severity="info" action={
+                    <Button color="inherit" size="small" onClick={this.switchToOldUI}>
+                        Switch to Old UI
+                    </Button>
+                }>You are using the new UI</Alert>
+            </React.Fragment>
         );
     }
 }
