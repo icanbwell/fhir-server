2e302598d8c404e6625c56ba6cacbefc0c0d662f
007sunny <sunny.singhal@joshtechnologygroup.com>
2023-08-04 18:57:50 +0530

    EFS-490 Fixed some bugs

--- src/tests/update/put_response/put_response.test.js
+++ src/tests/update/put_response/put_response.test.js
@@ -22,7 +22,7 @@ describe('ActivityDefinition Tests', () => {
             const request = await createTestRequest();
             // Case when meta.source doesn't exist
             let resp = await request
-                .post('/4_0_0/Practitioner/')
+                .post('/4_0_0/ActivityDefinition/')
                 .send(activitydefinition1Resource)
                 .set(getHeaders());
             // noinspection JSUnresolvedFunction
@@ -52,6 +52,25 @@ describe('ActivityDefinition Tests', () => {
             ).toStrictEqual(
                 'location.reference: location is an invalid reference'
             );
+        });
+
+        test('put works when resource is without id and _validate is true', async () => {
+            const request = await createTestRequest();
+
+            activitydefinition1Resource.meta.source = 'bwell';
+            let resp = await request
+                .post('/4_0_0/ActivityDefinition/$merge')
+                .send(activitydefinition1Resource)
+                .set(getHeaders())
+                .expect(200);
+
+            delete activitydefinition1Resource.id;
+            resp = await request
+                .put('/4_0_0/ActivityDefinition/ab2d17e3-3996-487c-bf81-cbe31abde0be?_validate=true')
+                .send(activitydefinition1Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResponse(expectedActivityDefinitionResources);
         });
     });
 });
