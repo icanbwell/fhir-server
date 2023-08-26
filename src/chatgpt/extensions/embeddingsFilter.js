c912fd119c143e548c69ec95ae707f87642503ee
imranq2 <imranq2@hotmail.com>
2023-08-25 22:42:14 -0400

    add converted functions for langchain

---
+++ src/chatgpt/extensions/embeddingsFilter.js
@@ -0,0 +1,59 @@
+const {BaseDocumentCompressor} = require('langchain/retrievers/document_compressors');
+const {cosineSimilarity} = require('langchain/util/math');
+const {get_stateful_documents, _get_embeddings_from_stateful_docs} = require('./embeddings_redundant_filter');
+
+/**
+ * @classdesc This class is present in langchain python but not in javascript yet
+ * so we converted this from python.  We should remove this once langchain js has this class
+ * https://github.com/langchain-ai/langchain/blob/master/libs/langchain/langchain/retrievers/document_compressors/embeddings_filter.py
+ */
+class EmbeddingsFilter extends BaseDocumentCompressor {
+    /**
+     * constructor
+     * @param {import('langchain/embeddings').Embeddings} embeddings
+     * @param {function(X: number[][], Y: number[][]): number[][]} similarity_fn
+     * @param {number|undefined} [k]
+     * @param {float|undefined} [similarity_threshold]
+     */
+    constructor(embeddings, similarity_fn = cosineSimilarity, k = 20, similarity_threshold = null) {
+        super();
+        this.embeddings = embeddings;
+        this.similarity_fn = similarity_fn;
+        this.k = k;
+        this.similarity_threshold = similarity_threshold;
+    }
+
+    // noinspection JSUnusedGlobalSymbols
+    validate_params(values) {
+        if (values['k'] === null && values['similarity_threshold'] === null) {
+            throw new Error('Must specify one of `k` or `similarity_threshold`.');
+        }
+        return values;
+    }
+
+    /**
+     * compresses the document
+     * @param {import('langchain/document').Document[]} documents
+     * @param {string} query
+     * @return {Promise<import('langchain/document').Document[]>}
+     */
+    async compressDocuments(documents, query) {
+        const stateful_documents = get_stateful_documents(documents);
+        const embedded_documents = _get_embeddings_from_stateful_docs(this.embeddings, stateful_documents);
+        const embedded_query = await this.embeddings.embedQuery(query);
+        const similarity = this.similarity_fn([embedded_query], embedded_documents)[0];
+        let included_idxs = Array.from(
+            {length: embedded_documents.length},
+            (_, i) => i);
+        if (this.k !== null) {
+            included_idxs = similarity.map((val, idx) => [val, idx]).sort((a, b) => b[0] - a[0]).slice(0, this.k).map(([_, idx]) => idx);
+        }
+        if (this.similarity_threshold !== null) {
+            const similar_enough = included_idxs.filter(idx => similarity[`${idx}`] > this.similarity_threshold);
+            included_idxs = similar_enough;
+        }
+        return included_idxs.map(i => stateful_documents[`${i}`]);
+    }
+}
+
+module.exports = {EmbeddingsFilter};
---
+++ src/chatgpt/extensions/embeddings_redundant_filter.js
@@ -0,0 +1,73 @@
+/**
+ * This file is converted since it is in python but not yet in javascript
+ * https://github.com/langchain-ai/langchain/blob/master/libs/langchain/langchain/document_transformers/embeddings_redundant_filter.py
+ */
+const {Document} = require('langchain/document');
+
+class _DocumentWithState extends Document {
+    constructor({pageContent, metadata, state = {}}) {
+        super({pageContent, metadata});
+        this.state = state;
+    }
+
+    // noinspection JSUnusedGlobalSymbols
+    to_document() {
+        return new Document({pageContent: this.pageContent, metadata: this.metadata});
+    }
+
+    static from_document(doc) {
+        if (doc instanceof this) {
+            return doc;
+        }
+        return new _DocumentWithState(
+            {
+                pageContent: doc.pageContent,
+                metadata: doc.metadata
+            }
+        );
+    }
+}
+
+/**
+ * gets stateful docs
+ * @param {import('langchain/document').Document[]} documents
+ * @return {_DocumentWithState[]}
+ */
+function get_stateful_documents(documents) {
+    return documents.map(doc => _DocumentWithState.from_document(doc));
+}
+
+/**
+ * zips two arrays
+ * @param arrays
+ * @return {*[][]}
+ */
+function zip(...arrays) {
+    const length = Math.min(...arrays.map(arr => arr.length));
+    return Array.from({length}, (_, i) => arrays.map(array => array[`${i}`]));
+}
+
+/**
+ * gets embeddings from stateful docs
+ * @param {import('langchain/embeddings').Embeddings} embeddings
+ * @param {_DocumentWithState[]} documents
+ * @return {number[][]}
+ */
+function _get_embeddings_from_stateful_docs(embeddings, documents) {
+    let embedded_documents;
+    if (documents.length && 'embedded_doc' in documents[0].state) {
+        embedded_documents = documents.map(doc => doc.state['embedded_doc']);
+    } else {
+        embedded_documents = embeddings.embedDocuments(documents.map(d => d.pageContent));
+        for (const [doc, embedding] of zip(documents, embedded_documents)) {
+            doc.state['embedded_doc'] = embedding;
+        }
+    }
+    return embedded_documents;
+}
+
+
+module.exports = {
+    get_stateful_documents,
+    _get_embeddings_from_stateful_docs
+};
