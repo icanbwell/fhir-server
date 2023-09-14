742da264195df3b5370776c88821e82890dd266b
Mintu Sah <mintu.sah@joshtechnologygroup.com>
2023-09-14 18:38:11 +0530

    EFS-548 Add few more test cases

--- src/tests/searchParameters/search_by_proxy_patient/search_by_multiple_proxy_patient/search_by_multiple_proxy_patient.test.js
+++ src/tests/searchParameters/search_by_proxy_patient/search_by_multiple_proxy_patient/search_by_multiple_proxy_patient.test.js
@@ -9,9 +9,16 @@ const bwellPerson2 = require('./fixtures/Person/bwellPerson2.json');
 const northwellPerson1 = require('./fixtures/Person/northwellPerson1.json');
 const northwellPerson2 = require('./fixtures/Person/northwellPerson2.json');
 
+const observation1 = require('./fixtures/Observations/observation1.json');
+const observation2 = require('./fixtures/Observations/observation2.json');
+const observation3 = require('./fixtures/Observations/observation3.json');
+const observation4 = require('./fixtures/Observations/observation4.json');
+
 // expected
 const expectedSinglePerson = require('./expected/expectedSinglePerson.json');
 const expectedMultiplePerson = require('./expected/expectedMultiplePerson.json');
+const expectedPatientsWithProxyPatient = require('./expected/expectedPatientsWithProxyPatient.json');
+const expectedObservationsWithProxyPatients = require('./expected/expectedObservationWithProxyPatient.json');
 
 const {
     commonBeforeEach,
@@ -76,7 +83,6 @@ describe('Patient Tests', () => {
                     '/4_0_0/Person/?_bundle=1&patient=Patient/person.54808e62-6445-4bb6-8f89-b2ed7e6865d2'
                 )
                 .set(getHeaders());
-            console.log(JSON.stringify(JSON.parse(resp.text), null, '\t'));
             // noinspection JSUnresolvedFunction
             expect(resp).toHaveResponse(expectedSinglePerson);
         });
@@ -111,9 +117,79 @@ describe('Patient Tests', () => {
                     '/4_0_0/Person/?_bundle=1&patient=Patient/person.54808e62-6445-4bb6-8f89-b2ed7e6865d2,Patient/person.cda43a72-b5e0-476a-a928-4d768e66d6f8'
                 )
                 .set(getHeaders());
-            console.log(JSON.stringify(JSON.parse(resp.text), null, '\t'));
             // noinspection JSUnresolvedFunction
             expect(resp).toHaveResponse(expectedMultiplePerson);
+        });
+
+        test('search patient by proxy-person should work correctly', async () => {
+            const request = await createTestRequest((c) => {
+                c.register('configManager', () => new MockConfigManager());
+                return c;
+            });
+            // ARRANGE
+            // add the resources to FHIR server
+            let resp = await request
+                .post('/4_0_0/Person/1/$merge?validate=true')
+                .send([
+                    bwellPerson1,
+                    bwellPerson2,
+                    northwellPerson1,
+                    northwellPerson2,
+                    patient1,
+                    patient2,
+                    patient3,
+                    patient4,
+                ])
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({ created: true });
+
+            // ACT & ASSERT
+            // search by token system and code and make sure we get the right Patient back
+            resp = await request
+                .get(
+                    '/4_0_0/Patient/?id=person.54808e62-6445-4bb6-8f89-b2ed7e6865d2,person.cda43a72-b5e0-476a-a928-4d768e66d6f8'
+                )
+                .set(getHeaders());
+            expect(resp).toHaveResponse(expectedPatientsWithProxyPatient);
+
+        });
+
+        test('search observation by proxy-person should work correctly with multiple proxy persons', async () => {
+            const request = await createTestRequest((c) => {
+                c.register('configManager', () => new MockConfigManager());
+                return c;
+            });
+            // ARRANGE
+            // add the resources to FHIR server
+            let resp = await request
+                .post('/4_0_0/Person/1/$merge?validate=true')
+                .send([
+                    bwellPerson1,
+                    bwellPerson2,
+                    northwellPerson1,
+                    northwellPerson2,
+                    patient1,
+                    patient2,
+                    patient3,
+                    patient4,
+                    observation1,
+                    observation2,
+                    observation3,
+                    observation4,
+                ])
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({ created: true });
+            // ACT & ASSERT
+            // search by token system and code and make sure we get the right Patient back
+            resp = await request
+                .get(
+                    '/4_0_0/Observation/?patient=Patient/person.54808e62-6445-4bb6-8f89-b2ed7e6865d2,Patient/person.cda43a72-b5e0-476a-a928-4d768e66d6f8'
+                )
+                .set(getHeaders());
+            expect(resp).toHaveResponse(expectedObservationsWithProxyPatients);
+
         });
     });
 });
