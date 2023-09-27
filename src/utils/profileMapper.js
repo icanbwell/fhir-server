45c6f81cb0d68f90b55242cc4653e9c1bad1bac4
Mintu Sah <mintu.sah@joshtechnologygroup.com>
2023-09-27 16:21:29 +0530

    EFS-574 Add canonicalUrl to profile map supported by hl7 R4

--- src/utils/profileMapper.js
+++ src/utils/profileMapper.js
@@ -1,93 +1,7 @@
-const { VERSIONS } = require('../middleware/fhir/utils/constants');
-
-const canonicalToOriginalUrl = {
-    [VERSIONS['4_0_0']]: {
-        AuditEvent: {
-            'http://hl7.org/fhir/StructureDefinition/ehrsrle-auditevent':
-                'http://hl7.org/fhir/R4/ehrsrle/ehrsrle-auditevent.profile.json',
-        },
-        ElementDefinition: {
-            'http://hl7.org/fhir/StructureDefinition/elementdefinition-de':
-                'http://hl7.org/fhir/R4/elementdefinition-de.profile.json',
-        },
-        ActivityDefinition: {
-            'http://hl7.org/fhir/StructureDefinition/shareableactivitydefinition':
-                'http://hl7.org/fhir/R4/shareableactivitydefinition.profile.json',
-        },
-        CodeSystem: {
-            'http://hl7.org/fhir/R4/shareableactivitydefinition.profile.json.html':
-                'http://hl7.org/fhir/R4/shareablecodesystem.profile.json',
-        },
-        Composition: {
-            'http://hl7.org/fhir/StructureDefinition/clinicaldocument':
-                'http://hl7.org/fhir/R4/clinicaldocument.profile.json',
-            'http://hl7.org/fhir/StructureDefinition/catalog':
-                'http://hl7.org/fhir/R4/catalog.profile.json',
-        },
-        DiagnosticReport: {
-            'http://hl7.org/fhir/StructureDefinition/lipidprofile':
-                'http://hl7.org/fhir/R4/lipidprofile.profile.json',
-            'http://hl7.org/fhir/StructureDefinition/cholesterol':
-                'http://hl7.org/fhir/R4/cholesterol.profile.json',
-            'http://hl7.org/fhir/R4/triglyceride':
-                'http://hl7.org/fhir/R4/triglyceride.profile.json',
-            'http://hl7.org/fhir/StructureDefinition/hdlcholesterol':
-                'http://hl7.org/fhir/R4/hdlcholesterol.profile.json',
-            'http://hl7.org/fhir/StructureDefinition/ldlcholesterol':
-                'http://hl7.org/fhir/R4/ldlcholesterol.profile.json',
-            'http://hl7.org/fhir/StructureDefinition/hlaresult':
-                'http://hl7.org/fhir/R4/hlaresult.profile.json',
-            'http://hl7.org/fhir/StructureDefinition/diagnosticreport-genetics':
-                'http://hl7.org/fhir/R4/diagnosticreport-genetics.profile.json',
-        },
-        Evidence: {
-            'http://hl7.org/fhir/StructureDefinition/synthesis':
-                'http://hl7.org/fhir/R4/synthesis.profile.json',
-        },
-        EvidenceVariable: {
-            'http://hl7.org/fhir/StructureDefinition/picoelement':
-                'http://hl7.org/fhir/R4/picoelement.profile.json.html',
-        },
-        FamilyMemberHistory: {
-            'http://hl7.org/fhir/StructureDefinition/familymemberhistory-genetic':
-                'http://hl7.org/fhir/R4/familymemberhistory-genetic.profile.json.html',
-        },
-        Group: {
-            'http://hl7.org/fhir/StructureDefinition/groupdefinition':
-                'http://hl7.org/fhir/R4/groupdefinition.profile.json',
-            'http://hl7.org/fhir/StructureDefinition/actualgroup':
-                'http://hl7.org/fhir/R4/actualgroup.profile.json',
-        },
-        GuidanceResponse: {
-            'http://hl7.org/fhir/StructureDefinition/cdshooksguidanceresponse':
-                'http://hl7.org/fhir/R4/cdshooksguidanceresponse.profile.json',
-        },
-        Library: {
-            'http://hl7.org/fhir/StructureDefinition/shareablelibrary':
-                'http://hl7.org/fhir/R4/shareablelibrary.profile.json',
-            'http://hl7.org/fhir/StructureDefinition/cqllibrary':
-                'http://hl7.org/fhir/R4/cqllibrary.profile.json',
-        },
-        Measure: {
-            'http://hl7.org/fhir/StructureDefinition/shareablemeasure':
-                'http://hl7.org/fhir/R4/shareablemeasure.profile.json',
-        },
-        Observation: {
-            'http://hl7.org/fhir/StructureDefinition/devicemetricobservation':
-                'http://hl7.org/fhir/R4/devicemetricobservation.profile.json',
-            'http://hl7.org/fhir/StructureDefinition/observation-genetics':
-                'http://hl7.org/fhir/R4/observation-genetics.profile.json',
-            'http://hl7.org/fhir/StructureDefinition/heartrate':
-                'http://hl7.org/fhir/R4/heartrate.profile.json',
-        },
-        PlanDefinition: {},
-        Provenance: {},
-        Questionnaire: {},
-        RequestGroup: {},
-        ServiceRequest: {},
-        ValueSet: {},
-    },
-};
+/**
+ * Currently supports profiles from http://hl7.org/fhir/R4/profilelist.html
+ */
+const { canonicalToOriginalUrlMap } = require('./data/profile.map');
 
 /**
  * Maps the canonical-url to its json url.
@@ -95,17 +9,33 @@ const canonicalToOriginalUrl = {
  */
 class ProfileUrlMapper {
     constructor() {
-        const mapper = canonicalToOriginalUrl['4_0_0'];
-        for (const profile in mapper) {
-            const profiles = mapper[`${profile}`];
+        const mapper = canonicalToOriginalUrlMap['4_0_0'];
+        const urls = Object.create({});
+
+        /**@type {Set<string>} */
+        this._supportedResources = new Set();
+
+        for (const resourceName in mapper) {
+            this.supportedResources.add(resourceName);
+            const profiles = mapper[`${resourceName}`];
             for (const canonicalUrl in profiles) {
-                Object.defineProperty(this, canonicalUrl, {
+                Object.defineProperty(urls, canonicalUrl, {
                     enumerable: true,
                     value: profiles[`${canonicalUrl}`],
                     writable: false,
                 });
             }
         }
+
+        Object.defineProperty(this, '_canonicalToOriginalMap', {
+            enumerable: true,
+            value: urls,
+            writable: false,
+        });
+    }
+
+    get supportedResources() {
+        return this._supportedResources;
     }
 
     /**
@@ -114,7 +44,7 @@ class ProfileUrlMapper {
      * @returns {string} Url
      */
     getOriginalUrl(canonicalUrl) {
-        return this[`${canonicalUrl}`] ?? canonicalUrl;
+        return this._canonicalToOriginalMap[`${canonicalUrl}`] ?? canonicalUrl;
     }
 }
 
