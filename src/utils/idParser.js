4d1de0a13e34f21bde0be1ab20620fb43bcc5bd5
drcate <drcate@pobox.com>
2023-10-03 10:13:26 -0700

    EFS-598 Throw NotValidatedError for invalid id

--- src/utils/idParser.js
+++ src/utils/idParser.js
@@ -1,9 +1,16 @@
+
+const OperationOutcome = require('../fhir/classes/4_0_0/resources/operationOutcome');
+const OperationOutcomeIssue = require('../fhir/classes/4_0_0/backbone_elements/operationOutcomeIssue');
+const CodeableConcept = require('../fhir/classes/4_0_0/complex_types/codeableConcept');
+const {NotValidatedError} = require('./httpErrors');
+
 class IdParser {
     /**
      * Parses id string
      * The id can be '123|medstar' or '123'
      * @param {string} id
      * @return {{ id: string, sourceAssigningAuthority: string|undefined}}
+     * @throws NotValidatedError
      */
     static parse(id) {
         let id1;
@@ -12,7 +19,20 @@ class IdParser {
         if (id && typeof id === 'string') {
             idParts = id.split('|');
         } else {
-            return {id: '', sourceAssigningAuthority: undefined};
+            const operationOutcome = new OperationOutcome({
+                id: 'validationfail',
+                resourceType: 'OperationOutcome',
+                issue: [
+                    new OperationOutcomeIssue({
+                        severity: 'error',
+                        code: 'structure',
+                        details: new CodeableConcept({
+                            text: `Invalid parameter id = ${id}`
+                        })
+                    })
+                ]
+            });
+            throw new NotValidatedError(operationOutcome);
         }
         if (idParts.length > 1) {
             id1 = idParts[0];
