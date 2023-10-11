a5fb57dc29345d77a62996a9de35f287f225c977
007sunny <sunny.singhal@joshtechnologygroup.com>
2023-10-11 15:05:04 +0530

    EFS-621 Changed trace exporter from console to OLTP

--- src/instrumentation.js
+++ src/instrumentation.js
@@ -1,25 +1,21 @@
 'use strict';
 
 const { NodeSDK } = require('@opentelemetry/sdk-node');
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
+const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-grpc');
 const { Resource } = require('@opentelemetry/resources');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-const {
-    PeriodicExportingMetricReader,
-    ConsoleMetricExporter,
-} = require('@opentelemetry/sdk-metrics');
-const { initialize, getLogger } = require('./winstonInit');
-
-initialize();
-const logger = getLogger();
-
-console.dir = (data) => logger.info(data);
+const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
 
 const sdk = new NodeSDK({
     resource: new Resource(),
-    traceExporter: new ConsoleSpanExporter(),
+    traceExporter: new OTLPTraceExporter({
+        url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
+    }),
     metricReader: new PeriodicExportingMetricReader({
-        exporter: new ConsoleMetricExporter(),
+        exporter: new OTLPMetricExporter({
+            url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
+        }),
     }),
     instrumentations: [getNodeAutoInstrumentations()],
 });
