18d97570d6ce5b45577b9b2495870c7e714157b3
007sunny <sunny.singhal@joshtechnologygroup.com>
2023-10-11 15:35:05 +0530

    EFS-621 Removed url as it will be picked directly from env in exporters

--- src/instrumentation.js
+++ src/instrumentation.js
@@ -1,21 +1,17 @@
 'use strict';
 
-const { NodeSDK } = require('@opentelemetry/sdk-node');
+const opentelemetry = require('@opentelemetry/sdk-node');
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
 const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-grpc');
 const { Resource } = require('@opentelemetry/resources');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
 
-const sdk = new NodeSDK({
+const sdk = new opentelemetry.NodeSDK({
     resource: new Resource(),
-    traceExporter: new OTLPTraceExporter({
-        url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
-    }),
+    traceExporter: new OTLPTraceExporter(),
     metricReader: new PeriodicExportingMetricReader({
-        exporter: new OTLPMetricExporter({
-            url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
-        }),
+        exporter: new OTLPMetricExporter(),
     }),
     instrumentations: [getNodeAutoInstrumentations()],
 });
