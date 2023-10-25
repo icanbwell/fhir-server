244a1ee1b6ec74dac387f972667f6ab1343c0191
Mintu Sah <mintu.sah@joshtechnologygroup.com>
2023-10-25 19:20:55 +0530

    EFS-651 Update addProxyPatientToConsentResource Script, link clientPerson instead of master-person

--- src/tests/admin/runners/addProxyPersonToConsentResource/addProxyPersonToConsentResource.test.js
+++ src/tests/admin/runners/addProxyPersonToConsentResource/addProxyPersonToConsentResource.test.js
@@ -21,12 +21,18 @@ const consent1 = require('./fixtures/consent/consent1.json');
 const consent2 = require('./fixtures/consent/consent2.json');
 const consentWithActor = require('./fixtures/consent/consent_with_actor.json');
 const consentWithProxyPerson = require('./fixtures/consent/consent_with_proxy_person_reference.json');
+const consent2WithBwellPerson = require('./fixtures/consent/consent2_with_master_person_reference.json');
+const consentWithWrongClientPerson = require('./fixtures/consent/consent_with_wrong_client_person.json');
+const consentWithClientPersonAsActor = require('./fixtures/consent/consent_with_client_person.json');
 
 // expected
 const expConsent1 = require('./fixtures/expected/consent1.json');
 const expConsent2 = require('./fixtures/expected/consent2.json');
 const expConsentWithActor = require('./fixtures/expected/consent_with_actor.json');
 const expConsentWithProxyPerson = require('./fixtures/expected/consent_with_proxy_person_reference.json');
+const expConsentWithClientPersonAsActor = require('./fixtures/expected/consent_with_client_person_as_actor.json');
+const expConsent2WithClientPersonAsActor = require('./fixtures/expected/consent_2_with_client_person_as_actor.json');
+const expConsentWithCorrectClientPersonAsActor = require('./fixtures/expected/consent_with_correct_client_person_as_actor.json');
 
 describe('Add Proxy Person link to Consent Resources Test', () => {
     beforeEach(async () => {
@@ -142,11 +148,9 @@ describe('Add Proxy Person link to Consent Resources Test', () => {
         resp = await request.get('/4_0_0/Consent').set(getHeaders()).expect(200);
         expect(resp).toHaveResourceCount(1);
         expect(resp).toHaveResponse([expConsentWithActor]);
-
-        console.log(JSON.stringify(JSON.parse(resp.text), undefined, '\t'));
     });
 
-    test('should update the consent resources only when proxy person reference is not present', async () => {
+    test('should update the consent resources when master person reference is present', async () => {
         const request = await createTestRequest();
 
         // add the resources to FHIR server
@@ -197,6 +201,113 @@ describe('Add Proxy Person link to Consent Resources Test', () => {
         expect(resp).toHaveResourceCount(2);
         expect(resp).toHaveResponse([expConsentWithActor, expConsentWithProxyPerson]);
 
-        console.log(JSON.stringify(JSON.parse(resp.text), undefined, '\t'));
+    });
+
+    test('should update the consent resources only when proxy person reference is not present', async () => {
+        const request = await createTestRequest();
+
+        // add the resources to FHIR server
+        let resp = await request
+            .post('/4_0_0/Person/$merge')
+            .send([
+                bwellPerson1,
+                bwellPerson2,
+                walgreensPerson,
+                medstartPerson,
+                walgreensPatient,
+                medstartPatient,
+                consentWithClientPersonAsActor,
+            ])
+            .set(getHeaders())
+            .expect(200);
+        expect(resp).toHaveResourceCount(7);
+        // run the script
+        const container = getTestContainer();
+        const collections = ['Consent_4_0_0'];
+        const batchSize = 1000;
+
+        // now add our class
+        container.register(
+            'addProxyPatientToConsentResourceRunner',
+            (c) =>
+                new AddProxyPatientToConsentResourceRunner({
+                    mongoCollectionManager: c.mongoCollectionManager,
+                    mongoDatabaseManager: c.mongoDatabaseManager,
+                    collections,
+                    batchSize,
+                    adminLogger: new AdminLogger(),
+                    bwellPersonFinder: c.bwellPersonFinder,
+                    preSaveManager: c.preSaveManager,
+                })
+        );
+
+        const addProxyPatientToConsentResourceRunner =
+            container.addProxyPatientToConsentResourceRunner;
+        expect(
+            typeof addProxyPatientToConsentResourceRunner === AddProxyPatientToConsentResourceRunner
+        );
+
+        await addProxyPatientToConsentResourceRunner.processAsync();
+
+        resp = await request.get('/4_0_0/Consent').set(getHeaders()).expect(200);
+        expect(resp).toHaveResourceCount(1);
+        expect(resp).toHaveResponse([expConsentWithClientPersonAsActor]);
+    });
+
+    test('should update the consent resources fill it with correct client person reference', async () => {
+        const request = await createTestRequest();
+
+        // add the resources to FHIR server
+        let resp = await request
+            .post('/4_0_0/Person/$merge')
+            .send([
+                bwellPerson1,
+                bwellPerson2,
+                walgreensPerson,
+                medstartPerson,
+                walgreensPatient,
+                medstartPatient,
+                consentWithActor,
+                consent2WithBwellPerson,
+                consentWithWrongClientPerson,
+            ])
+            .set(getHeaders())
+            .expect(200);
+        expect(resp).toHaveResourceCount(9);
+        // run the script
+        const container = getTestContainer();
+        const collections = ['Consent_4_0_0'];
+        const batchSize = 1000;
+
+        // now add our class
+        container.register(
+            'addProxyPatientToConsentResourceRunner',
+            (c) =>
+                new AddProxyPatientToConsentResourceRunner({
+                    mongoCollectionManager: c.mongoCollectionManager,
+                    mongoDatabaseManager: c.mongoDatabaseManager,
+                    collections,
+                    batchSize,
+                    adminLogger: new AdminLogger(),
+                    bwellPersonFinder: c.bwellPersonFinder,
+                    preSaveManager: c.preSaveManager,
+                })
+        );
+
+        const addProxyPatientToConsentResourceRunner =
+            container.addProxyPatientToConsentResourceRunner;
+        expect(
+            typeof addProxyPatientToConsentResourceRunner === AddProxyPatientToConsentResourceRunner
+        );
+
+        await addProxyPatientToConsentResourceRunner.processAsync();
+
+        resp = await request.get('/4_0_0/Consent').set(getHeaders()).expect(200);
+        expect(resp).toHaveResourceCount(3);
+        expect(resp).toHaveResponse([
+            expConsent2WithClientPersonAsActor,
+            expConsentWithCorrectClientPersonAsActor,
+            expConsentWithClientPersonAsActor,
+        ]);
     });
 });
