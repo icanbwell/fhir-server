a5de90672fe6c445f9ca21fff9d7886c85739eb8
Mintu Sah <mintu.sah@joshtechnologygroup.com>
2023-10-26 18:41:26 +0530

    EFS-653 Optimize search query based on id, use $in instead of $or

--- src/operations/query/filters/id.js
+++ src/operations/query/filters/id.js
@@ -66,6 +66,84 @@ class FilterById extends BaseFilter {
         return query;
 
     }
+
+    /**
+     * Generates filter for parsedArgItem
+     * @return {import('mongodb').Filter<import('mongodb').DefaultSchema>[]}
+     */
+    filter() {
+        /**
+         * @type {import('mongodb').Filter<import('mongodb').DefaultSchema>[]}
+         */
+        const and_segments = [];
+
+        if (this.parsedArg.queryParameterValue.values) {
+            and_segments.push({
+                    $or: this.propertyObj.fields.flatMap((field) => {
+                            return {
+                                [this.parsedArg.queryParameterValue.operator]:
+                                this.filterByItems(field, this.parsedArg.queryParameterValue.values)
+                            };
+                        }
+                    ),
+                },
+            );
+        }
+
+        return and_segments;
+    }
+
+    /**
+     * Generate filter based of field and values
+     * @param {string} field
+     * @param {string[]} values
+     * @returns {Array<Object>}
+     */
+    filterByItems(field, values) {
+        if (this.enableGlobalIdSupport) {
+            const filters = [];
+            /**
+             * 3 types of values are possible
+             * 1. uuid, 2. sourceId, 3. sourceId and sourceAssigningAuthority
+             */
+            let /**@type {string[]}*/uuids = [], /**@type {string[]}*/sourceIds = [], /**@type {string[]}*/sourceIdsWithSourceAssigningAuthority = [];
+
+            values.forEach((value) => {
+                const {id, sourceAssigningAuthority} = IdParser.parse(value);
+                if (isUuid(id)) {
+                    uuids.push(id);
+                } else if (sourceAssigningAuthority) {
+                    sourceIdsWithSourceAssigningAuthority.push(value);
+                } else {
+                    sourceIds.push(id);
+                }
+            });
+
+            if (uuids.length > 0) {
+                filters.push({
+                    [this.fieldMapper.getFieldName(uuidFieldName)]: {
+                        $in: uuids,
+                    }
+                });
+            }
+
+            if (sourceIds.length > 0) {
+                filters.push({
+                    [this.fieldMapper.getFieldName(field)]: {
+                        $in: sourceIds,
+                    }
+                });
+            }
+
+            if (sourceIdsWithSourceAssigningAuthority.length > 0) {
+                filters.push(...sourceIdsWithSourceAssigningAuthority.flatMap(v => this.filterByItem(field, v)));
+            }
+            return filters;
+        } else {
+            return values.flatMap((v) => this.filterByItem(field, v));
+        }
+    }
+
 }
 
 module.exports = {
