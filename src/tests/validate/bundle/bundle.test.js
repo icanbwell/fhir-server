b084414fca6fb7f46da63d971e3e954f15415ead
Imran Qureshi <imranq2@hotmail.com>
2023-10-27 19:36:49 -0700

    truncate ids of resources in bundles

--- src/tests/validate/bundle/bundle.test.js
+++ src/tests/validate/bundle/bundle.test.js
@@ -2,7 +2,7 @@
 const bundle1Resource = require('./fixtures/Bundle/bundle1.json');
 
 // expected
-const expectedBundleResources = require('./fixtures/expected/expected_bundle.json');
+const expectedBundleValidation = require('./fixtures/expected/expected_bundle_validation.json');
 
 const {commonBeforeEach, commonAfterEach, getHeaders, createTestRequest} = require('../../common');
 const {describe, beforeEach, afterEach, test} = require('@jest/globals');
@@ -17,7 +17,7 @@ describe('Bundle Tests', () => {
     });
 
     describe('Bundle bundle Tests', () => {
-        test('bundle works', async () => {
+        test('bundle validates with long id for resource 4', async () => {
             const request = await createTestRequest();
             let resp = await request
                 .get('/4_0_0/Bundle')
@@ -30,7 +30,22 @@ describe('Bundle Tests', () => {
                 .send(bundle1Resource)
                 .set(getHeaders());
             // noinspection JSUnresolvedFunction
-            expect(resp).toHaveResponse(expectedBundleResources);
+            expect(resp).toHaveResponse(expectedBundleValidation);
+        });
+        test('bundle saves with long id for resource 4', async () => {
+            const request = await createTestRequest();
+            let resp = await request
+                .get('/4_0_0/Bundle')
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResourceCount(0);
+
+            resp = await request
+                .post('/4_0_0/Bundle/1/$merge')
+                .send(bundle1Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({created: true});
         });
     });
 });
