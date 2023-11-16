bb814ec527168cdd8b377934df339ded7ca6a835
Mintu Sah <mintu.sah@icanbwell.com>
2023-11-16 14:01:11 +0530

    EFS-675 Updat dateTime filter to generate correct range query when column is of date type

--- src/operations/query/filters/dateTime.js
+++ src/operations/query/filters/dateTime.js
@@ -53,6 +53,97 @@ class FilterByDateTime extends BaseFilter {
             };
         }
     }
+
+    /**
+     * filter function that calls filterByItem for each field and each value supplied
+     * @return {import('mongodb').Filter<import('mongodb').DefaultSchema>[]}
+     */
+    filter() {
+        /**
+         * @type {import('mongodb').Filter<import('mongodb').DefaultSchema>[]}
+         */
+        const and_segments = [];
+
+        if (this.parsedArg.queryParameterValue.values) {
+            and_segments.push({
+                    $or: this.propertyObj.fields.flatMap((field) => {
+                            return this.filterByField(field, this.parsedArg.queryParameterValue);
+                        }
+                    ),
+                },
+            );
+        }
+
+        return and_segments;
+    }
+
+    /**
+     * Generate filter for each field
+     * @param {string} field
+     * @param {import('../queryParameterValue').QueryParameterValue} queryParameterValue
+     */
+    filterByField(field, queryParameterValue) {
+        const childQueries = queryParameterValue.values.flatMap((v) => {
+            return this.filterByItem(field, v);
+        });
+
+        const query = {
+            [queryParameterValue.operator]: childQueries
+        };
+
+        if (
+            field === 'meta.lastUpdated' ||
+            isColumnDateType(this.resourceType, this.fieldMapper.getFieldName(field))
+        ) {
+            const simplifiedRangeQuery = {};
+            const newChildQueries = [];
+            // correct the query
+            const fieldName = this.fieldMapper.getFieldName(field);
+            const possibleRanges = ['$lt', '$lte', '$gt', '$gte'];
+
+            if (Array.isArray(childQueries)) {
+                childQueries.forEach((childQuery) => {
+                    const nestedRangeQuery = childQuery[`${fieldName}`];
+
+                    if (!nestedRangeQuery) {
+                        return;
+                    }
+                    // check if query doesn't have correct range query
+                    const nestedKeys = Object.keys(nestedRangeQuery);
+                    const canQueryBeSimplified =
+                        nestedKeys.length === 1 && nestedKeys.every((v) => possibleRanges.includes(v));
+                    if (canQueryBeSimplified) {
+                        nestedKeys.forEach((k) => {
+                            simplifiedRangeQuery[`${k}`] = nestedRangeQuery[`${k}`];
+                        });
+                    }
+                    else {
+                        newChildQueries.push(childQuery);
+                    }
+                });
+            }
+
+            // simplify the range query
+            if (simplifiedRangeQuery['$lt'] && simplifiedRangeQuery['$lte']) {
+                // give more preference to $lte
+                delete simplifiedRangeQuery['$lt'];
+            }
+
+            if (simplifiedRangeQuery['$gt'] && simplifiedRangeQuery['$gte']) {
+                // give more preference to $gte
+                delete simplifiedRangeQuery['$gt'];
+            }
+
+            if (Object.keys(simplifiedRangeQuery).length > 0) {
+                newChildQueries.push({
+                    [fieldName]: simplifiedRangeQuery,
+                });
+            }
+
+            query[queryParameterValue.operator] = newChildQueries;
+        }
+        return query;
+    }
 }
 
 module.exports = {
