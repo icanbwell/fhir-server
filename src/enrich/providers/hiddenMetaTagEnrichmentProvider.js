871f156272226dc844874af5a6d0bc56cd09904c
Imran Qureshi <imranq2@hotmail.com>
2023-12-15 10:32:04 -0800

    implement hidden meta tag

--- src/enrich/providers/hiddenMetaTagEnrichmentProvider.js
+++ src/enrich/providers/hiddenMetaTagEnrichmentProvider.js
@@ -1,8 +1,18 @@
 const {EnrichmentProvider} = require('./enrichmentProvider');
 
 class HiddenMetaTagEnrichmentProvider extends EnrichmentProvider {
+    /**
+     * enrich the specified resources
+     * @param {Resource[]} resources
+     * @param {ParsedArgs} parsedArgs
+     * @return {Promise<Resource[]>}
+     */
     // eslint-disable-next-line no-unused-vars
     async enrichAsync({resources, parsedArgs}) {
+        // if id is specified, return all resources
+        if (parsedArgs.id) {
+            return resources;
+        }
         // skip resources that have the meta.tag of
         // system=https://fhir.icanbwell.com/4_0_0/CodeSystem/server-behavior
         // code=hidden
@@ -16,6 +26,10 @@ class HiddenMetaTagEnrichmentProvider extends EnrichmentProvider {
 
     // eslint-disable-next-line no-unused-vars
     async enrichBundleEntriesAsync({entries, parsedArgs}) {
+        // if id is specified, return all resources
+        if (parsedArgs.id) {
+            return entries;
+        }
         // skip resources that have the meta.tag of
         // system=https://fhir.icanbwell.com/4_0_0/CodeSystem/server-behavior
         // code=hidden
