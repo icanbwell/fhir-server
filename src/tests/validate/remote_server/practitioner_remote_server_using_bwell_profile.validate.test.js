26bf3047548f0b0566e59c7b5e019aacfcf606c6
Imran Qureshi <imranq2@hotmail.com>
2023-12-24 16:42:54 -0800

    set profile in meta tag before calling HAPI fhir server to validate

--- src/tests/validate/remote_server/practitioner_remote_server_using_bwell_profile.validate.test.js
+++ src/tests/validate/remote_server/practitioner_remote_server_using_bwell_profile.validate.test.js
@@ -1,6 +1,7 @@
 const invalidPractitionerResourceUsingBwellProfile = require('./fixtures/invalid_practitioner_using_bwell_profile.json');
 
-const expectedValidPractitionerResponse = require('./expected/invalid_practitioner_using_bwell_profile.json');
+const expectedInvalidPractitionerResponseForUSCore = require('./expected/invalid_practitioner_using_us_core_profile.json');
+const expectedInvalidPractitionerResponseForBwellProfile = require('./expected/invalid_practitioner_using_bwell_profile.json');
 
 const {
     commonBeforeEach,
@@ -52,7 +53,7 @@ describe('Practitioner Update Tests', () => {
                 .get('/4_0_0/Practitioner/fe274326-fd32-4a06-a68d-9435aed8af98/$validate?profile=http://hl7.org/fhir/us/core/StructureDefinition/us-core-practitioner')
                 .set(getHeaders());
             // noinspection JSUnresolvedFunction
-            expect(resp).toHaveResponse(expectedValidPractitionerResponse,
+            expect(resp).toHaveResponse(expectedInvalidPractitionerResponseForUSCore,
                 resource => {
                     delete resource.details; // has lastUpdated
                     return resource;
@@ -81,7 +82,7 @@ describe('Practitioner Update Tests', () => {
                 .get('/4_0_0/Practitioner/fe274326-fd32-4a06-a68d-9435aed8af98/$validate?profile=https://fhir.simplifier.net/bwellFHIRProfiles/StructureDefinition/pr-practitioner')
                 .set(getHeaders());
             // noinspection JSUnresolvedFunction
-            expect(resp).toHaveResponse(expectedValidPractitionerResponse,
+            expect(resp).toHaveResponse(expectedInvalidPractitionerResponseForBwellProfile,
                 resource => {
                     delete resource.details; // has lastUpdated
                     return resource;
