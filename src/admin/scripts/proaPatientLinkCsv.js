9b51ef2defa7b98ed89fa0101f26d7d6643c1eaa
007sunny <sunny.singhal@joshtechnologygroup.com>
2024-02-09 17:47:56 +0530

    EFS-783 Created script to get proaPatient data graph

---
+++ src/admin/scripts/proaPatientLinkCsv.js
@@ -0,0 +1,63 @@
+// load config from .env.  Should be first thing so env vars are available to rest of the code
+const path = require('path');
+const dotenv = require('dotenv');
+const pathToEnv = path.resolve(__dirname, '.env');
+dotenv.config({
+    path: pathToEnv
+});
+console.log(`Reading config from ${pathToEnv}`);
+console.log(`MONGO_URL=${process.env.MONGO_URL}`);
+const { createContainer } = require('../../createContainer');
+const { AdminLogger } = require('../adminLogger');
+const { CommandLineParser } = require('./commandLineParser');
+const { ProaPatientLinkCsvRunner } = require('../runners/proaPatientLinkCsvRunner');
+
+/**
+ * main function
+ * @returns {Promise<void>}
+ */
+async function main() {
+    const parameters = CommandLineParser.parseCommandLine();
+
+    const batchSize = parameters.batchSize || process.env.BULK_BUFFER_SIZE || 10000;
+
+    const clientSourceAssigningAuthorities = parameters.clientSourceAssigningAuthorities ?
+        parameters.clientSourceAssigningAuthorities.split(',') :
+        ['bwell_demo'];
+
+    const adminLogger = new AdminLogger();
+
+    let currentDateTime = new Date();
+    adminLogger.logInfo(`[${currentDateTime}] Running proaPatientLinkCsvRunner script`);
+
+    // set up all the standard services in the container
+    const container = createContainer();
+
+    // now add our class
+    container.register('proaPatientLinkCsvRunner', (c) => new ProaPatientLinkCsvRunner({
+        mongoCollectionManager: c.mongoCollectionManager,
+        mongoDatabaseManager: c.mongoDatabaseManager,
+        adminLogger,
+        batchSize,
+        clientSourceAssigningAuthorities,
+    }));
+
+    /**
+     * @type {ProaPatientLinkCsvRunner}
+     */
+    const proaPatientLinkCsvRunner = container.proaPatientLinkCsvRunner;
+    await proaPatientLinkCsvRunner.processAsync();
+
+    adminLogger.logInfo('Exiting process');
+    process.exit(0);
+}
+
+/**
+ * To run this:
+ * nvm use
+ * node src/admin/scripts/proaPatientLinkCsv.js
+ * NODE_OPTIONS=--max_old_space_size=8192 node --max-old-space-size=8192 src/admin/scripts/proaPatientLinkCsv.js
+ */
+main().catch(reason => {
+    console.error(reason);
+});
