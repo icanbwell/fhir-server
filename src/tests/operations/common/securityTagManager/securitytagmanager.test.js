af18c04d95abc2e60b5d23efac96834fdeb2cac4
Imran Qureshi <imranq2@hotmail.com>
2024-02-24 15:29:45 -0800

    add test for Patient resource

--- src/tests/operations/common/securityTagManager/securitytagmanager.test.js
+++ src/tests/operations/common/securityTagManager/securitytagmanager.test.js
@@ -1,4 +1,5 @@
 // test file
+const patient1Resource = require('./fixtures/Patient/patient1.json');
 const condition1Resource = require('./fixtures/Condition/condition1.json');
 
 // expected
@@ -8,7 +9,9 @@ const expectedConditionResources = require('./fixtures/expected/expected_conditi
 const { describe, beforeEach, afterEach, test, expect } = require('@jest/globals');
 const { commonBeforeEach, commonAfterEach } = require('../../../common');
 const { createTestContainer } = require('../../../createTestContainer');
+const Patient = require('../../../../fhir/classes/4_0_0/resources/patient');
 const Condition = require('../../../../fhir/classes/4_0_0/resources/condition');
+const { generateUUIDv5 } = require('../../../../utils/uid.util');
 
 describe('SecurityTagManager Tests', () => {
     beforeEach(async () => {
@@ -19,8 +22,23 @@ describe('SecurityTagManager Tests', () => {
         await commonAfterEach();
     });
 
-    describe('Condition securityTagManager Tests', () => {
-        test('securityTagManager works', async () => {
+    describe('securityTagManager Tests', () => {
+        test('securityTagManager getValueOfPatientPropertyFromResource works for Patient', async () => {
+            /** @type {SimpleContainer} */
+            const container = createTestContainer();
+            /** @type {SecurityTagManager} */
+            const securityTagManager = container.securityTagManager;
+            /** @type {PreSaveManager} */
+            const preSaveManager = container.preSaveManager;
+            /** @type {Patient} */
+            const patient = new Patient(patient1Resource);
+            // generate all the uuids
+            await preSaveManager.preSaveAsync(patient);
+            const patientUuid = securityTagManager.getValueOfPatientPropertyFromResource({ resource: patient });
+            const expectedPatientUuid = generateUUIDv5(`${patient1Resource.id}|${patient1Resource.meta.security[0].code}`);
+            expect(patientUuid).toStrictEqual(expectedPatientUuid);
+        });
+        test('securityTagManager getValueOfPatientPropertyFromResource works for Condition', async () => {
             /** @type {SimpleContainer} */
             const container = createTestContainer();
             /** @type {SecurityTagManager} */
@@ -32,7 +50,10 @@ describe('SecurityTagManager Tests', () => {
             // generate all the uuids
             await preSaveManager.preSaveAsync(condition);
             const patientUuid = securityTagManager.getValueOfPatientPropertyFromResource({ resource: condition });
-            expect(patientUuid).toStrictEqual('24a5930e-11b4-5525-b482-669174917044');
+            const patientReference = condition.subject.reference;
+            const patientId = patientReference.split('/')[1];
+            const expectedPatientUuid = generateUUIDv5(`${patientId}|${condition.meta.security[0].code}`);
+            expect(patientUuid).toStrictEqual(expectedPatientUuid);
         });
     });
 });
