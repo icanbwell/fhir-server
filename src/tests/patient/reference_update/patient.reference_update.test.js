9bfc09e69986727d9326d5ac433a2ff3477e9dbc
007sunny <sunny.singhal@icanbwell.com>
2024-04-15 16:53:10 +0530

    EFS-921 Added logic to make main patient reference read-only (#1534)
    * EFS-921 Added logic to make main patient reference read-only
    
    * EFS-921 Minor change
    
    * EFS-921 Fixed testcases
    
    * EFS-921 Minor fix
    
    * EFS-921 Added comment

---
+++ src/tests/patient/reference_update/patient.reference_update.test.js
@@ -0,0 +1,217 @@
+// provider file
+const task1Resource = require('./fixtures/task/task1.json');
+const task2Resource = require('./fixtures/task/task2.json');
+const observation1Resource = require('./fixtures/observation/observation1.json');
+const observation2Resource = require('./fixtures/observation/observation2.json');
+
+// expected
+const expectedTaskResource = require('./fixtures/expected/expectedTask.json');
+const expectedObservationResource = require('./fixtures/expected/expectedObservation.json');
+
+const {
+    commonBeforeEach,
+    commonAfterEach,
+    getHeaders,
+    createTestRequest,
+    getHeadersJsonPatch
+} = require('../../common');
+const { describe, beforeEach, afterEach, test, expect } = require('@jest/globals');
+
+describe('Patient reference tests', () => {
+    beforeEach(async () => {
+        await commonBeforeEach();
+    });
+
+    afterEach(async () => {
+        await commonAfterEach();
+    });
+
+    describe('Patient reference update', () => {
+        test('Reference update using merge for Task', async () => {
+            const request = await createTestRequest();
+
+            let resp = await request
+                .post('/4_0_0/Task/$merge')
+                .send(task1Resource)
+                .set(getHeaders())
+                .expect(200);
+
+            expect(resp).toHaveMergeResponse({ created: true });
+
+            resp = await request
+                .get('/4_0_0/Task/1')
+                .set(getHeaders())
+                .expect(200);
+
+            expect(resp).toHaveResponse(expectedTaskResource);
+
+            resp = await request
+                .post('/4_0_0/Task/$merge')
+                .send(task2Resource)
+                .set(getHeaders())
+                .expect(200);
+
+            expect(resp).toHaveMergeResponse({ created: false, updated: false });
+
+            resp = await request
+                .get('/4_0_0/Task/1')
+                .set(getHeaders())
+                .expect(200);
+
+            expect(resp).toHaveResponse(expectedTaskResource);
+        });
+
+        test('Reference update using merge for Observation', async () => {
+            const request = await createTestRequest();
+
+            let resp = await request
+                .post('/4_0_0/Observation/$merge')
+                .send(observation1Resource)
+                .set(getHeaders())
+                .expect(200);
+
+            expect(resp).toHaveMergeResponse({ created: true });
+
+            resp = await request
+                .get('/4_0_0/Observation/1')
+                .set(getHeaders())
+                .expect(200);
+
+            expect(resp).toHaveResponse(expectedObservationResource);
+
+            resp = await request
+                .post('/4_0_0/Observation/$merge')
+                .send(observation2Resource)
+                .set(getHeaders())
+                .expect(200);
+
+            expect(resp).toHaveMergeResponse({ created: false, updated: false });
+
+            resp = await request
+                .get('/4_0_0/Observation/1')
+                .set(getHeaders())
+                .expect(200);
+
+            expect(resp).toHaveResponse(expectedObservationResource);
+        });
+
+        test('Reference update using update for Task', async () => {
+            const request = await createTestRequest();
+
+            let resp = await request
+                .post('/4_0_0/Task/$merge')
+                .send(task1Resource)
+                .set(getHeaders())
+                .expect(200);
+
+            expect(resp).toHaveMergeResponse({ created: true });
+
+            resp = await request
+                .get('/4_0_0/Task/1')
+                .set(getHeaders())
+                .expect(200);
+
+            expect(resp).toHaveResponse(expectedTaskResource);
+
+            resp = await request
+                .put('/4_0_0/Task/1')
+                .send(task2Resource)
+                .set(getHeaders())
+                .expect(200);
+
+            expect(resp).toHaveResponse(expectedTaskResource);
+        });
+
+        test('Reference update using merge for Observation', async () => {
+            const request = await createTestRequest();
+
+            let resp = await request
+                .post('/4_0_0/Observation/$merge')
+                .send(observation1Resource)
+                .set(getHeaders())
+                .expect(200);
+
+            expect(resp).toHaveMergeResponse({ created: true });
+
+            resp = await request
+                .get('/4_0_0/Observation/1')
+                .set(getHeaders())
+                .expect(200);
+
+            expect(resp).toHaveResponse(expectedObservationResource);
+
+            resp = await request
+                .put('/4_0_0/Observation/1')
+                .send(observation2Resource)
+                .set(getHeaders())
+                .expect(200);
+
+            expect(resp).toHaveResponse(expectedObservationResource);
+        });
+
+        test('Reference update using patch for Task', async () => {
+            const request = await createTestRequest();
+
+            let resp = await request
+                .post('/4_0_0/Task/$merge')
+                .send(task1Resource)
+                .set(getHeaders())
+                .expect(200);
+
+            expect(resp).toHaveMergeResponse({ created: true });
+
+            resp = await request
+                .get('/4_0_0/Task/1')
+                .set(getHeaders())
+                .expect(200);
+
+            expect(resp).toHaveResponse(expectedTaskResource);
+
+            resp = await request
+                .patch('/4_0_0/Task/1')
+                .send([
+                    {
+                        op: 'replace',
+                        path: '/for/reference',
+                        value: 'patient/2'
+                    }
+                ])
+                .set(getHeadersJsonPatch())
+
+            expect(resp).toHaveResponse(expectedTaskResource);
+        });
+
+        test('Reference update using patch for Observation', async () => {
+            const request = await createTestRequest();
+
+            let resp = await request
+                .post('/4_0_0/Observation/$merge')
+                .send(observation1Resource)
+                .set(getHeaders())
+                .expect(200);
+
+            expect(resp).toHaveMergeResponse({ created: true });
+
+            resp = await request
+                .get('/4_0_0/Observation/1')
+                .set(getHeaders())
+                .expect(200);
+
+            expect(resp).toHaveResponse(expectedObservationResource);
+
+            resp = await request
+                .patch('/4_0_0/Observation/1')
+                .send([
+                    {
+                        op: 'replace',
+                        path: '/subject/reference',
+                        value: 'patient/2'
+                    }
+                ])
+                .set(getHeadersJsonPatch())
+                .expect(200);
+
+            expect(resp).toHaveResponse(expectedObservationResource);
+        });
+    });
+});
