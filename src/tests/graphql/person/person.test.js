0d75c298f5a939500105417a351ea61b5793b9e8
007sunny <sunny.singhal@icanbwell.com>
2024-04-23 15:38:39 +0530

    EFS-943 Fixed query for graphql (#1546)

--- src/tests/graphql/person/person.test.js
+++ src/tests/graphql/person/person.test.js
@@ -93,6 +93,11 @@ describe('GraphQL Patient Tests', () => {
                 return r;
             });
             expect(resp.headers['x-request-id']).toBeDefined();
+            expect(
+                resp.body.data.person.meta.tag.find(
+                    t => t.system === 'https://www.icanbwell.com/query'
+                ).display
+            ).toEqual("[db.Person_4_0_0.find({'$and':[{'identifier':{'$elemMatch':{'system':'http://www.client.com/profileid','value':'healthsystemId1'}}},{'meta.tag':{'$not':{'$elemMatch':{'system':'https://fhir.icanbwell.com/4_0_0/CodeSystem/server-behavior','code':'hidden'}}}}]}, {}).sort({'_uuid':1}).limit(100),db.Patient_4_0_0.find({'_uuid':'6bcdef66-b41c-413b-b9e3-40ffe20dd18e'}, {}).sort({'_uuid':1}).limit(1000)]");
 
             // uncomment this to test out timing of events
             // await new Promise(resolve => setTimeout(resolve, 30 * 1000));
