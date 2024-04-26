4aeb3ff290f776c3cc645221150f0b026d4b3cab
Imran Qureshi <imranq2@hotmail.com>
2024-04-26 09:20:08 -0700

    add subscriptionStatus1.json

--- src/graphql/resolvers/custom/subscription.js
+++ src/graphql/resolvers/custom/subscription.js
@@ -5,7 +5,7 @@ module.exports = {
          * @param {Object} args
          * @param {GraphQLContext} context
          * @param {Object} info
-         * @return {Promise<Resource>}
+         * @return {Promise<Resource[]>}
          */
         subscriptionStatus: async (parent, args, context, info) => {
             return await context.dataApi.getResources(
@@ -19,5 +19,24 @@ module.exports = {
                 'SubscriptionStatus'
             );
         }
+        // /**
+        //  * @param {Resource|null} parent
+        //  * @param {Object} args
+        //  * @param {GraphQLContext} context
+        //  * @param {Object} info
+        //  * @return {Promise<Resource[]>}
+        //  */
+        // topic: async (parent, args, context, info) => {
+        //     return await context.dataApi.getResources(
+        //         parent,
+        //         {
+        //             ...args,
+        //             subscription: parent.id
+        //         },
+        //         context,
+        //         info,
+        //         'SubscriptionTopic'
+        //     );
+        // }
     }
 };
