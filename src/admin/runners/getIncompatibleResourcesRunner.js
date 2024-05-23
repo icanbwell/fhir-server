f9ebe9cf3a629bf20b5429cbcaa3b538cdabc1d2
007sunny <sunny.singhal@icanbwell.com>
2024-05-23 22:55:34 +0530

    EFS-1005 Created script to validate resources (#1595)
    * EFS-1005 Created script to validate resources
    
    * EFS-1005 Changed script to generate one csv file for each resource
    
    * EFS-1005 Added parallel command to script

---
+++ src/admin/runners/getIncompatibleResourcesRunner.js
@@ -0,0 +1,211 @@
+const fs = require('fs');
+const { DatabaseQueryFactory } = require('../../dataLayer/databaseQueryFactory');
+const { BaseBulkOperationRunner } = require('./baseBulkOperationRunner');
+const { RethrownError } = require('../../utils/rethrownError');
+const { isUuid } = require('../../utils/uid.util');
+const { validateResource } = require('../../utils/validator.util');
+
+class GetIncompatibleResourcesRunner extends BaseBulkOperationRunner {
+    /**
+     * constructor
+     * @typedef {Object} ConstructorParams
+     * @property {DatabaseQueryFactory} databaseQueryFactory
+     * @property {string[]} collections
+     * @property {string|undefined} startFromCollection
+     * @property {number|undefined} limit
+     * @property {string|undefined} skip
+     * @property {string|undefined} startFromId
+     * @property {string|undefined} afterLastUpdatedDate
+     * @property {string|undefined} beforeLastUpdatedDate
+     * @property {Object} args
+     *
+     * @param {ConstructorParams}
+     */
+    constructor({
+        databaseQueryFactory,
+        collections,
+        startFromCollection,
+        limit,
+        skip,
+        startFromId,
+        afterLastUpdatedDate,
+        beforeLastUpdatedDate,
+        ...args
+    }) {
+        super(args);
+        /**
+         * @type {DatabaseQueryFactory}
+         */
+        this.databaseQueryFactory = databaseQueryFactory;
+        /**
+         * @type {string[]}
+         */
+        this.collections = collections;
+
+        /**
+         * @type {string|undefined}
+         */
+        this.startFromCollection = startFromCollection;
+
+        /**
+         * @type {number|undefined}
+         */
+        this.limit = limit;
+
+        /**
+         * @type {string|undefined}
+         */
+        this.skip = skip;
+
+        /**
+         * @type {string|undefined}
+         */
+        this.startFromId = startFromId;
+
+        /**
+         * @type {string|undefined}
+         */
+        this.afterLastUpdatedDate = afterLastUpdatedDate;
+
+        /**
+         * @type {string|undefined}
+         */
+        this.beforeLastUpdatedDate = beforeLastUpdatedDate;
+    }
+
+    /**
+     * Main process
+     */
+    async processAsync() {
+        try {
+            if (this.collections.length > 0 && this.collections[0] === 'all') {
+                this.collections = await this.getAllCollectionNamesAsync({
+                    useAuditDatabase: false,
+                    includeHistoryCollections: false
+                });
+                this.collections = this.collections.sort();
+                if (this.startFromCollection) {
+                    this.collections = this.collections.filter(
+                        (c) => c >= this.startFromCollection
+                    );
+                }
+            }
+
+            // create directory to store validation errors for all resources
+            fs.mkdirSync('validationErrors');
+
+            for (const collectionName of this.collections) {
+                this.adminLogger.logInfo(`Processing ${collectionName} collection`);
+                await this.validateCollectionAsync(collectionName);
+                this.adminLogger.logInfo(`Finished processing ${collectionName} collection`);
+            }
+        } catch (err) {
+            this.adminLogger.logError(`ERROR: ${err.message}`, { stack: err.stack });
+        }
+    }
+
+    /**
+     * Validates collection documents one by one
+     * @param {string} collectionName
+     */
+    async validateCollectionAsync(collectionName) {
+        try {
+            const resourceType = collectionName.replace('_4_0_0', '');
+
+            const databaseQueryManager = this.databaseQueryFactory.createQuery({
+                resourceType,
+                base_version: '4_0_0'
+            });
+
+            const query = this.getQueryFromParams();
+
+            const cursor = await databaseQueryManager.findAsync({
+                query,
+                options: {
+                    skip: this.skip,
+                    limit: this.limit
+                }
+            });
+
+            this.writeStream = fs.createWriteStream(`./validationErrors/${resourceType}-errors.csv`, { flags: 'w' });
+            this.writeStream.write('ResourceType| ResourceId| ValidationOperationOutcome|\n');
+
+            while (await cursor.hasNext()) {
+                const resource = await cursor.next();
+
+                this.adminLogger.logInfo(`Processing ${resourceType}/${resource._uuid}`);
+
+                // Use toJSON to remove all the underscore fields
+                const resourceJson = resource.toJSON();
+
+                // delete meta.lastUpdated as this field is converted to date type but requires string type for validation
+                delete resourceJson.meta.lastUpdated;
+
+                const validationOperationOutcome = validateResource({
+                    resourceBody: resourceJson,
+                    resourceName: resourceType
+                });
+
+                if (validationOperationOutcome) {
+                    this.writeStream.write(`${resourceType}| ${resource._uuid}| ${JSON.stringify(validationOperationOutcome)}|\n`);
+                }
+            }
+
+            this.writeStream.close();
+            return new Promise((r) => this.writeStream.on('close', r));
+        } catch (err) {
+            throw new RethrownError({
+                message: `Error in validateCollection: ${err.message}`,
+                error: err,
+                args: {
+                    collectionName
+                }
+            });
+        }
+    }
+
+    /**
+     * Returns query based on the params passed
+     */
+    getQueryFromParams() {
+        let query = {};
+
+        if (this.beforeLastUpdatedDate && this.afterLastUpdatedDate) {
+            query = {
+                'meta.lastUpdated': {
+                    $gt: this.afterLastUpdatedDate,
+                    $lt: this.beforeLastUpdatedDate
+                }
+            };
+        } else if (this.beforeLastUpdatedDate) {
+            query = {
+                'meta.lastUpdated': {
+                    $lt: this.beforeLastUpdatedDate
+                }
+            };
+        } else if (this.afterLastUpdatedDate) {
+            query = {
+                'meta.lastUpdated': {
+                    $gt: this.afterLastUpdatedDate
+                }
+            };
+        }
+
+        if (this.startFromId) {
+            if (Object.keys(query).length > 0) {
+                query = {
+                    $and: [
+                        query,
+                        { [isUuid(this.startFromId) ? '_uuid' : '_sourceId']: this.startFromId }
+                    ]
+                };
+            } else {
+                query = { [isUuid(this.startFromId) ? '_uuid' : '_sourceId']: this.startFromId };
+            }
+        }
+
+        return query;
+    }
+}
+
+module.exports = { GetIncompatibleResourcesRunner };
---
+++ src/admin/scripts/getIncompatibleResources.js
@@ -0,0 +1,96 @@
+const path = require('path');
+const dotenv = require('dotenv');
+const pathToEnv = path.resolve(__dirname, '.env');
+dotenv.config({
+    path: pathToEnv
+});
+console.log(`Reading config from ${pathToEnv}`);
+console.log(`MONGO_URL=${process.env.MONGO_URL}`);
+const { createContainer } = require('../../createContainer');
+const { CommandLineParser } = require('./commandLineParser');
+const { AdminLogger } = require('../adminLogger');
+const { GetIncompatibleResourcesRunner } = require('../runners/getIncompatibleResourcesRunner');
+
+/**
+ * main function
+ * @returns {Promise<void>}
+ */
+async function main() {
+    /**
+     * @type {Object}
+     */
+    const parameters = CommandLineParser.parseCommandLine();
+    const currentDateTime = new Date();
+    /**
+     * @type {string[]}
+     */
+    const collections = parameters.collections
+        ? parameters.collections.split(',').map((x) => x.trim())
+        : ['all'];
+
+    const batchSize = parameters.batchSize || process.env.BULK_BUFFER_SIZE || 10000;
+
+    /**
+     * @type {Date|undefined}
+     */
+    const afterLastUpdatedDate = parameters.after ? new Date(parameters.after) : undefined;
+
+    /**
+     * @type {Date|undefined}
+     */
+    const beforeLastUpdatedDate = parameters.before ? new Date(parameters.before) : undefined;
+
+    const adminLogger = new AdminLogger();
+    adminLogger.logInfo(
+        `[${currentDateTime}] Running script for collections: ${collections.join(',')}`
+    );
+
+    // set up all the standard services in the container
+    const container = createContainer();
+
+    // now add our class
+    container.register(
+        'getIncompatibleResourcesRunner',
+        (c) =>
+            new GetIncompatibleResourcesRunner({
+                mongoCollectionManager: c.mongoCollectionManager,
+                mongoDatabaseManager: c.mongoDatabaseManager,
+                databaseQueryFactory: c.databaseQueryFactory,
+                adminLogger,
+                batchSize,
+                collections,
+                startFromCollection: parameters.startFromCollection,
+                limit: parameters.limit,
+                skip: parameters.skip,
+                startFromId: parameters.startFromId,
+                afterLastUpdatedDate,
+                beforeLastUpdatedDate
+            })
+    );
+
+    /**
+     * @type {GetIncompatibleResourcesRunner}
+     */
+    const getIncompatibleResourcesRunner = container.getIncompatibleResourcesRunner;
+    await getIncompatibleResourcesRunner.processAsync();
+
+    adminLogger.logInfo('Exiting process');
+    process.exit(0);
+}
+
+/**
+ * To run this:
+ * nvm use
+ * node src/admin/scripts/getIncompatibleResources.js --collections=Practitioner_4_0_0 --batchSize=10000
+ * parallel --ungroup --jobs 3 --eta --colsep '\t' NODE_OPTIONS=--max_old_space_size=8192 node --max-old-space-size=8192 src/admin/scripts/getIncompatibleResources.js --collections ::: Account_4_0_0 ActivityDefinition_4_0_0 AllergyIntolerance_4_0_0 Appointment_4_0_0 Binary_4_0_0 Bundle_4_0_0 CarePlan_4_0_0 CareTeam_4_0_0 ChargeItem_4_0_0 CodeSystem_4_0_0 Communication_4_0_0 Condition_4_0_0 Consent_4_0_0 Contract_4_0_0 Coverage_4_0_0 CoverageEligibilityResponse_4_0_0 DiagnosticReport_4_0_0 DocumentReference_4_0_0 Encounter_4_0_0 Endpoint_4_0_0 EnrollmentRequest_4_0_0 EpisodeOfCare_4_0_0 ExplanationOfBenefit_4_0_0 Group_4_0_0 HealthcareService_4_0_0 Immunization_4_0_0 InsurancePlan_4_0_0 Invoice_4_0_0 Library_4_0_0 Location_4_0_0 Measure_4_0_0 MeasureReport_4_0_0 Medication_4_0_0 MedicationDispense_4_0_0 MedicationRequest_4_0_0 MedicationStatement_4_0_0 Observation_4_0_0 Organization_4_0_0 OrganizationAffiliation_4_0_0 Patient_4_0_0 Person_4_0_0 Practitioner_4_0_0 PractitionerRole_4_0_0 Procedure_4_0_0 Provenance_4_0_0 Questionnaire_4_0_0 QuestionnaireResponse_4_0_0 RelatedPerson_4_0_0 Schedule_4_0_0 ServiceRequest_4_0_0 Slot_4_0_0 Task_4_0_0 ValueSet_4_0_0
+ * NODE_OPTIONS=--max_old_space_size=8192 node --max-old-space-size=8192 src/admin/scripts/getIncompatibleResources.js --collections=all --batchSize=10000
+ * NODE_OPTIONS=--max_old_space_size=8192 node --max-old-space-size=8192 src/admin/scripts/getIncompatibleResources.js --collections=all --batchSize=10000 --startFromCollection FamilyMemberHistory_4_0_0
+ * NODE_OPTIONS=--max_old_space_size=8192 node --max-old-space-size=8192 src/admin/scripts/getIncompatibleResources.js --collections=Person_4_0_0 --batchSize=10000 --startFromId 123
+ * NODE_OPTIONS=--max_old_space_size=8192 node --max-old-space-size=8192 src/admin/scripts/getIncompatibleResources.js --collections=Person_4_0_0 --batchSize=10000 --skip 200000
+ * NODE_OPTIONS=--max_old_space_size=8192 node --max-old-space-size=8192 src/admin/scripts/getIncompatibleResources.js --collections=Person_4_0_0 --batchSize=10000 --limit 10
+ * NODE_OPTIONS=--max_old_space_size=8192 node --max-old-space-size=8192 src/admin/scripts/getIncompatibleResources.js --collections=all --batchSize=10000 --after 2021-12-31
+ * NODE_OPTIONS=--max_old_space_size=8192 node --max-old-space-size=8192 src/admin/scripts/getIncompatibleResources.js --collections=all --batchSize=10000 --before 2021-12-31
+ */
+main().catch((reason) => {
+    console.error(reason);
+});
