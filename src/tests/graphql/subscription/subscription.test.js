d4cfd72ec8af4100c550997b513d546c52b7d0c8
mintusah25 <mintu.sah@joshtechnologygroup.com>
2024-05-31 13:44:12 +0530

    [EFS 1035]: Add more testcases for patient/user JWT

--- src/tests/graphql/subscription/subscription.test.js
+++ src/tests/graphql/subscription/subscription.test.js
@@ -11,11 +11,16 @@ const subscriptionTopic3Resource = require('./fixtures/SubscriptionTopic/subscri
 const patientBundleResource = require('./fixtures/Patient/patient1.json');
 const personBundleResource = require('./fixtures/Person/person1.json');
 const personBundle2Resource = require('./fixtures/Person/person2.json');
+const personBundle3Resource = require('./fixtures/Person/person3.json');
 
 // expected
 const expectedSubscriptionResources = require('./fixtures/expected/expected_subscription.json');
 const expectedSubscriptionResources2 = require('./fixtures/expected/expected_subscription2.json');
+const expectedSubscriptionListResponse = require('./fixtures/expected/expected_subscriptions_list.json');
 const expectedSubscriptionInvalidResources = require('./fixtures/expected/expected_subscription_invalid.json');
+const expectedUpdateSubscriptionResponse = require('./fixtures/expected/expected_update_subscription.json');
+const expectedCreateSubscriptionResponse = require('./fixtures/expected/expected_create_subscription.json');
+const expectedsubscriptionNotFoundResponse = require('./fixtures/expected/expected_subscription_not_found.json');
 
 const deepcopy = require('deepcopy');
 const fs = require('fs');
@@ -32,6 +37,9 @@ const {
     getHeadersWithCustomPayload
 } = require('../../common');
 const {describe, beforeEach, afterEach, test, expect} = require('@jest/globals');
+const { head } = require('superagent');
+const ContractLegal = require('../../../fhir/classes/4_0_0/backbone_elements/contractLegal');
+const { Console } = require('console');
 
 describe('GraphQL Subscription Tests', () => {
     beforeEach(async () => {
@@ -232,12 +240,110 @@ describe('GraphQL Subscription Tests', () => {
             // noinspection JSUnresolvedFunction
             expect(resp).toHaveGraphQLResponse(expectedSubscriptionInvalidResources, 'subscription');
         });
-        test('Read & Write Subscription with Patient Scope', async () => {
+
+        test('Using patient/user JWT Create/Update/Get/List/Delete Subscriptions', async () => {
             const request = await createTestRequest();
 
             // ARRANGE
             // add the resources to FHIR server
+            let personResponse = await request
+                .post('/4_0_0/Person/$merge')
+                .send(personBundleResource)
+                .set(getHeaders());
 
+            // noinspection JSUnresolvedFunction
+            expect(personResponse).toHaveMergeResponse({ created: true });
+
+
+            let personResponse2 = await request
+                .post('/4_0_0/Person/$merge')
+                .send(personBundle2Resource)
+                .set(getHeaders());
+
+            // noinspection JSUnresolvedFunction
+            expect(personResponse2).toHaveMergeResponse({ created: true });
+
+            await request
+                .post('/4_0_0/Subscription/$merge')
+                .send(subscription4Resource)
+                .set(getHeaders())
+                .expect(200)
+
+            const person_payload = {
+                scope: 'patient/*.* user/*.* access/*.*',
+                username: 'patient-123@example.com',
+                clientFhirPersonId: personResponse2.body.uuid,
+                clientFhirPatientId: 'clientFhirPatient',
+                bwellFhirPersonId: personResponse.body.uuid,
+                bwellFhirPatientId: 'bwellFhirPatient',
+                token_use: 'access'
+            };
+
+            const headers1 = getHeadersWithCustomPayload(person_payload);
+
+            // Get Subscriptions
+            let subscriptionResponse = await request
+                .get('/4_0_0/Subscription/?id=subscription4&_debug=1&_bundle=1')
+                .set(headers1)
+                .expect(200)
+
+            expect(subscriptionResponse).toHaveResponse(expectedSubscriptionResources2)
+
+            await request
+                .post('/4_0_0/Subscription/$merge')
+                .send(subscription2Resource)
+                .set(getHeaders())
+                .expect(200)
+
+            // Get Subscriptions List
+            let subscriptionListResponse = await request
+                .get('/4_0_0/Subscription?_bundle=1&_debug=1')
+                .set(headers1)
+                .expect(200)
+
+            expect(subscriptionListResponse).toHaveResponse(expectedSubscriptionListResponse)
+
+
+            // Update Subscriptions
+            let subscriptionResponseWithChange = deepcopy(subscription4Resource.entry[0].resource);
+            subscriptionResponseWithChange.reason = 'Monitor new neonatal function';
+
+            resp = await request
+                .put('/4_0_0/Subscription/subscription4?_debug=1')
+                .set(headers1)
+                .send(subscriptionResponseWithChange)
+                .expect(403)
+
+            expect(resp).toHaveResponse(expectedUpdateSubscriptionResponse);
+
+            // Create Subscriptions
+            resp = await request
+                .post('/4_0_0/Subscription/$merge?_debug=1')
+                .send(subscription2Resource)
+                .set(headers1)
+                .expect(200)
+
+            expect(resp).toHaveResponse(expectedCreateSubscriptionResponse);
+
+            // Delete Subscriptions
+            await request
+                .delete('/4_0_0/Subscription/subscription4')
+                .set(headers1)
+                .expect(204)
+
+            subscriptionListResponse = await request
+                .get('/4_0_0/Subscription?_bundle=1')
+                .set(headers1)
+                .expect(200)
+
+            expect(subscriptionListResponse.body.entry).toHaveLength(2)
+        });
+
+        test('Read & Write Subscription of another User', async () => {
+            const request = await createTestRequest();
+
+            // ARRANGE
+            // add the resources to FHIR server
             let personResponse = await request
                 .post('/4_0_0/Person/$merge')
                 .send(personBundleResource)
@@ -254,81 +360,52 @@ describe('GraphQL Subscription Tests', () => {
             // noinspection JSUnresolvedFunction
             expect(personResponse2).toHaveMergeResponse({ created: true });
 
+            let personResponse3 = await request
+                .post('/4_0_0/Person/$merge')
+                .send(personBundle3Resource)
+                .set(getHeaders());
+
+            // noinspection JSUnresolvedFunction
+            expect(personResponse3).toHaveMergeResponse({ created: true });
+
             let subscriptionResponse = await request
                 .post('/4_0_0/Subscription/$merge')
                 .send(subscription4Resource)
                 .set(getHeaders());
 
-            // noinspection JSUnresolvedFunction
+            // noinspection JSUnresolvedFunctiFon
             expect(subscriptionResponse).toHaveMergeResponse({ created: true });
 
             const person_payload = {
                 scope: 'patient/Subscription.*',
                 username: 'patient-123@example.com',
-                clientFhirPersonId: personResponse2.body.uuid,
+                clientFhirPersonId: personResponse3.body.uuid,
                 clientFhirPatientId: 'clientFhirPatient',
                 bwellFhirPersonId: personResponse.body.uuid,
                 bwellFhirPatientId: 'bwellFhirPatient',
                 token_use: 'access'
             };
 
-            const headers1 = getHeadersWithCustomPayload(person_payload);
-
-            // Successful read Subscription request for Patient Scope
-            let resp = await request
-                .get('/4_0_0/Subscription/subscription4')
-                .set(headers1)
-                .expect(200)
-
-            expect(resp).toHaveResponse(expectedSubscriptionResources2)
-
-            let subscriptionResponseWithChange = deepcopy(subscription4Resource.entry[0].resource);
-
-            subscriptionResponseWithChange.reason = 'Monitor new neonatal function';
-
-            // Unsuccessful write Subscription request for Patient Scope
-            resp = await request
-                .put('/4_0_0/Subscription/subscription4')
-                .set(headers1)
-                .send(subscriptionResponseWithChange)
-                .expect(403)
-
-            expect(resp).toHaveResponse(
-                {
-                    resourceType: 'OperationOutcome',
-                    issue: [
-                        {
-                            severity: 'error',
-                            code: 'forbidden',
-                            details: {
-                                text: "The current patient scope and person id in the JWT token do not allow writing the Subscription resource."
-                            },
-                            diagnostics: "The current patient scope and person id in the JWT token do not allow writing the Subscription resource."
-                        }
-                    ]
-                }
-            );
-
-            person_payload.clientFhirPersonId = personResponse.body.uuid;
-
-            const headers2 = getHeadersWithCustomPayload(person_payload);
+            const headers = getHeadersWithCustomPayload(person_payload)
 
             resp = await request
                 .get('/4_0_0/Subscription/subscription4')
-                .set(headers2)
+                .set(headers)
                 .expect(404)
 
-            expect(resp).toHaveResponse(
-                {
-                    resourceType: 'OperationOutcome',
-                    issue: [{
-                        severity: 'error', code: 'not-found', details: {
-                            text: 'Resource not found: Subscription/subscription4'
-                        }
-                    }]
-                }
+            expect(resp).toHaveResponse(expectedsubscriptionNotFoundResponse)
 
-            )
+            // Update Subscriptions
+            let subscriptionResponseWithChange = deepcopy(subscription4Resource.entry[0].resource);
+            subscriptionResponseWithChange.reason = 'Monitor new neonatal function';
+
+            resp = await request
+                .put('/4_0_0/Subscription/subscription4')
+                .send(subscriptionResponseWithChange)
+                .set(headers)
+                .expect(403)
+
+            expect(resp).toHaveResponse(expectedUpdateSubscriptionResponse)
         });
     });
 });
