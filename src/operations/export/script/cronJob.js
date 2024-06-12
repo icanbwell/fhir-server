97282e8b2c5b00375826478add8ca4b08a9cdb05
mintusah25 <mintu.sah@joshtechnologygroup.com>
2024-06-12 14:34:10 +0530

    EFS-1052: Added cron job script to trigger k8 job and updated status of ExportStatus

---
+++ src/operations/export/script/cronJob.js
@@ -0,0 +1,65 @@
+if (process.argv.includes('--dotenv')) {
+    const path = require('path');
+    const dotenv = require('dotenv');
+    const pathToEnv = path.resolve(__dirname, '.env');
+    dotenv.config({
+        path: pathToEnv
+    });
+    console.log(`Reading config from ${pathToEnv}`);
+}
+console.log(`MONGO_URL=${process.env.MONGO_URL}`);
+const { createContainer } = require('../../../createContainer');
+const { CommandLineParser } = require('../../../admin/scripts/commandLineParser');
+const { CronJobRunner } = require('./cronJobRunner');
+const { logInfo } = require('../../common/logging');
+
+/**
+ * main function
+ * @returns {Promise<void>}
+ */
+async function main() {
+    /**
+     * @type {Object}
+     */
+    const parameters = CommandLineParser.parseCommandLine();
+
+    const currentDateTime = new Date();
+
+    logInfo(
+        `[${currentDateTime}] Running cron job script to update status of ExportStatus resources & export data`
+    );
+
+    // set up all the standard services in the container
+    const container = createContainer();
+
+    // now add our class
+    container.register(
+        'cronJobRunner',
+        (c) =>
+            new CronJobRunner({
+                databaseQueryFactory: c.databaseQueryFactory,
+                databaseExportManager: c.databaseExportManager,
+                k8sClient: c.k8sClient,
+                configManager: c.configManager
+            })
+    );
+
+    /**
+     * @type {CronJobRunner}
+     */
+    const cronJobRunner = container.cronJobRunner;
+    await cronJobRunner.processAsync();
+
+    logInfo('Exiting process');
+    process.exit(0);
+}
+
+/**
+ * To run this:
+ * nvm use
+ * node src/operations/export/script/cronJob.js
+ * NODE_OPTIONS=--max_old_space_size=8192 node --max-old-space-size=8192 src/operations/export/script/cronJob.js
+ */
+main().catch((reason) => {
+    console.error(reason);
+});
---
+++ src/operations/export/script/cronJobRunner.js
@@ -0,0 +1,172 @@
+const { logInfo, logError } = require('../../common/logging');
+const { DatabaseExportManager } = require('../../../dataLayer/databaseExportManager');
+const { DatabaseQueryFactory } = require('../../../dataLayer/databaseQueryFactory');
+const { assertTypeEquals } = require('../../../utils/assertType');
+const { ConfigManager } = require('../../../utils/configManager');
+const { K8sClient } = require('../../../utils/k8sClient');
+const { RethrownError } = require('../../../utils/rethrownError');
+const { DatabaseQueryManager } = require('../../../dataLayer/databaseQueryManager');
+
+class CronJobRunner {
+    /**
+     * @typedef {Object} ConstructorParams
+
+     * @property {DatabaseQueryFactory} databaseQueryFactory
+     * @property {DatabaseExportManager} databaseExportManager
+     * @property {K8sClient} k8sClient
+     * @property {ConfigManager} configManager
+     *
+     * @param {ConstructorParams}
+     */
+    constructor({
+        databaseQueryFactory,
+        databaseExportManager,
+        k8sClient,
+        configManager
+    }) {
+        /**
+         * @type {DatabaseQueryFactory}
+         */
+        this.databaseQueryFactory = databaseQueryFactory;
+        assertTypeEquals(databaseQueryFactory, DatabaseQueryFactory);
+
+        /**
+         * @type {DatabaseExportManager}
+         */
+        this.databaseExportManager = databaseExportManager;
+        assertTypeEquals(databaseExportManager, DatabaseExportManager);
+
+        /**
+         * @type {K8sClient}
+         */
+        this.k8sClient = k8sClient;
+        assertTypeEquals(k8sClient, K8sClient);
+
+        /**
+         * @type {ConfigManager}
+         */
+        this.configManager = configManager;
+        assertTypeEquals(configManager, ConfigManager);
+    }
+
+    /**
+     * Main function
+     */
+    async processAsync() {
+        try {
+            const databaseQueryManager = this.databaseQueryFactory.createQuery({
+                resourceType: 'ExportStatus',
+                base_version: '4_0_0'
+            });
+
+            await this.triggerK8JobForAcceptedResources({ databaseQueryManager });
+            await this.updateInProgressResources({ databaseQueryManager });
+        } catch (err) {
+            logError(`Error in processAsync: ${err.message}`, {
+                error: err.stack
+            });
+        }
+    }
+
+    /**
+     * Function to fetch and trigger k8 job for all ExportStatus resources with accepted status
+     * @typedef {Object} TriggerK8JobForAcceptedResourcesParams
+     * @property {DatabaseQueryManager} databaseQueryManager
+     *
+     * @param {TriggerK8JobForAcceptedResourcesParams}
+     */
+    async triggerK8JobForAcceptedResources({ databaseQueryManager }) {
+        try {
+            // Query to fetch ExportStatus resources with accepted status & sorted from oldest to newest
+            const query = {
+                query: { status: 'accepted' },
+                options: {
+                    sort: { transactionTime: 1 },
+                    projection: { _uuid: 1 }
+                }
+            };
+
+            logInfo(`Fetching ExportStatus resource with query: ${JSON.stringify(query)}`);
+            const exportStatusCursor = await databaseQueryManager.findAsync(query);
+
+            while (await exportStatusCursor.hasNext()) {
+                const exportStatusData = await exportStatusCursor.next();
+                logInfo(
+                    `Triggering k8 job for ExportStatus resource with id: ${exportStatusData._uuid}`
+                );
+
+                // Trigger k8s job to export data
+                const jobResult = await this.k8sClient.createJob(
+                    'node /srv/src/src/operations/export/script/bulkDataExport.js ' +
+                        `--exportStatusId ${exportStatusData._uuid} ` +
+                        `--bulkExportS3BucketName ${this.configManager.bulkExportS3BucketName} ` +
+                        `--awsRegion ${this.configManager.awsRegion}`
+                );
+
+                // Break the loop if the job limit is exceeded
+                if (!jobResult) {
+                    logInfo(
+                        'Maximum number of active jobs reached in the namespace, stopping job creation for the remaining resources'
+                    );
+                    break;
+                }
+            }
+            logInfo(
+                'Successfully finished triggering k8 job for the ExportStatus resources with accepted status'
+            );
+        } catch (err) {
+            logError(`Error in triggerK8JobForAcceptedResources: ${err.message}`, {
+                error: err.stack
+            });
+            throw new RethrownError({
+                message: err.message,
+                source: 'CronJobRunner.triggerK8JobForAcceptedResources',
+                error: err
+            });
+        }
+    }
+
+    /**
+     * Function to fetch ExportStatus resources with in-progress status & in same state for last 24 hrs
+     * @typedef {Object} TriggerK8JobForAcceptedResourcesParams
+     * @property {DatabaseQueryManager} databaseQueryManager
+     *
+     * @param {TriggerK8JobForAcceptedResourcesParams}
+     */
+    async updateInProgressResources({ databaseQueryManager }) {
+        try {
+            // Query to fetch ExportStatus resources with in-progress status from last 24 hrs
+            const query = {
+                status: 'in-progress',
+                'meta.lastUpdated': { $lt: new Date(Date.now() - 24 * 60 * 60 * 1000) }
+            };
+
+            logInfo(`Fetching ExportStatus resource with query: ${JSON.stringify(query)}`);
+            const exportStatusCursor = await databaseQueryManager.findAsync({ query });
+
+            // Setting status to 'entered-in-error' for the above fetched resources
+            while (await exportStatusCursor.hasNext()) {
+                const exportStatusResource = await exportStatusCursor.next();
+                logInfo(
+                    `Updating status to 'entered-in-error' for ExportStatus resource with id: ${exportStatusResource._uuid}`
+                );
+                exportStatusResource.status = 'entered-in-error';
+                await this.databaseExportManager.updateExportStatusAsync({ exportStatusResource });
+            }
+            logInfo(
+                `Successfully finished updating status to 'entered-in-error' for the fetched ExportStatus resources`
+            );
+        } catch (err) {
+            logError(`Error in updateInProgressResources: ${err.message}`, {
+                error: err.stack
+            });
+            throw new RethrownError({
+                message: err.message,
+                source: 'CronJobRunner.updateInProgressResources',
+                error: err
+            });
+        }
+    }
+}
+
+module.exports = { CronJobRunner };
