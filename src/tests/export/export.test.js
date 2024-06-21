e25765289f2a3fd6da6d6313fbfad312f525954e
007sunny <sunny.singhal@icanbwell.com>
2024-06-21 12:00:29 +0530

    EFS-1081 Added support for query generator in bulk export

--- src/tests/export/export.test.js
+++ src/tests/export/export.test.js
@@ -4,7 +4,7 @@ const patient2Resource = require('./fixtures/patient/patient2.json');
 const person1Resource = require('./fixtures/person/person1.json');
 
 const { commonBeforeEach, commonAfterEach, getHeaders, createTestRequest, getTestContainer } = require('../common');
-const { describe, beforeEach, afterEach, test, expect } = require('@jest/globals');
+const { describe, beforeEach, afterEach, test, expect, jest } = require('@jest/globals');
 const env = require('var');
 const { BulkDataExportRunner } = require('../../operations/export/script/bulkDataExportRunner');
 const { MockK8sClient } = require('./mocks/k8sClient');
@@ -76,10 +76,10 @@ describe('Export Tests', () => {
                 patientFilterManager: c.patientFilterManager,
                 databaseAttachmentManager: c.databaseAttachmentManager,
                 r4SearchQueryCreator: c.r4SearchQueryCreator,
-                securityTagManager: c.securityTagManager,
                 patientQueryCreator: c.patientQueryCreator,
                 enrichmentManager: c.enrichmentManager,
                 r4ArgsParser: c.r4ArgsParser,
+                searchManager: c.searchManager,
                 exportStatusId,
                 batchSize: 1000,
                 minUploadBatchSize: 1000,
@@ -165,10 +165,10 @@ describe('Export Tests', () => {
                 patientFilterManager: c.patientFilterManager,
                 databaseAttachmentManager: c.databaseAttachmentManager,
                 r4SearchQueryCreator: c.r4SearchQueryCreator,
-                securityTagManager: c.securityTagManager,
                 patientQueryCreator: c.patientQueryCreator,
                 enrichmentManager: c.enrichmentManager,
                 r4ArgsParser: c.r4ArgsParser,
+                searchManager: c.searchManager,
                 exportStatusId,
                 batchSize: 1000,
                 minUploadBatchSize: 1,
@@ -243,10 +243,10 @@ describe('Export Tests', () => {
                 patientFilterManager: c.patientFilterManager,
                 databaseAttachmentManager: c.databaseAttachmentManager,
                 r4SearchQueryCreator: c.r4SearchQueryCreator,
-                securityTagManager: c.securityTagManager,
                 patientQueryCreator: c.patientQueryCreator,
                 enrichmentManager: c.enrichmentManager,
                 r4ArgsParser: c.r4ArgsParser,
+                searchManager: c.searchManager,
                 exportStatusId,
                 batchSize: 1000,
                 minUploadBatchSize: 1000,
@@ -323,10 +323,10 @@ describe('Export Tests', () => {
                 patientFilterManager: c.patientFilterManager,
                 databaseAttachmentManager: c.databaseAttachmentManager,
                 r4SearchQueryCreator: c.r4SearchQueryCreator,
-                securityTagManager: c.securityTagManager,
                 patientQueryCreator: c.patientQueryCreator,
                 enrichmentManager: c.enrichmentManager,
                 r4ArgsParser: c.r4ArgsParser,
+                searchManager: c.searchManager,
                 exportStatusId,
                 batchSize: 1000,
                 minUploadBatchSize: 1000,
@@ -425,10 +425,10 @@ describe('Export Tests', () => {
                         patientFilterManager: c.patientFilterManager,
                         databaseAttachmentManager: c.databaseAttachmentManager,
                         r4SearchQueryCreator: c.r4SearchQueryCreator,
-                        securityTagManager: c.securityTagManager,
                         patientQueryCreator: c.patientQueryCreator,
                         enrichmentManager: c.enrichmentManager,
                         r4ArgsParser: c.r4ArgsParser,
+                        searchManager: c.searchManager,
                         exportStatusId,
                         batchSize: 1000,
                         minUploadBatchSize: 1000,
@@ -441,9 +441,39 @@ describe('Export Tests', () => {
                     })
             );
 
+            const mockProcessResourceAsync = jest.spyOn(container.bulkDataExportRunner, 'processResourceAsync');
+
             const bulkDataExportRunner = container.bulkDataExportRunner;
 
             await bulkDataExportRunner.processAsync();
+
+            // Verifying processResourceAsync is called correct number of times with proper query and resource
+            expect(mockProcessResourceAsync).toHaveBeenCalledTimes(1);
+            expect(mockProcessResourceAsync).toHaveBeenCalledWith({
+                query: {
+                    $and: [
+                        {
+                            'meta.tag': {
+                                $not: {
+                                    $elemMatch: {
+                                        system: 'https://fhir.icanbwell.com/4_0_0/CodeSystem/server-behavior',
+                                        code: 'hidden'
+                                    }
+                                }
+                            }
+                        },
+                        {
+                            'meta.security': {
+                                $elemMatch: {
+                                    system: 'https://www.icanbwell.com/access',
+                                    code: { $in: ['client', 'client1'] }
+                                }
+                            }
+                        }
+                    ]
+                },
+                resourceType: 'Patient'
+            });
 
             // Query again to check the status
             resp = await request
@@ -529,10 +559,10 @@ describe('Export Tests', () => {
                         patientFilterManager: c.patientFilterManager,
                         databaseAttachmentManager: c.databaseAttachmentManager,
                         r4SearchQueryCreator: c.r4SearchQueryCreator,
-                        securityTagManager: c.securityTagManager,
                         patientQueryCreator: c.patientQueryCreator,
                         enrichmentManager: c.enrichmentManager,
                         r4ArgsParser: c.r4ArgsParser,
+                        searchManager: c.searchManager,
                         exportStatusId,
                         batchSize: 1000,
                         minUploadBatchSize: 1000,
@@ -558,6 +588,168 @@ describe('Export Tests', () => {
             expect(resp.body.output).toHaveLength(1);
             expect(resp.body.output[0].type).toEqual('Patient');
             expect(resp.body.output[0].url.split('/').pop()).toEqual('Patient.ndjson');
+
+            expect(resp.body.errors).toHaveLength(0);
+        });
+
+        test('Export triggering work with all available query params', async () => {
+            const request = await createTestRequest((c) => {
+                c.register(
+                    'k8sClient',
+                    (c) =>
+                        new MockK8sClient({
+                            configManager: c.configManager
+                        })
+                );
+                return c;
+            });
+            const container = getTestContainer();
+
+            /**
+             * @type {MongoDatabaseManager}
+             */
+            const mongoDatabaseManager = container.mongoDatabaseManager;
+            const fhirDb = await mongoDatabaseManager.getClientDbAsync();
+            const collection = fhirDb.collection('ExportStatus_4_0_0');
+
+            let resp = await request
+                .post(
+                    '/4_0_0/$export?_type=Patient,Person&_since=2023-10-10&patient=patient/1,2&_includeHidden=1'
+                )
+                .set(getHeaders('access/client.* access/client1.* user/*.*'))
+                .expect(202);
+
+            expect(resp.headers['content-location']).toBeDefined();
+            const exportStatusId = resp.headers['content-location'].split('/').pop();
+
+            const exportStatusResource = await collection.find({}).toArray();
+            expect(exportStatusResource[0].meta.security).toEqual([
+                { code: 'bwell', system: 'https://www.icanbwell.com/owner' },
+                { code: 'client', system: 'https://www.icanbwell.com/access' },
+                { code: 'client1', system: 'https://www.icanbwell.com/access' },
+                { code: 'bwell', system: 'https://www.icanbwell.com/sourceAssigningAuthority' }
+            ]);
+
+            resp = await request
+                .get(`/4_0_0/$export/${exportStatusId}`)
+                .set(getHeaders())
+                .expect(202);
+
+            expect(resp.headers['x-progress']).toEqual('accepted');
+
+            // create patients to export
+            resp = await request
+                .post('/4_0_0/Patient/$merge')
+                .send(patient1Resource)
+                .set(getHeaders())
+                .expect(200);
+
+            expect(resp).toHaveMergeResponse({ created: true });
+
+            resp = await request
+                .post('/4_0_0/Patient/$merge')
+                .send(patient2Resource)
+                .set(getHeaders())
+                .expect(200);
+
+            expect(resp).toHaveMergeResponse({ created: true });
+
+            // Update the status of ExportStatus resource to completed
+
+            container.register(
+                'bulkDataExportRunner',
+                (c) =>
+                    new BulkDataExportRunner({
+                        databaseQueryFactory: c.databaseQueryFactory,
+                        databaseExportManager: c.databaseExportManager,
+                        patientFilterManager: c.patientFilterManager,
+                        databaseAttachmentManager: c.databaseAttachmentManager,
+                        r4SearchQueryCreator: c.r4SearchQueryCreator,
+                        patientQueryCreator: c.patientQueryCreator,
+                        enrichmentManager: c.enrichmentManager,
+                        r4ArgsParser: c.r4ArgsParser,
+                        searchManager: c.searchManager,
+                        exportStatusId,
+                        batchSize: 1000,
+                        minUploadBatchSize: 1000,
+                        logAfterReads: 1000,
+                        uploadPartSize: 1024 * 1024,
+                        s3Client: new MockS3Client({
+                            bucketName: 'test',
+                            region: 'test'
+                        })
+                    })
+            );
+
+            const mockProcessResourceAsync = jest.spyOn(
+                container.bulkDataExportRunner,
+                'processResourceAsync'
+            );
+
+            const bulkDataExportRunner = container.bulkDataExportRunner;
+
+            await bulkDataExportRunner.processAsync();
+
+            // Verifying processResourceAsync is called correct number of times with proper query and resource
+            expect(mockProcessResourceAsync).toHaveBeenCalledTimes(2);
+            expect(mockProcessResourceAsync.mock.calls).toEqual([
+                [
+                    {
+                        query: {
+                            $and: [
+                                {
+                                    'meta.security': {
+                                        $elemMatch: {
+                                            system: 'https://www.icanbwell.com/access',
+                                            code: { $in: ['client', 'client1'] }
+                                        }
+                                    }
+                                },
+                                {
+                                    'meta.lastUpdated': {
+                                        $gte: new Date('2023-10-10T00:00:00.000Z')
+                                    }
+                                }
+                            ]
+                        },
+                        resourceType: 'Patient'
+                    }
+                ],
+                [
+                    {
+                        query: {
+                            $and: [
+                                {
+                                    'meta.security': {
+                                        $elemMatch: {
+                                            system: 'https://www.icanbwell.com/access',
+                                            code: { $in: ['client', 'client1'] }
+                                        }
+                                    }
+                                },
+                                {
+                                    'meta.lastUpdated': {
+                                        $gte: new Date('2023-10-10T00:00:00.000Z')
+                                    }
+                                }
+                            ]
+                        },
+                        resourceType: 'Person'
+                    }
+                ]
+            ]);
+
+            // Query again to check the status
+            resp = await request
+                .get(`/4_0_0/$export/${exportStatusId}`)
+                .set(getHeaders())
+                .expect(200);
+
+            expect(resp.body.output).toHaveLength(2);
+            expect(resp.body.output[0].type).toEqual('Patient');
+            expect(resp.body.output[0].url.split('/').pop()).toEqual('Patient.ndjson');
+            expect(resp.body.output[1].type).toEqual('Person');
+            expect(resp.body.output[1].url.split('/').pop()).toEqual('Person.ndjson');
 
             expect(resp.body.errors).toHaveLength(0);
         });
