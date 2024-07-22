8f92f17112a500445fb08fc2441872d4172ac897
007sunny <sunny.singhal@icanbwell.com>
2024-07-22 18:12:36 +0530

    EFS-1100: Update text modifier

--- src/operations/query/filters/partialText.js
+++ src/operations/query/filters/partialText.js
@@ -1,40 +1,60 @@
-const { partialTextQueryBuilder } = require('../../../utils/querybuilder.util');
+const { textQueryBuilder } = require('../../../utils/querybuilder.util');
 const { BaseFilter } = require('./baseFilter');
 
 /**
  * @classdesc Filters by missing
  * https://www.hl7.org/fhir/search.html#modifiers
  */
-class FilterByPartialText extends BaseFilter {
-    /**
-     * @param {string} field
-     * @param {string} value
-     * @return {import('mongodb').Filter<import('mongodb').DefaultSchema>|import('mongodb').Filter<import('mongodb').DefaultSchema>[]}
-     */
-    filterByItem (field, value) {
-        return {
-            $or: [
-                // 1. search in text field
-                partialTextQueryBuilder(
-                    {
-                        field: this.fieldMapper.getFieldName(`${field}.text`),
-                        partialText: value,
-                        ignoreCase: true
-                    }
-                ),
-                // 2. search in display field for every coding
-                partialTextQueryBuilder(
-                    {
-                        field: this.fieldMapper.getFieldName(`${field}.coding.display`),
-                        partialText: value,
-                        ignoreCase: true
-                    }
+class FilterByText extends BaseFilter {
+
+    filterText() {
+        const and_segments = [];
+
+        if (this.parsedArg.queryParameterValue.values) {
+            and_segments.push({
+                $or: this.propertyObj.fields.flatMap((field) => {
+                    return {
+                        [this.parsedArg.queryParameterValue.operator]:
+                            this.parsedArg.queryParameterValue.values.flatMap(v => {
+                                return {
+                                    $or: [
+                                        // 1. search in text field
+                                        textQueryBuilder(
+                                            {
+                                                field: this.fieldMapper.getFieldName(`${field}.text`),
+                                                text: this.parsedArg.queryParameterValue.value,
+                                                ignoreCase: false
+                                            }
+                                        ),
+                                        // 2. search in display field for every coding
+                                        textQueryBuilder(
+                                            {
+                                                field: this.fieldMapper.getFieldName(`${field}.coding.display`),
+                                                text: this.parsedArg.queryParameterValue.value,
+                                                ignoreCase: true
+                                            }
+                                        ),
+                                        // 3. search in identifier.type.text
+                                        textQueryBuilder(
+                                            {
+                                                field: this.fieldMapper.getFieldName(`identifier.type.text`),
+                                                text: this.parsedArg.queryParameterValue.value,
+                                                ignoreCase: true
+                                            }
+                                        )
+                                    ]
+                                };
+                            })
+                    };
+                }
                 )
-            ]
-        };
+            }
+            );
+        }
+        return and_segments;
     }
 }
 
 module.exports = {
-    FilterByPartialText
+    FilterByText
 };
--- src/tests/searchParameters/search_by_code_text/search_by_code_text.test.js
+++ src/tests/searchParameters/search_by_code_text/search_by_code_text.test.js
@@ -2,9 +2,16 @@
 const medication1Resource = require('./fixtures/Medication/medication1.json');
 const medication2Resource = require('./fixtures/Medication/medication2.json');
 const medication3Resource = require('./fixtures/Medication/medication3.json');
+const medication4Resource = require('./fixtures/Medication/medication4.json');
+const medication5Resource = require('./fixtures/Medication/medication5.json');
 
 // expected
 const expectedMedicationResources = require('./fixtures/expected/expected_Medication.json');
+const expectedMedicationResources2 = require('./fixtures/expected/expected_medication2.json');
+const expectedMedicationResources3 = require('./fixtures/expected/expected_medication3.json');
+const expectedMedicationResources4 = require('./fixtures/expected/expected_medication4.json');
+const expectedMedicationResources5 = require('./fixtures/expected/expected_medication5.json');
+const expectedMedicationResources6 = require('./fixtures/expected/expected_medication6.json');
 
 const {
     commonBeforeEach,
@@ -56,6 +63,117 @@ describe('Medication Tests', () => {
                 .set(getHeaders());
             // noinspection JSUnresolvedFunction
             expect(resp).toHaveResponse(expectedMedicationResources);
+        });
+
+        test('Search By Code Text via identifier.type.text field', async () => {
+            const request = await createTestRequest();
+            // ARRANGE
+            // add the resources to FHIR server
+            resp = await request
+                .post('/4_0_0/Medication/$merge?validate=true')
+                .send(medication4Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({ created: true });
+
+            // ACT & ASSERT
+            resp = await request
+                .get('/4_0_0/Medication?code:text=predniso&_debug=1&_bundle=1')
+                .set(getHeaders());
+
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResponse(expectedMedicationResources2);
+        });
+
+        test('Search By Code Text for multiple comma separated texts', async () => {
+            const request = await createTestRequest();
+            // ARRANGE
+            // add the resources to FHIR server
+            resp = await request
+                .post('/4_0_0/Medication/$merge?validate=true')
+                .send(medication4Resource)
+                .set(getHeaders());
+
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({ created: true });
+
+            resp = await request
+                .post('/4_0_0/Medication/$merge?validate=true')
+                .send(medication5Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({ created: true });
+
+            // ACT & ASSERT
+            resp = await request
+                .get('/4_0_0/Medication?code:text=predniso,sample&_debug=1&_bundle=1')
+                .set(getHeaders());
+
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResponse(expectedMedicationResources3);
+        });
+
+        test('Search By Code Text starting with a given text', async () => {
+            const request = await createTestRequest();
+            // ARRANGE
+            // add the resources to FHIR server
+            resp = await request
+                .post('/4_0_0/Medication/$merge?validate=true')
+                .send(medication4Resource)
+                .set(getHeaders());
+
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({ created: true });
+
+            // ACT & ASSERT
+            resp = await request
+                .get('/4_0_0/Medication?code:text=pred&_debug=1&_bundle=1')
+                .set(getHeaders());
+
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResponse(expectedMedicationResources4);
+        });
+
+        test.only('Search By Code Text with given text anywhere in the string', async () => {
+            const request = await createTestRequest();
+            // ARRANGE
+            // add the resources to FHIR server
+            resp = await request
+                .post('/4_0_0/Medication/$merge?validate=true')
+                .send(medication4Resource)
+                .set(getHeaders());
+
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({ created: true });
+
+            // ACT & ASSERT
+            resp = await request
+                .get('/4_0_0/Medication?code:text=Record&_debug=1&_bundle=1')
+                .set(getHeaders());
+
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResponse(expectedMedicationResources5);
+        });
+
+        test('Search By Code Text with given text exactly matches the string', async () => {
+            const request = await createTestRequest();
+            // ARRANGE
+            // add the resources to FHIR server
+            resp = await request
+                .post('/4_0_0/Medication/$merge?validate=true')
+                .send(medication4Resource)
+                .set(getHeaders());
+
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({ created: true });
+
+            // ACT & ASSERT
+            resp = await request
+                .get('/4_0_0/Medication?code:text=prednisoLONE Record Number&_debug=1&_bundle=1')
+                .set(getHeaders());
+
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResponse(expectedMedicationResources6);
         });
     });
 });
