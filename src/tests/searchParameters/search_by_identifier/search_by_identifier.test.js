808a21fbe66cfeb8bbbd23da0a9ed43011c0af2f
007sunny <sunny.singhal@icanbwell.com>
2024-07-26 11:30:59 +0530

    EFS-1101 Added support for of-type modifier

--- src/tests/searchParameters/search_by_identifier/search_by_identifier.test.js
+++ src/tests/searchParameters/search_by_identifier/search_by_identifier.test.js
@@ -6,6 +6,8 @@ const expectedPractitionerResources = require('./fixtures/expected/expected_Prac
 const expectedPractitionerContainsResources = require('./fixtures/expected/expected_Practitioner_contains.json');
 const expectedPractitionerByGenderResources = require('./fixtures/expected/expected_Practitioner_gender.json');
 const expectedPractitionerBySystemResources = require('./fixtures/expected/expected_Practitioner_by_system.json');
+const expectedPractitionerByOfTypeResources = require('./fixtures/expected/expected_Practitioner_by_of_type.json');
+const expectedPractitionerByOfTypeIncorrectValue = require('./fixtures/expected/expected_Practitioner_by_of_type_incorrect.json');
 
 const {
     commonBeforeEach,
@@ -100,6 +102,37 @@ describe('Practitioner Tests', () => {
                 .set(getHeaders());
             // noinspection JSUnresolvedFunction
             expect(resp).toHaveResponse(expectedPractitionerBySystemResources);
+        });
+
+        test('search_by_identifier with of-type modifier works', async () => {
+            const request = await createTestRequest();
+            // ARRANGE
+            // add the resources to FHIR server
+            let resp = await request
+                .post('/4_0_0/Practitioner/1/$merge?validate=true')
+                .send(practitioner1Resource)
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveMergeResponse({ created: true });
+
+            // ACT & ASSERT
+            // now search by of-type parameter
+            resp = await request
+                .get(
+                    '/4_0_0/Practitioner?_bundle=1&identifier:of-type=http://terminology.hl7.org/CodeSystem/v2-0203|PRN|4657&_debug=1'
+                )
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResponse(expectedPractitionerByOfTypeResources);
+
+            // search with of-type parameter when incorrect value is given
+            resp = await request
+                .get(
+                    '/4_0_0/Practitioner?_bundle=1&identifier:of-type=http://terminology.hl7.org/CodeSystem/v2-0203||4657&_debug=1'
+                )
+                .set(getHeaders());
+            // noinspection JSUnresolvedFunction
+            expect(resp).toHaveResponse(expectedPractitionerByOfTypeIncorrectValue);
         });
     });
 });
