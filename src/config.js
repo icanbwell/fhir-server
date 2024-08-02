acae4bd00a57d87c8b926751fb19d6679ac1d49e
Mintu Sah <mintu.sah@icanbwell.com>
2024-08-02 18:44:42 +0530

    Test appName

--- src/config.js
+++ src/config.js
@@ -29,7 +29,7 @@ delete queryParams.w;
  */
 const options = {
     ...queryParams,
-    appName: 'fhir',
+    // appName: 'fhir',
     connectTimeoutMS: env.MONGO_CONNECT_TIMEOUT ? parseInt(env.MONGO_CONNECT_TIMEOUT) : 60 * 60 * 1000,
     socketTimeoutMS: env.MONGO_SOCKET_TIMEOUT ? parseInt(env.MONGO_SOCKET_TIMEOUT) : 60 * 60 * 1000,
     retryReads: true,
