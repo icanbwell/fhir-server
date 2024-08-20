5ae7ca521e07a75e0e6edd0d025bf1a164f19bb8
mintusah25 <mintu.sah@joshtechnologygroup.com>
2024-08-20 12:07:59 +0530

    EFS-1170: Added ignored urls in dd instrumentation

--- src/dd_instrumentation.js
+++ src/dd_instrumentation.js
@@ -1,3 +1,13 @@
-require('dd-trace').init({
+const tracer = require('dd-trace').init({
     logInjection: true
+});
+
+const ignoreUrls = ['/health', '/live', '/ready'].concat(
+    process.env.OPENTELEMETRY_IGNORE_URLS?.split(',')
+);
+
+tracer.use('http', {
+    server: {
+        blocklist: ignoreUrls
+    }
 });
