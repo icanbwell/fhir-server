75c9a3ca996d017872f8e074c56b40ce4eb4ac0c
shubhamgoel24 <shub.goel@icanbwell.com>
2024-12-19 12:01:45 +0530

    EFS-1357 Removed mutations and custom queries from GraphQL V2

--- src/tests/graphqlv2/medicationRequest/graphql.medicationRequest.test.js
+++ src/tests/graphqlv2/medicationRequest/graphql.medicationRequest.test.js
@@ -1,5 +1,4 @@
 const expectedGraphQlResponse = require('./fixtures/expected_graphql_response.json');
-const expectedGraphQlFilterByCodeResponse = require('./fixtures/expected_graphql_filter_by_code_response.json');
 const patientBundleResource = require('./fixtures/patients.json');
 const medicationRequestBundleResource = require('./fixtures/medication_requests.json');
 const medicationDispenseBundleResource = require('./fixtures/medication_dispenses.json');
@@ -8,7 +7,6 @@ const fs = require('fs');
 const path = require('path');
 
 const query = fs.readFileSync(path.resolve(__dirname, './fixtures/query.graphql'), 'utf8');
-const queryFilterByCode = fs.readFileSync(path.resolve(__dirname, './fixtures/queryFilterByCode.graphql'), 'utf8');
 
 const {
     commonBeforeEach,
@@ -74,54 +72,6 @@ describe('GraphQL MedicationRequest Tests', () => {
                 expect(body.errors).toBeUndefined();
             }
             expect(resp).toHaveResponse(expectedGraphQlResponse, r => {
-                return r;
-            });
-        });
-        test('GraphQL get MedicationRequest filter by code', async () => {
-            const request = await createTestRequest();
-            const graphqlQueryText = queryFilterByCode.replace(/\\n/g, '');
-            let resp = await request.get('/4_0_0/MedicationRequest').set(getHeaders());
-            // noinspection JSUnresolvedFunction
-            expect(resp).toHaveResourceCount(0);
-
-            resp = await request
-                .post('/4_0_0/Patient/$merge')
-                .send(patientBundleResource)
-                .set(getHeaders());
-
-            // noinspection JSUnresolvedFunction
-            expect(resp).toHaveMergeResponse([{ created: true }]);
-
-            resp = await request
-                .post('/4_0_0/MedicationRequest/$merge')
-                .send(medicationRequestBundleResource)
-                .set(getHeaders());
-
-            // noinspection JSUnresolvedFunction
-            expect(resp).toHaveMergeResponse([{ created: true }]);
-
-            resp = await request
-                .post('/4_0_0/MedicationDispense/$merge')
-                .send(medicationDispenseBundleResource)
-                .set(getHeaders());
-
-            // noinspection JSUnresolvedFunction
-            expect(resp).toHaveMergeResponse([{ created: true }]);
-
-            resp = await request
-                .post('/4_0_0/$graphqlv2')
-                .send({
-                    operationName: null,
-                    variables: {},
-                    query: graphqlQueryText
-                })
-                .set(getGraphQLHeaders())
-                .expect(200);
-            const body = resp.body;
-            if (body.errors) {
-                expect(body.errors).toBeUndefined();
-            }
-            expect(resp).toHaveResponse(expectedGraphQlFilterByCodeResponse, r => {
                 return r;
             });
         });
