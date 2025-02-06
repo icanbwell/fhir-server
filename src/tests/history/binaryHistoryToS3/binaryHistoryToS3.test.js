9a80ca3ff340e37392137b2efa86fdcfd0c651b6
shubhamgoel24 <shub.goel@icanbwell.com>
2025-02-06 16:58:49 +0530

    EFS-1411 Filter fields written to MongoDB for S3 history resources

--- src/tests/history/binaryHistoryToS3/binaryHistoryToS3.test.js
+++ src/tests/history/binaryHistoryToS3/binaryHistoryToS3.test.js
@@ -30,6 +30,7 @@ const binaryResources = require('./fixtures/binary/binary.json');
 
 // expected
 const expectedBinaryHistory = require('./fixtures/expected/expectedBinaryHistory.json');
+const expectedBinaryHistoryWithS3Path = require('./fixtures/expected/expectedBinaryHistoryWithS3Path.json');
 const expectedBinaryHistoryS3Data = require('./fixtures/expected/expectedBinaryHistoryS3Data.json');
 
 describe('Binary history resource should be written to S3', () => {
@@ -108,7 +109,7 @@ describe('Binary history resource should be written to S3', () => {
             entry.resource.meta.lastUpdated = null;
         });
 
-        expect(binaryHistoryEntries).toEqual(expectedBinaryHistory);
+        expect(binaryHistoryEntries).toEqual(expectedBinaryHistoryWithS3Path);
 
         // to ignore updated lastUpdated field
         expectedBinaryHistoryS3Data.fileDataWithPath.forEach((element) => {
@@ -116,5 +117,77 @@ describe('Binary history resource should be written to S3', () => {
             return element;
         });
         expect(mockS3UploadInBatchAsync).toHaveReturnedWith(expectedBinaryHistoryS3Data);
+        mockS3UploadInBatchAsync.mockReset();
+    });
+
+
+    test('Binary history resource should not be written to S3 when configured', async () => {
+        let s3HistoryResources = env.S3_HISTORY_RESOURCES;
+        env.S3_HISTORY_RESOURCES = 'Observation';
+
+        const request = await createTestRequest((c) => {
+            c.register(
+                'historyResourceS3Client',
+                (c) =>
+                    new MockS3Client({
+                        bucketName: 'test',
+                        region: 'test'
+                    })
+            );
+            return c;
+        });
+        const container = getTestContainer();
+
+        const mockS3UploadInBatchAsync = jest.spyOn(
+            container.historyResourceS3Client,
+            'uploadInBatchAsync'
+        );
+
+        // Create resource
+        resp = await request.post('/4_0_0/Binary/1/$merge').send(binaryResources).set(getHeaders());
+        // noinspection JSUnresolvedFunction
+        expect(resp).toHaveMergeResponse([{ created: true }, { created: true }]);
+
+        const postRequestProcessor = container.postRequestProcessor;
+        await postRequestProcessor.waitTillDoneAsync({ requestId });
+
+        /**
+         * @type {HistoryTestMongoDatabaseManager}
+         */
+        const mongoDatabaseManager = container.mongoDatabaseManager;
+        /**
+         * mongo fhirDb connection
+         * @type {import('mongodb').Db}
+         */
+        const resourceHistoryDb = await mongoDatabaseManager.getResourceHistoryDbAsync();
+        const resourceHistoryCollections = await resourceHistoryDb.listCollections().toArray();
+        const resourceHistoryCollectionNames = resourceHistoryCollections.map(
+            (collection) => collection.name
+        );
+        expect(resourceHistoryCollectionNames).toEqual(
+            expect.arrayContaining(['Binary_4_0_0', 'Binary_4_0_0_History'])
+        );
+        const binaryHistoryCollection = resourceHistoryDb.collection('Binary_4_0_0_History');
+        /**
+         * @type {import('mongodb').DefaultSchema[]}
+         */
+        const binaryHistoryEntries = await binaryHistoryCollection.find({}).toArray();
+
+        binaryHistoryEntries.forEach((entry) => {
+            entry._id = null;
+            entry.resource.meta.lastUpdated = null;
+        });
+
+        expect(binaryHistoryEntries).toEqual(expectedBinaryHistory);
+
+        // to ignore updated lastUpdated field
+        expectedBinaryHistoryS3Data.fileDataWithPath.forEach((element) => {
+            element.data.resource.meta.lastUpdated = expect.any(String);
+            return element;
+        });
+        expect(mockS3UploadInBatchAsync).toHaveBeenCalledTimes(0);
+
+        mockS3UploadInBatchAsync.mockReset();
+        env.S3_HISTORY_RESOURCES = s3HistoryResources;
     });
 });
