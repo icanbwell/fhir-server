0ccd43d8e97b46c572bf281099b48e088b006e1e
Mintu Kumar Sah <mintu.sah@icanbwell.com>
2026-01-12 23:45:43 +0530

    MS-000 remove redis endpoint that added to debuging

--- src/routeHandlers/fhirServer.js
+++ src/routeHandlers/fhirServer.js
@@ -19,7 +19,6 @@ const httpContext = require('express-http-context');
 const { REQUEST_ID_TYPE } = require('../constants');
 const { convertErrorToOperationOutcome } = require('../utils/convertErrorToOperationOutcome');
 const { ConfigManager } = require('../utils/configManager');
-const {handleRedisRequest} = require('./redis.js');
 
 class MyFHIRServer {
     /**
@@ -377,10 +376,6 @@ class MyFHIRServer {
     } // Setup custom logging
 
     setCustomRoutes () {
-        if (isTrue(process.env.ENABLE_REDIS_ENDPOINT)) {
-            this.app.get('/redis', (req, res) => handleRedisRequest(this.container, req, res))
-            this.app.post('/redis', (req, res) => handleRedisRequest(this.container, req, res));
-        }
         return this;
     }
 }
