919ebe8c65c07868d0ecafa88f50285fff84b577
Mintu Kumar Sah <mintu.sah@icanbwell.com>
2026-04-06 13:00:12 +0530

    unable higher compression for the json field

--- clickhouse-init/02-history-sync-schema.sql
+++ clickhouse-init/02-history-sync-schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS fhir.fhir_resource_history (
     resource_uuid   String,
     mongo_id        String,
     last_updated    DateTime64(3, 'UTC'),
-    raw             String
+    raw             String CODEC(ZSTD(7))
 ) ENGINE = ReplacingMergeTree()
 ORDER BY (resource_type, resource_uuid, last_updated, mongo_id)
 PARTITION BY (resource_type, toYYYYMM(last_updated));
