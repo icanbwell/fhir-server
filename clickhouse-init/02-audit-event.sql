a58b8e9dc21c789eaf1e49242b398adfe1c4f1aa
Mintu Kumar Sah <mintu.sah@icanbwell.com>
2026-04-16 22:24:14 +0530

    DCON-3064: Add bloom filter index on access_tags for fast tenant filtering

--- clickhouse-init/02-audit-event.sql
+++ clickhouse-init/02-audit-event.sql
@@ -48,6 +48,7 @@ CREATE TABLE IF NOT EXISTS fhir.AuditEvent_4_0_0 (
     resource                     JSON(max_dynamic_paths=256),
 
     -- Skip indexes for array search columns
+    INDEX idx_access_tags   access_tags   TYPE bloom_filter(0.01) GRANULARITY 4,
     INDEX idx_entity_what   entity_what   TYPE bloom_filter(0.01) GRANULARITY 4,
     INDEX idx_agent_who     agent_who     TYPE bloom_filter(0.01) GRANULARITY 4
 )
