659e501243660d423e14deacee4d12093fa3fcb1
Mintu Kumar Sah <mintu.sah@icanbwell.com>
2026-04-20 16:10:16 +0530

    DCON-3443: Restore non-Kafka 'Access Log is created' test
    Prior commit over-pruned the file. Restores the Mongo-path assertion
    (via /admin/searchLogResults) and keeps only the Kafka-specific setup
    removed: env flag toggle, MockKafkaClient override, and
    mockKafkaClient.messages assertion.

--- src/tests/accessLog/accessLogs.create.test.js
+++ src/tests/accessLog/accessLogs.create.test.js
@@ -1,14 +1,18 @@
 // test file
 const observationResource = require('./fixtures/observation.json');
+// expected
+const accessLogs1 = require('./fixtures/access-logs1.json');
 
 const {
     commonBeforeEach,
     commonAfterEach,
     getHeaders,
     createTestRequest,
-    getTestContainer
+    getTestContainer,
+    getJsonHeadersWithAdminToken
 } = require('../common');
 const { describe, beforeEach, afterEach, test, expect, jest } = require('@jest/globals');
+const { AccessLogger } = require('../../utils/accessLogger');
 
 describe('AccessLogs Tests', () => {
     beforeEach(async () => {
@@ -20,6 +24,52 @@ describe('AccessLogs Tests', () => {
     });
 
     describe('AccessLogs create Tests', () => {
+        test('Access Log is created', async () => {
+            const request = await createTestRequest((container) => {
+                container.register('accessLogger', (c) => new AccessLogger({
+                    scopesManager: c.scopesManager,
+                    fhirOperationsManager: c.fhirOperationsManager,
+                    configManager: c.configManager,
+                    databaseBulkInserter: c.databaseBulkInserter
+                }));
+                return container;
+            });
+
+            const container = await getTestContainer();
+            /**
+             * @type {AccessLogger}
+             */
+            const accessLogger = container.accessLogger;
+
+            const logAccessLogAsync = jest.spyOn(
+                accessLogger,
+                'logAccessLogAsync'
+            );
+
+            expect(logAccessLogAsync).toHaveBeenCalledTimes(0);
+
+            await request
+                .post('/4_0_0/Observation/$merge')
+                .send(observationResource)
+                .set({...getHeaders(), 'Origin-Service': 'test-server', 'x-request-id': 'test-request-id'})
+                .expect(200);
+
+            expect(logAccessLogAsync).toHaveBeenCalledTimes(1);
+
+            await accessLogger.flushAsync();
+
+            const resp = await request.get('/admin/searchLogResults?id=test-request-id').set(getJsonHeadersWithAdminToken());
+
+            accessLogs1._id = expect.any(String);
+            accessLogs1.timestamp = expect.any(String);
+            accessLogs1.request.start = expect.any(String);
+            accessLogs1.details.host = expect.any(String);
+            accessLogs1.request.end = expect.any(String);
+            accessLogs1.request.systemGeneratedRequestId = expect.any(String);
+            accessLogs1.request.duration = expect.any(Number);
+            expect(resp.body[0]).toEqual(accessLogs1);
+        });
+
         test('AccessLog is called every time as expected', async () => {
             const request = await createTestRequest();
 
