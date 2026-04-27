52677be59105c595d2c4f7685e4ce044a1bedb2e
Vinay Kushwaha <vinay.kushwaha@bwell.com>
2026-04-27 13:24:37 +0530

    DCON-3476: Resolved cms user type from jwt claim user_type

--- src/strategies/authService.js
+++ src/strategies/authService.js
@@ -1,6 +1,5 @@
 const async = require('async');
 const superagent = require('superagent');
-const Sentry = require('@sentry/node');
 const {LRUCache} = require('lru-cache');
 const {
     EXTERNAL_REQUEST_RETRY_COUNT,
@@ -13,7 +12,6 @@ const {logDebug, logError, logInfo} = require('../operations/common/logging');
 const {WellKnownConfigurationManager} = require('../utils/wellKnownConfiguration/wellKnownConfigurationManager');
 const {assertTypeEquals} = require("../utils/assertType");
 const {ConfigManager} = require("../utils/configManager");
-const {UserTypeManager} = require("../utils/userTypeManager");
 
 /**
  * @typedef {Object} UserInfo
@@ -41,12 +39,10 @@ class AuthService {
      * Constructor for the AuthService
      * @param {ConfigManager} configManager
      * @param {WellKnownConfigurationManager} wellKnownConfigurationManager
-     * @param {UserTypeManager} userTypeManager
      */
     constructor({
                     configManager,
-                    wellKnownConfigurationManager,
-                    userTypeManager
+                    wellKnownConfigurationManager
                 }) {
         /**
          * @type {ConfigManager}
@@ -60,18 +56,14 @@ class AuthService {
         this.wellKnownConfigurationManager = wellKnownConfigurationManager;
         assertTypeEquals(wellKnownConfigurationManager, WellKnownConfigurationManager);
 
-        /**
-         * @type {UserTypeManager}
-         */
-        this.userTypeManager = userTypeManager;
-        assertTypeEquals(userTypeManager, UserTypeManager);
-
         this.requestTimeout = (this.configManager.externalRequestTimeoutSec || 30) * 1000;
         this.requiredJWTFields = {
             clientFhirPersonId: 'clientFhirPersonId',
             clientFhirPatientId: 'clientFhirPatientId',
             bwellFhirPersonId: 'bwellFhirPersonId',
-            bwellFhirPatientId: 'bwellFhirPatientId',
+            bwellFhirPatientId: 'bwellFhirPatientId'
+        };
+        this.optionalJWTFields = {
             managingOrganization: 'managingOrganization'
         };
         this.cacheOptions = {
@@ -224,7 +216,7 @@ class AuthService {
             }
             context.personIdFromJwtToken = jwt_payload[this.requiredJWTFields.clientFhirPersonId];
             context.masterPersonIdFromJwtToken = jwt_payload[this.requiredJWTFields.bwellFhirPersonId];
-            context.managingOrganizationId = jwt_payload[this.requiredJWTFields.managingOrganization];
+            context.managingOrganizationId = jwt_payload[this.optionalJWTFields.managingOrganization];
 
             context.subject = jwt_payload['sub'];
             context.username = context.personIdFromJwtToken;
@@ -238,22 +230,8 @@ class AuthService {
                     context.userType = AUTH_USER_TYPES.delegatedUser;
                 }
             }
-            if (this.configManager.enableUserTypeResolutionFromOrganization) {
-                this.userTypeManager.resolveUserTypeAsync({
-                    managingOrganizationId: jwt_payload[this.requiredJWTFields.managingOrganization]
-                }).then((resolvedUserType) => {
-                    if (resolvedUserType) {
-                        context.userType = resolvedUserType;
-                    }
-                    logDebug(`JWT payload`, {user: '', args: {jwt_payload}});
-                    const effectiveUsername = context.username || username;
-                    done(null, {id: client_id, isUser, name: effectiveUsername, username: effectiveUsername}, {scope, context});
-                }).catch((error) => {
-                    logError(`Error resolving user type: ${error.message}`, {error: error});
-                    Sentry.captureException(error);
-                    done(error);
-                });
-                return;
+            if (!context.userType && jwt_payload.user_type) {
+                context.userType = jwt_payload.user_type;
             }
         }
         if (context.userType) {
