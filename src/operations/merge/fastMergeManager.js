2f2b711895175399a0662333a792f343823fd9a8
Nirbhay Kumar Nachiketa <nirbhay.nachiketa@icanbwell.com>
2026-05-18 18:49:39 +0530

    PAY-1831 Add in fastMerger as well

--- src/operations/merge/fastMergeManager.js
+++ src/operations/merge/fastMergeManager.js
@@ -23,6 +23,7 @@ const { logDebug, logError, logWarn } = require('../common/logging');
 const { groupByLambda } = require('../../utils/list.util');
 const { isUuid, generateUUIDv5 } = require('../../utils/uid.util');
 const { mergeObject } = require('../../utils/mergeHelper');
+const { ReadPreference } = require('mongodb');
 const { SecurityTagSystem } = require('../../utils/securityTagSystem');
 const { buildContextDataForHybridStorage } = require('../../utils/contextDataBuilder');
 const deepcopy = require('deepcopy');
@@ -327,7 +328,8 @@ class FastMergeManager {
                 );
 
                 currentResource = await databaseQueryManager.fastFindOneAsync({
-                    query: { _uuid: uuid.toString() }
+                    query: { _uuid: uuid.toString() },
+                    options: { readPreference: ReadPreference.PRIMARY }
                 });
             }
 
