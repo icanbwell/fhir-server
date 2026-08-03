eb54b6f30b476cdeabc6010612e71d96562e6380
Imran Qureshi <imranq2@hotmail.com>
2026-08-03 16:15:57 -0700

    DCON-4780 chore: remove credentialExposureInLogs.test.js from jest ignore list
    Test now passes after merging main (main's superset version of the test
    was kept during conflict resolution).

--- jest.config.js
+++ jest.config.js
@@ -102,7 +102,7 @@ module.exports = {
         '<rootDir>/src/tests/unit/operations/subscription/webhookPhiLeakage.test.js',
         '<rootDir>/src/tests/unit/operations/update/conditionalCrossTenant.test.js',
         '<rootDir>/src/tests/unit/strategies/authFailureMode.test.js',
-        '<rootDir>/src/tests/unit/utils/credentialExposureInLogs.test.js',
+        '<rootDir>/src/tests/unit/strategies/jwtCacheThunderingHerd.test.js',
         '<rootDir>/src/tests/unit/utils/personToPatientIdsExpander.crossTenant.test.js'
     ],
     setupFiles: [
